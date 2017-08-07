7346f37aff52fda0e9ed5c9e19ed4e1f428cf8bd
Devon Govett <devongovett@gmail.com>
2017-08-07 09:36:16 -0700

    Add JSPackager

--- test.js
+++ test.js
@@ -1,12 +1,27 @@
 const Bundle = require('./src/Bundle');
+const JSPackager = require('./src/packagers/JSPackager');
 // const babel = require('babel-core');
 
 process.on('unhandledRejection', console.error)
 
 async function run() {
   let bundle = new Bundle('/Users/devongovett/projects/Storify/liveblog-editor/src/Editor.js');
+
+  // bundle.package('*.css', new CSSPackager).pipe(fs.createWriteStream('out.js'));
+  // bundle.package('*.')
+
+  console.profile();
+
   let module = await bundle.collectDependencies();
-  printDeps(module);
+  // printDeps(module);
+
+  console.profileEnd();
+  console.log('here')
+
+  let packager = new JSPackager;
+  packager.pipe(require('fs').createWriteStream('out.js'));
+  packager.addAsset(module);
+  packager.end();
 }
 
 function printDeps(module, indent = '', deps = new Set) {
@@ -15,7 +30,6 @@ function printDeps(module, indent = '', deps = new Set) {
     if (!deps.has(mod.name)) {
       deps.add(mod.name);
       printDeps(mod, indent + '  ', deps);
-      // babel.transformFromAst(mod.ast);
     }
   }
 }
