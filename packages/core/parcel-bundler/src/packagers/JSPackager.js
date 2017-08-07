c289323b55774ecbcdf261cf3925aef8a9490a08
Devon Govett <devongovett@gmail.com>
2017-08-07 09:36:16 -0700

    Add JSPackager

---
+++ packages/core/parcel-bundler/src/packagers/JSPackager.js
@@ -0,0 +1,55 @@
+const Readable = require('stream').Readable;
+
+const prelude = `(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({`;
+
+class JSPackager extends Readable {
+  constructor(options) {
+    super();
+    this.options = options;
+    this.includedAssets = new Map;
+    this.id = 1;
+    this.first = true;
+  }
+
+  _read() {}
+
+  addAsset(asset) {
+    if (this.includedAssets.has(asset)) {
+      return;
+    }
+
+    if (this.includedAssets.size === 0) {
+      this.push(prelude);
+    }
+
+    let id = this.id++;
+    this.includedAssets.set(asset, id);
+
+    for (let mod of asset.modules.values()) {
+      this.addAsset(mod);
+    }
+
+    // let {code, sourceMap} = asset.transform('js');
+    let wrapped = this.first ? '' : ',';
+
+    wrapped += id + ':[function(require,module,exports) {\n' + asset.contents + '\n},';
+
+    let deps = {};
+    for (let [dep, mod] of asset.modules) {
+      deps[dep] = this.includedAssets.get(mod);
+    }
+
+    wrapped += JSON.stringify(deps);
+    wrapped += ']';
+
+    this.first = false;
+    this.push(wrapped);
+  }
+
+  end() {
+    this.push('},{},' + JSON.stringify([1]) + ')');
+    this.push(null);
+  }
+}
+
+module.exports = JSPackager;
