1aff5f6ac13c70572b98ac3072418ff6075ea734
Devon Govett <devongovett@gmail.com>
2017-09-04 21:23:30 -0700

    Add less plugin to add url() dependencies

--- packages/core/parcel-bundler/test/integration.js
+++ packages/core/parcel-bundler/test/integration.js
@@ -432,6 +432,35 @@ describe('integration', function () {
     assert(css.includes('.base'));
   });
 
+  it('should support linking to assets with url() from less', async function () {
+    let b = await bundle(__dirname + '/integration/less-url/index.js');
+
+    assertBundleTree(b, {
+      name: 'index.js',
+      assets: ['index.js', 'index.less'],
+      childBundles: [{
+        name: 'index.css',
+        assets: ['index.less'],
+        childBundles: [{
+          type: 'raw',
+          assets: ['test.woff2'],
+          childBundles: []
+        }]
+      }]
+    });
+
+    let output = run(b);
+    assert.equal(typeof output, 'function');
+    assert.equal(output(), 2);
+
+    let css = fs.readFileSync(__dirname + '/dist/index.css', 'utf8');
+    assert(/url\("[0-9a-f]+\.woff2"\)/.test(css));
+    assert(css.includes('url("http://google.com")'));
+    assert(css.includes('.index'));
+
+    assert(fs.existsSync(__dirname + '/dist/' + css.match(/url\("([0-9a-f]+\.woff2)"\)/)[1]));
+  });
+
   it('should support transforming less with postcss', async function () {
     let b = await bundle(__dirname + '/integration/less-postcss/index.js');
 
