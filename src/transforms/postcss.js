9aadfedd26a7d6a7f37bc68bd1a27b1b2e26caf1
Devon Govett <devongovett@gmail.com>
2017-09-08 22:28:31 -0700

    Refactor postcss plugin loading

--- src/transforms/postcss.js
+++ src/transforms/postcss.js
@@ -1,4 +1,5 @@
 const localRequire = require('../utils/localRequire');
+const loadPlugins = require('../utils/loadPlugins');
 const postcss = require('postcss');
 const Config = require('../utils/config');
 const cssnano = require('cssnano');
@@ -23,42 +24,18 @@ async function getConfig(asset) {
   }
 
   config = config || {};
-  let plugins = [];
+  config.plugins = loadPlugins(config.plugins, asset.name);
 
-  // load plugins
-  if (typeof config.plugins === 'object' && !Array.isArray(config.plugins)) {
-    for (let key in config.plugins) {
-      let plugin = localRequire(key, asset.name);
-      let options = config.plugins[key];
-      if (typeof options !== 'object') {
-        options = {};
-      }
-
-      // Hack to get CSS modules class map JSON
-      if (key === 'postcss-modules') {
-        options.getJSON = (filename, json) => {
-          asset.cssModules = json;
-        };
-      }
-
-      if (Object.keys(options).length > 0) {
-        plugin = plugin(options);
-      }
-
-      plugin = plugin.postcss || plugin.default || plugin;
-      if (!(typeof plugin === 'object' && Array.isArray(plugin.plugins) || typeof plugin === 'function')) {
-        throw new TypeError('Invalid PostCSS Plugin found: ' + '[' + key + ']')
-      }
-
-      plugins.push(plugin);
-    }
+  if (config.modules) {
+    config.plugins.push(localRequire('postcss-modules', asset.name)({
+      getJSON: (filename, json) => asset.cssModules = json
+    }));
   }
 
   if (asset.options.minify) {
-    plugins.push(cssnano());
+    config.plugins.push(cssnano());
   }
 
-  config.plugins = plugins;
   config.from = asset.name;
   config.to = asset.name;
   return config;
