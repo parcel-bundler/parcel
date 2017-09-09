c746ab32065b35135dd938a187e309864669272e
Devon Govett <devongovett@gmail.com>
2017-09-09 10:14:57 -0700

    Add support for inserting environment variables at build time

--- src/visitors/globals.js
+++ src/visitors/globals.js
@@ -17,6 +17,19 @@ const VARS = {
 };
 
 module.exports = {
+  MemberExpression(node, asset, ancestors) {
+    // Inline environment variables accessed on process.env
+    if (matchesPattern(node.object, 'process.env')) {
+      let key = types.toComputedKey(node);
+      if (types.isStringLiteral(key)) {
+        let val = types.valueToNode(process.env[key.value]);
+        replace(ancestors[ancestors.length - 2], node, val);
+        delete node.object; // prevent traversing into this node
+        asset.isAstDirty = true;
+      }
+    }
+  },
+
   Identifier(node, asset, ancestors) {
     let parent = ancestors[ancestors.length - 2];
     if (VARS.hasOwnProperty(node.name) && !asset.globals.has(node.name) && types.isReferenced(node, parent)) {
@@ -44,4 +57,55 @@ function inScope(ancestors) {
   }
 
   return false;
+}
+
+// from babel-types. remove when we upgrade to babel 7.
+// https://github.com/babel/babel/blob/0189b387026c35472dccf45d14d58312d249f799/packages/babel-types/src/index.js#L347
+function matchesPattern(member, match) {
+  // not a member expression
+  if (!types.isMemberExpression(member)) {
+    return false;
+  }
+
+  const parts = Array.isArray(match) ? match : match.split(".");
+  const nodes = [];
+
+  let node;
+  for (node = member; types.isMemberExpression(node); node = node.object) {
+    nodes.push(node.property);
+  }
+
+  nodes.push(node);
+
+  if (nodes.length !== parts.length) {
+    return false;
+  }
+
+  for (let i = 0, j = nodes.length - 1; i < parts.length; i++, j--) {
+    const node = nodes[j];
+    let value;
+    if (types.isIdentifier(node)) {
+      value = node.name;
+    } else if (types.isStringLiteral(node)) {
+      value = node.value;
+    } else {
+      return false;
+    }
+
+    if (parts[i] !== value) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Replaces the key in `parent` whose value is `from` with `to`
+function replace(parent, from, to) {
+  for (let key in parent) {
+    if (parent[key] === from) {
+      parent[key] = to;
+      break;
+    }
+  }
 }
