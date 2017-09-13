613f0e0e24e83ea9ebab3781727e0c56657e2519
Devon Govett <devongovett@gmail.com>
2017-09-13 08:58:33 -0700

    Implement support for hot module replacement

---
+++ packages/core/parcel-bundler/src/builtins/hmr-runtime.js
@@ -0,0 +1,99 @@
+var global = (1,eval)('this');
+var OldModule = module.bundle.Module;
+function Module() {
+  OldModule.call(this);
+  this.hot = {
+    accept: function (fn) {
+      this._acceptCallback = fn || function () {};
+    },
+    dispose: function (fn) {
+      this._disposeCallback = fn;
+    }
+  };
+}
+
+module.bundle.Module = Module;
+
+if (!module.bundle.parent) {
+  var ws = new WebSocket('ws://localhost:5555/');
+  ws.onmessage = (e) => {
+    var data = JSON.parse(e.data);
+
+    if (data.type === 'update') {
+      for (let asset of data.assets) {
+        hmrApply(global.require, asset);
+      }
+
+      for (let asset of data.assets) {
+        if (!asset.isNew) {
+          hmrAccept(global.require, asset.id);
+        }
+      }
+    }
+  };
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  let parents = [];
+  for (let k in modules) {
+    for (let d in modules[k][1]) {
+      let dep = modules[k][1][d];
+      if (dep === id || (Array.isArray(dep) && dep[dep.length - 1] === id)) {
+        parents.push(+k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    let fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  let cached = bundle.cache[id];
+  if (cached && cached.hot._disposeCallback) {
+    cached.hot._disposeCallback();
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallback) {
+    cached.hot._acceptCallback();
+    return true;
+  }
+
+  return getParents(global.require, id).some(id => hmrAccept(global.require, id));
+}
