b31314379721a9b23360aec7745f25b8ed48f3b1
Devon Govett <devongovett@gmail.com>
2017-09-20 00:09:40 -0700

    Determine which babylon plugins to enable from babelrc
    JSX and Flow plugins are slow, so we want those to be disabled by default.

--- src/transforms/babel.js
+++ src/transforms/babel.js
@@ -3,37 +3,13 @@ const path = require('path');
 const config = require('../utils/config');
 
 module.exports = async function (asset) {
-  if (!(await shouldTransform(asset))) {
+  if (!asset.babelConfig) {
     return;
   }
-
-  // console.time ('babel: ' + process.pid + ':' + asset.name)
 
   await asset.parseIfNeeded();
 
   let res = babel.transformFromAst(asset.ast, asset.contents, {code: false, filename: asset.name});
   asset.ast = res.ast;
   asset.isAstDirty = true;
-  // console.timeEnd('babel: ' + process.pid + ':' + asset.name)
 };
-
-async function shouldTransform(asset) {
-  if (/\.json$/.test(asset.name)) {
-    return false;
-  }
-
-  if (asset.package && asset.package.babel) {
-    return true;
-  }
-
-  // if (asset.package && asset.package.browserify && asset.package.browserify.transform && asset.package.browserify.transform.includes('babelify')) {
-  //   return true;
-  // }
-
-  let babelrc = await config.resolve(asset.name, ['.babelrc', '.babelrc.js']);
-  if (babelrc) {
-    return true;
-  }
-
-  return false;
-}
