ed43dcca86efeae184eaeef696a15abd4e8d2629
Devon Govett <devongovett@gmail.com>
2017-09-29 00:13:19 -0700

    Add tests for server

---
+++ test/server.js
@@ -0,0 +1,65 @@
+const assert = require('assert');
+const fs = require('fs');
+const {bundler, run, assertBundleTree} = require('./utils');
+const http = require('http');
+
+describe('server', function () {
+  let server;
+  afterEach(function () {
+    if (server) {
+      server.close();
+      server = null;
+    }
+  });
+
+  function get(file) {
+    return new Promise((resolve, reject) => {
+      http.get('http://localhost:' + server.address().port + file, res => {
+        if (res.statusCode !== 200) {
+          return reject(new Error('Request failed: ' + res.statusCode));
+        }
+
+        res.setEncoding('utf8');
+        let data = '';
+        res.on('data', c => data += c);
+        res.on('end', () => {
+          resolve(data);
+        });
+      });
+    });
+  }
+
+  it('should serve files', async function () {
+    let b = bundler(__dirname + '/integration/commonjs/index.js');
+    server = b.serve(0);
+
+    let data = await get('/dist/index.js');
+    assert.equal(data, fs.readFileSync(__dirname + '/dist/index.js', 'utf8'));
+  });
+
+  it('should serve a default page if the main bundle is an HTML asset', async function () {
+    this.timeout(5000);
+    let b = bundler(__dirname + '/integration/html/index.html');
+    server = b.serve(0);
+
+    let data = await get('/');
+    assert.equal(data, fs.readFileSync(__dirname + '/dist/index.html', 'utf8'));
+
+    data = await get('/foo/bar');
+    assert.equal(data, fs.readFileSync(__dirname + '/dist/index.html', 'utf8'));
+  });
+
+  it('should serve a 404 if the file does not exist', async function () {
+    let b = bundler(__dirname + '/integration/commonjs/index.js');
+    server = b.serve(0);
+
+    let threw = false;
+    try {
+      await get('/dist/fake.js');
+    } catch (err) {
+      threw = true;
+    }
+
+    assert(threw);
+  });
+});
