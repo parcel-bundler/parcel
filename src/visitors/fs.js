e0af8bcbccc755a84d2afc666a228bd056da69f1
Devon Govett <devongovett@gmail.com>
2017-09-29 00:14:44 -0700

    Inline files when calling fs.readFileSync

---
+++ src/visitors/fs.js
@@ -0,0 +1,156 @@
+const t = require('babel-types');
+const Path = require('path');
+const fs = require('fs');
+const template = require('babel-template');
+
+const bufferTemplate = template('Buffer(CONTENT, ENC)');
+
+module.exports = {
+  AssignmentExpression(path) {
+    if (!isRequire(path.node.right, 'fs', 'readFileSync')) {
+      return;
+    }
+
+    for (let name in path.getBindingIdentifiers()) {
+      const binding = path.scope.getBinding(name);
+      if (!binding) continue;
+
+      binding.path.setData('__require', path.node);
+    }
+  },
+
+  CallExpression(path, asset) {
+    let callee = path.node.callee;
+    if (referencesImport(path, 'fs', 'readFileSync')) {
+      let vars = {__dirname: Path.dirname(asset.name), __filename: asset.basename};
+      let [filename, ...args] = path.get('arguments').map(arg => evaluate(arg, vars));
+      filename = Path.resolve(filename);
+
+      let res = fs.readFileSync(filename, ...args);
+      let replacementNode;
+      if (Buffer.isBuffer(res)) {
+        replacementNode = bufferTemplate({
+          CONTENT: t.stringLiteral(res.toString('base64')),
+          ENC: t.stringLiteral('base64')
+        });
+      } else {
+        replacementNode = t.stringLiteral(res);
+      }
+
+      asset.addDependency(filename, {includedInParent: true});
+      path.replaceWith(replacementNode);
+      asset.isAstDirty = true;
+    }
+  }
+};
+
+function isRequire(node, name, method) {
+  // e.g. require('fs').readFileSync
+  if (t.isMemberExpression(node) && node.property.name === method) {
+    node = node.object;
+  }
+
+  if (!t.isCallExpression(node)) {
+    return false;
+  }
+
+  let {callee, arguments: args} = node;
+  let isRequire = t.isIdentifier(callee)
+    && callee.name === 'require'
+    && args.length === 1
+    && t.isStringLiteral(args[0]);
+
+  if (!isRequire) {
+    return false;
+  }
+
+  if (name && args[0].value !== name) {
+    return false;
+  }
+
+  return true;
+}
+
+function referencesImport(path, name, method) {
+  let callee = path.node.callee;
+  let bindingPath;
+
+  // e.g. readFileSync()
+  if (t.isIdentifier(callee)) {
+    bindingPath = getBindingPath(path, callee.name);
+  } else if (t.isMemberExpression(callee)) {
+    if (callee.property.name !== method) {
+      return false;
+    }
+
+    // e.g. fs.readFileSync()
+    if (t.isIdentifier(callee.object)) {
+      bindingPath = getBindingPath(path, callee.object.name);
+
+    // require('fs').readFileSync()
+    } else if (isRequire(callee.object, name)) {
+      return true;
+    }
+  } else {
+    return false;
+  }
+
+  if (!bindingPath) {
+    return;
+  }
+
+  let bindingNode = bindingPath.getData('__require') || bindingPath.node;
+  let parent = bindingPath.parentPath;
+
+  // e.g. import fs from 'fs';
+  if (parent.isImportDeclaration()) {
+    if (bindingPath.isImportSpecifier() && bindingPath.node.imported.name !== method) {
+      return false;
+    }
+
+    return parent.node.source.value === name;
+
+  // e.g. var fs = require('fs');
+  } else if (t.isVariableDeclarator(bindingNode) || t.isAssignmentExpression(bindingNode)) {
+    let left = bindingNode.id || bindingNode.left;
+    let right = bindingNode.init || bindingNode.right;
+
+    // e.g. var {readFileSync} = require('fs');
+    if (t.isObjectPattern(left)) {
+      let prop = left.properties.find(p => p.value.name === callee.name);
+      if (!prop || prop.key.name !== method) {
+        return false;
+      }
+    } else if (!t.isIdentifier(left)) {
+      return false;
+    }
+
+    return isRequire(right, name, method);
+  }
+
+  return false;
+}
+
+function getBindingPath(path, name) {
+  let binding = path.scope.getBinding(name);
+  return binding && binding.path;
+}
+
+function evaluate(path, vars) {
+  // Inline variables
+  path.traverse({
+    Identifier: function (ident) {
+      let key = ident.node.name;
+      if (key in vars) {
+        ident.replaceWith(t.valueToNode(vars[key]));
+      }
+    }
+  });
+
+  let res = path.evaluate();
+  if (!res.confident) {
+    throw new Error('Could not statically evaluate fs call');
+  }
+
+  return res.value;
+}
---
+++ test/fs.js
@@ -0,0 +1,54 @@
+const assert = require('assert');
+const fs = require('fs');
+const {bundle, run, assertBundleTree} = require('./utils');
+
+describe('fs', function () {
+  it('should inline a file as a string', async function () {
+    let b = await bundle(__dirname + '/integration/fs/index.js');
+    let output = run(b);
+    assert.equal(output, 'hello');
+  });
+
+  it('should inline a file as a buffer', async function () {
+    let b = await bundle(__dirname + '/integration/fs-buffer/index.js');
+    let output = run(b);
+    assert.equal(output.constructor.name, 'Buffer');
+    assert.equal(output.length, 5);
+  });
+
+  it('should inline a file with fs require alias', async function () {
+    let b = await bundle(__dirname + '/integration/fs-alias/index.js');
+    let output = run(b);
+    assert.equal(output, 'hello');
+  });
+
+  it('should inline a file with fs require inline', async function () {
+    let b = await bundle(__dirname + '/integration/fs-inline/index.js');
+    let output = run(b);
+    assert.equal(output, 'hello');
+  });
+
+  it('should inline a file with fs require assignment', async function () {
+    let b = await bundle(__dirname + '/integration/fs-assign/index.js');
+    let output = run(b);
+    assert.equal(output, 'hello');
+  });
+
+  it('should inline a file with fs require assignment alias', async function () {
+    let b = await bundle(__dirname + '/integration/fs-assign-alias/index.js');
+    let output = run(b);
+    assert.equal(output, 'hello');
+  });
+
+  it('should inline a file with fs require destructure', async function () {
+    let b = await bundle(__dirname + '/integration/fs-destructure/index.js');
+    let output = run(b);
+    assert.equal(output, 'hello');
+  });
+
+  it('should inline a file with fs require destructure assignment', async function () {
+    let b = await bundle(__dirname + '/integration/fs-destructure-assign/index.js');
+    let output = run(b);
+    assert.equal(output, 'hello');
+  });
+});
