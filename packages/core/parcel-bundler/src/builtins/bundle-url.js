85c6bf7503b1b719e37e7ee961e82a0448c08b8c
Devon Govett <devongovett@gmail.com>
2017-12-06 20:37:43 -0800

    firefox fix, #36
    commit 49c2cc213aa73dce89ab9021bd5d09bca0e783b6
    Author: Devon Govett <devongovett@gmail.com>
    Date:   Wed Dec 6 20:35:19 2017 -0800
    
        cleanup
    
    commit 091177ff471e68703449fa608ab72123c9565ea6
    Merge: dbba6ef 1afb09f
    Author: Devon Govett <devongovett@gmail.com>
    Date:   Wed Dec 6 20:33:22 2017 -0800
    
        Merge branch 'master' of https://github.com/DeMoorJasper/parcel into DeMoorJasper-master
    
    commit 1afb09f001df2ce0a9e0ff8e41fddbf0b3014b15
    Merge: eb70efb 90f69ff
    Author: Jasper De Moor <jdemo@live.be>
    Date:   Wed Dec 6 21:20:57 2017 +0100
    
        Merge branch 'master' into master
    
    commit eb70efbb98af2150d3b70ad078c5c16bf7019085
    Merge: 2d81326 90f69ff
    Author: Jasper De Moor <jdemo@live.be>
    Date:   Wed Dec 6 21:20:26 2017 +0100
    
        Merge branch 'master' into master
    
    commit 2d8132637deef533acc27bf2d888be2f25aa4a5c
    Author: Jasper De Moor <jasperdemoor@gmail.com>
    Date:   Wed Dec 6 20:43:54 2017 +0100
    
        firefox fix, #36
    
    commit 1f5ed9f6243e0c69ca5ae6ccc44eac6127e64cbb
    Author: Jasper De Moor <jasperdemoor@gmail.com>
    Date:   Wed Dec 6 17:02:42 2017 +0100
    
        Tiny gitignore cleanup

--- packages/core/parcel-bundler/src/builtins/bundle-url.js
+++ packages/core/parcel-bundler/src/builtins/bundle-url.js
@@ -12,7 +12,7 @@ function getBundleURL() {
   try {
     throw new Error;
   } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp):\/\/[^\)]+/g);
+    var matches = ('' + err.stack).match(/(https?|file|ftp):\/\/[^\)\n]+/g);
     if (matches) {
       return getBaseURL(matches[0]);
     }
