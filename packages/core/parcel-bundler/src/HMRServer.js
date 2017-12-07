b4e0036bc3b5209b1be09bfaf43af0f778e45ec5
spalger <spalger@users.noreply.github.com>
2017-12-07 13:28:14 -0700

    Emit bundling errors via HMR

--- packages/core/parcel-bundler/src/HMRServer.js
+++ packages/core/parcel-bundler/src/HMRServer.js
@@ -6,6 +6,12 @@ class HMRServer {
       this.wss = new WebSocket.Server({port: 0}, resolve);
     });
 
+    this.wss.on('connection', (ws) => {
+      if (this.unresolvedError) {
+        ws.send(JSON.stringify(this.unresolvedError))
+      }
+    });
+
     return this.wss._server.address().port;
   }
 
@@ -13,8 +19,50 @@ class HMRServer {
     this.wss.close();
   }
 
+  emitError(err) {
+    let message = typeof err === 'string' ? err : err.message;
+    if (!message) {
+      message = 'unknown error'
+    }
+
+    if (err.fileName) {
+      let fileName = err.fileName;
+      if (err.loc) {
+        fileName += `:${err.loc.line}:${err.loc.column}`;
+      }
+
+      message = `${fileName}: ${message}`;
+    }
+
+    let stack
+    if (err.codeFrame) {
+      stack = err.codeFrame;
+    } else if (err.stack) {
+      stack = err.stack.slice(err.stack.indexOf('\n') + 1);
+    }
+    
+    // store the most recent error so we can notify new connections
+    // and so we can broadcast when the error is resolved
+    this.unresolvedError = {
+      type: 'error',
+      error: {
+        message,
+        stack
+      }
+    }
+
+    this.broadcast(this.unresolvedError)
+  }
+
   emitUpdate(assets) {
-    let msg = JSON.stringify({
+    if (this.unresolvedError) {
+      this.unresolvedError = null
+      this.broadcast({
+        type: 'error-resolved'
+      })
+    }
+
+    this.broadcast({
       type: 'update',
       assets: assets.map(asset => {
         let deps = {};
@@ -30,9 +78,12 @@ class HMRServer {
         };
       })
     });
-
+  }
+  
+  broadcast(msg) {
+    const json = JSON.stringify(msg)
     for (let ws of this.wss.clients) {
-      ws.send(msg);
+      ws.send(json);
     }
   }
 }
