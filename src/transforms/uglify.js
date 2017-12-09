95ea5361c73292187e48526f97a962cae3e3d2f3
Jasper De Moor <jasperdemoor@gmail.com>
2017-12-09 11:49:01 +0100

    uglify-es

--- src/transforms/uglify.js
+++ src/transforms/uglify.js
@@ -1,4 +1,4 @@
-const {AST_Node, minify} = require('uglify-js');
+const {AST_Node, minify} = require('uglify-es');
 const {toEstree} = require('babel-to-estree');
 const types = require('babel-types');
 const walk = require('babylon-walk');
@@ -6,7 +6,7 @@ const walk = require('babylon-walk');
 module.exports = async function(asset) {
   await asset.parseIfNeeded();
 
-  // Convert to UglifyJS AST
+  // Convert to UglifyES AST
   var ast = AST_Node.from_mozilla_ast(toEstree(asset.ast, asset.contents));
   var result = minify(ast, {
     toplevel: true
