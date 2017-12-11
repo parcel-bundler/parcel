4bcf01b01c49c4fcae3fedb0aa339af29b4722ef
Shawn Presser <shawnpresser@gmail.com>
2017-12-10 21:30:49 -0600

    Simplify CoffeeScriptAsset

--- src/assets/CoffeeScriptAsset.js
+++ src/assets/CoffeeScriptAsset.js
@@ -3,19 +3,13 @@ const config = require('../utils/config');
 const localRequire = require('../utils/localRequire');
 
 class CoffeeScriptAsset extends JSAsset {
-  async transform() {
-    super.transform();
-
-    await this.parseIfNeeded();
-    this.isAstDirty = true;
-  }
-
   async parse(code) {
     // require coffeescript, installed locally in the app
     let coffee = localRequire('coffeescript', this.name);
 
     // Transpile Module using CoffeeScript and parse result as ast format through babylon
-    return await super.parse(coffee.compile(code));
+    this.contents = coffee.compile(code, {});
+    return await super.parse(this.contents);
   }
 }
 
