08f1c617e92b238167ff60c73907cbdf407be67f
Suman <ssubash@commvault.com>
2017-12-16 15:15:39 -0500

    Removing the boolean variable. And using the options variable

--- src/Server.js
+++ src/Server.js
@@ -32,7 +32,9 @@ function middleware(bundler) {
     function sendIndex() {
       // If the main asset is an HTML file, serve it
       if (bundler.mainAsset.type === 'html') {
-        req.url = `/${bundler.mainAsset.generateBundleName(true)}`;
+        req.url = `/${bundler.mainAsset.generateBundleName(
+          bundler.options.mainFile
+        )}`;
         serve(req, res, send404);
       } else {
         send404();
