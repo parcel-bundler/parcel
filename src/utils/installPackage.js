a2cc052c528c217353f8342b11805ff15511eeb7
Jasper De Moor <jasperdemoor@gmail.com>
2017-12-16 14:49:34 +0100

    fix secu vuln and add yarn support

--- src/utils/installPackage.js
+++ src/utils/installPackage.js
@@ -1,15 +1,37 @@
-const exec = require('child_process').exec;
+const {spawn} = require('child_process');
+const config = require('./config');
 
-module.exports = function(dir, name) {
+module.exports = async function(dir, name) {
+  let yarn;
+  try {
+    yarn = await config.resolve(dir, ['yarn.lock']);
+  } catch (e) {}
+
   return new Promise((resolve, reject) => {
-    exec(
-      `cd ${dir} && npm install ${name} --save-dev`,
-      (error, stdout, stderr) => {
-        if (error) {
-          return reject(new Error(`Failed to install package ${name}`));
-        }
-        return resolve(name);
+    let options = {
+      cwd: dir
+    };
+
+    let install = spawn('yarn', ['add', name, '--dev'], options);
+    if (!yarn) {
+      install = spawn('npm', ['install', name, '--save-dev'], options);
+    }
+
+    install.stdout.on('data', data => {
+      // TODO: Log this using logger
+      // console.log(data.toString());
+    });
+
+    install.stderr.on('data', data => {
+      // TODO: Log this using logger
+      // console.log(data.toString());
+    });
+
+    install.on('close', code => {
+      if (code !== 0) {
+        reject(new Error(`Failed to install ${name}.`));
       }
-    );
+      resolve();
+    });
   });
 };
