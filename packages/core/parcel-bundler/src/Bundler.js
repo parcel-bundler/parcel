023b16ebf9b54274174185e364c2dde3dbb38aeb
Pontus Alexander <pomle@users.noreply.github.com>
2017-12-20 07:59:20 +0100

    Display absolute path on failed dependency resolve. (#271)
    * Display absolute path on failed dependency resolve.
    
    When a file path can not be resolved the error message is cleaned
    up but the relative path to file is displayed.
    
    In cases where a mistake have been made this path can be very confusing,
    like `Cannot resolve dependency './../../../../../../main.js'` for when
    a path starts with `/`.
    
    This PR resolves attempted path into an absolute path in hope of
    reducing confusion and minimize cognitive overhead of `..` chasing.
    
    * Resolve absolute path relative to asset.
    
    * Log both relative name and absolute search path.

--- packages/core/parcel-bundler/src/Bundler.js
+++ packages/core/parcel-bundler/src/Bundler.js
@@ -274,7 +274,8 @@ class Bundler extends EventEmitter {
       return await this.resolveAsset(dep.name, asset.name);
     } catch (err) {
       if (err.message.indexOf(`Cannot find module '${dep.name}'`) === 0) {
-        err.message = `Cannot resolve dependency '${dep.name}'`;
+        const absPath = Path.resolve(Path.dirname(asset.name), dep.name);
+        err.message = `Cannot resolve dependency '${dep.name}' at '${absPath}'`;
 
         // Generate a code frame where the dependency was used
         if (dep.loc) {
