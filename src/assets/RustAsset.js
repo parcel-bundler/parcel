001270436ddb1f4c546aeab8202068409414b82b
Jose Albizures <albizures3601@gmail.com>
2017-12-22 23:05:07 -0600

    RustAsset refactor  get rid of wargo  use wasm32-unknown-unknown

--- src/assets/RustAsset.js
+++ src/assets/RustAsset.js
@@ -3,87 +3,34 @@ const path = require('path');
 const toml = require('toml');
 const {exec} = require('child-process-promise');
 
+const md5 = require('../utils/md5');
 const fs = require('../utils/fs');
 const JSAsset = require('./JSAsset');
 const localRequire = require('../utils/localRequire');
 
-const rustTarget = `wasm32-unknown-emscripten`;
+const rustTarget = `wasm32-unknown-unknown`;
 
 class RustAsset extends JSAsset {
   collectDependencies() {
     // Do nothing. Dependencies are collected by cargo :).
   }
 
-  async parse(code) {
-    const {name, encoding, options} = this;
+  async parse() {
     const release = process.env.NODE_ENV === 'production';
-    const cmd = `wargo build ${release ? ' --release' : ''}`;
-
-    const projectDir = path.join(path.dirname(name), '..');
-    const cargoFile = await fs.readFile(
-      path.join(projectDir, 'Cargo.toml'),
-      encoding
+    const {dir, ext, name, base} = path.parse(this.name);
+    const wasmPath = path.join(
+      this.options.publicURL,
+      md5(this.name) + '.wasm'
     );
-    const packageName = toml.parse(cargoFile.toString()).package.name;
+    const cmd = `rustc +nightly --target ${rustTarget} -O --crate-type=cdylib ${base} -o .${wasmPath} ${
+      release ? ' --release' : ''
+    }`;
 
-    const outDir = path.join(
-      projectDir,
-      'target',
-      rustTarget,
-      release ? 'release' : 'debug'
-    );
-    const depsDir = path.join(outDir, 'deps');
+    await exec(cmd, {cwd: dir});
 
-    const outFile = path.join(outDir, `${packageName}.js`);
+    const pathToWasm = JSON.stringify(wasmPath);
 
-    await exec(cmd, {cwd: projectDir});
-
-    const out = await fs.readFile(outFile, encoding);
-    const deps = await fs.readdir(path.join(outDir, 'deps'));
-
-    const wasmFile = deps.find(f => /\.wasm$/.test(f));
-
-    if (!wasmFile) {
-      throw new Error('No wasm file found', null);
-    }
-
-    const generatedName = this.addURLDependency(path.join(depsDir, wasmFile));
-
-    const wasmBinaryFile = url.resolve(
-      path.join(options.publicURL, generatedName),
-      ''
-    );
-
-    const Module = {
-      // Path in the built project to the wasm file
-      wasmBinaryFile,
-      // Indicates that we are NOT running in node, despite 'require' being defined
-      ENVIRONMENT: 'WEB'
-    };
-
-    if (options.from === 'html') {
-      // if the rust file is called from an html, it's going to be called right away
-      // when the wasm file is loaded as a standalone executable
-      this.contents = `((Module) => {
-        ${out}
-      })(${JSON.stringify(Module)})`;
-    } else {
-      this.contents = `module.exports = (function(existingModule){
-        return {
-          // Returns a promise that resolves when the wasm runtime is initialized and ready for use
-          initialize: function(userDefinedModule) {
-            return new Promise((resolve, reject) => {
-              if (!userDefinedModule) {
-                userDefinedModule = {}
-              }
-              var Module = Object.assign({}, userDefinedModule, existingModule);
-              Module['onRuntimeInitialized'] = () => resolve(Module);
-              \n${out}\n
-            });
-          }
-        }
-      })(${JSON.stringify(Module)})`;
-    }
+    this.contents = `module.exports=${pathToWasm};`;
 
     return await super.parse(this.contents);
   }
