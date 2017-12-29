500de12c7ce3782353392b7e8e2128d30c517bb2
Jeremy Danyow <jdanyow@gmail.com>
2017-12-29 15:16:08 -0500

    Add url dependency for serviceWorker.register calls (#398)
    * Add url dependency for serviceWorker.register calls
    
    * extract matchesPattern helper
    
    * sync SW regexp with matchesPattern arg

--- packages/core/parcel-bundler/src/assets/JSAsset.js
+++ packages/core/parcel-bundler/src/assets/JSAsset.js
@@ -15,6 +15,7 @@ const config = require('../utils/config');
 const IMPORT_RE = /\b(?:import\b|export\b|require\s*\()/;
 const GLOBAL_RE = /\b(?:process|__dirname|__filename|global|Buffer)\b/;
 const FS_RE = /\breadFileSync\b/;
+const SW_RE = /\bnavigator\s*\.\s*serviceWorker\s*\.\s*register\s*\(/;
 
 class JSAsset extends Asset {
   constructor(name, pkg, options) {
@@ -30,7 +31,8 @@ class JSAsset extends Asset {
     return (
       !/.js$/.test(this.name) ||
       IMPORT_RE.test(this.contents) ||
-      GLOBAL_RE.test(this.contents)
+      GLOBAL_RE.test(this.contents) ||
+      SW_RE.test(this.contents)
     );
   }
 
