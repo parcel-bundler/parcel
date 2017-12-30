ec034b62726e9de6247e5586350127180522f29a
Jose Albizures <albizures3601@gmail.com>
2017-12-29 19:58:54 -0600

    use dep-info to watch the rust deps

--- src/assets/RustAsset.js
+++ src/assets/RustAsset.js
@@ -3,27 +3,55 @@ const {exec} = require('child-process-promise');
 
 const urlJoin = require('../utils/urlJoin');
 const md5 = require('../utils/md5');
+const fs = require('../utils/fs');
 const JSAsset = require('./JSAsset');
 
 const rustTarget = `wasm32-unknown-unknown`;
 
 class RustAsset extends JSAsset {
+  async getRustDeps(dir, base, name) {
+    const depsCmd = `rustc ${base} --emit=dep-info`;
+    // TODO: this will generate a .d file, we need to figure out what to do with this kind of temp files
+    await exec(depsCmd, {cwd: dir});
+
+    const deps = await fs.readFile(
+      path.format({
+        dir,
+        name,
+        ext: '.d'
+      }),
+      this.encoding
+    );
+
+    return deps
+      .split('\n')
+      .filter(Boolean)
+      .slice(1)
+      .map(dep => path.join(dir, dep.replace(':', '')));
+  }
+
   collectDependencies() {
-    // Do nothing. Dependencies are collected by cargo :).
+    for (let dep of this.rustDeps) {
+      console.log(dep);
+      console.log(dep);
+      this.addDependency(dep, {includedInParent: true});
+    }
   }
 
   async parse() {
     const release = process.env.NODE_ENV === 'production';
-    const {dir, base} = path.parse(this.name);
+    const {dir, base, name} = path.parse(this.name);
     const generatedName = md5(this.name);
     const wasmPath = path.join(this.options.outDir, generatedName + '.wasm');
     const wasmUrl = urlJoin(this.options.publicURL, generatedName + '.wasm');
 
-    const cmd = `rustc +nightly --target ${rustTarget} -O --crate-type=cdylib ${base} -o ${wasmPath} ${
+    const compileCmd = `rustc +nightly --target ${rustTarget} -O --crate-type=cdylib ${base} -o ${wasmPath} ${
       release ? ' --release' : ''
     }`;
 
-    await exec(cmd, {cwd: dir});
+    await exec(compileCmd, {cwd: dir});
+
+    this.rustDeps = await this.getRustDeps(dir, base, name);
 
     const urlToWasm = JSON.stringify(wasmUrl);
 
