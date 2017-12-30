2836ac2635f7157567db501b02152b5b4e46e260
Devon Govett <devongovett@gmail.com>
2017-12-29 20:21:40 -0500

    Update scheme regex

--- src/utils/is-url.js
+++ src/utils/is-url.js
@@ -4,7 +4,7 @@ const isURL = require('is-url');
 const ANCHOR_REGEXP = /^#/;
 
 // Matches scheme (ie: tel:, mailto:, data:, itms-apps:)
-const SCHEME_REGEXP = /^[a-z-]*:/i;
+const SCHEME_REGEXP = /^[a-z][a-z0-9\-+.]*:/i;
 
 module.exports = function(url) {
   return isURL(url) || ANCHOR_REGEXP.test(url) || SCHEME_REGEXP.test(url);
