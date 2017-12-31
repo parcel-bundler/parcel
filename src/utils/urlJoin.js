e3d9f074f4b9aca7d01f8503c9430be7388549b1
senze.fan <fansenze@songxiaocai.com>
2017-12-31 14:47:36 +0800

    fix: when the publicUrl is url not a path of os

--- src/utils/urlJoin.js
+++ src/utils/urlJoin.js
@@ -1,8 +1,13 @@
 const url = require('url');
 const path = require('path');
+const isUrl = require('./is-url');
 
 module.exports = function(publicURL, assetPath) {
-  // Use url.resolve to normalize path for windows
+  // Use url.resolve to resolve path when the publicURL is a url
+  // Use path.normalize to normalize path for windows
   // from \path\to\res.js to /path/to/res.js
-  return url.resolve(path.join(publicURL, assetPath), '');
+
+  return isUrl(publicURL)
+    ? url.resolve(publicURL, assetPath)
+    : path.normalize(path.join(publicURL, assetPath), '');
 };
