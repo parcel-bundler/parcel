39c26b1d2483ddef12646793eb53054d0928c41d
senze.fan <fansenze@songxiaocai.com>
2017-12-31 17:42:10 +0800

    fix

--- src/utils/urlJoin.js
+++ src/utils/urlJoin.js
@@ -4,10 +4,10 @@ const isUrl = require('./is-url');
 
 module.exports = function(publicURL, assetPath) {
   // Use url.resolve to resolve path when the publicURL is a url
-  // Use path.normalize to normalize path for windows
+  // Use url.resolve to normalize path for windows
   // from \path\to\res.js to /path/to/res.js
 
   return isUrl(publicURL)
     ? url.resolve(publicURL, assetPath)
-    : path.normalize(path.join(publicURL, assetPath));
+    : url.resolve(path.join(publicURL, assetPath), '');
 };
