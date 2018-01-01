ee8f043930d1ea89a3bba6c73327857076060bd4
Richard Torruellas <richard.torruellas@am.sony.com>
2018-01-01 10:46:40 -0800

    check for data coming back from elm compiler compileToStringSync method

--- src/assets/ElmAsset.js
+++ src/assets/ElmAsset.js
@@ -2,19 +2,39 @@ const JSAsset = require('./JSAsset');
 const localRequire = require('../utils/localRequire');
 const process = require('process');
 
+// Currently no pretty way to install Elm tools on behalf of the user :/
+async function compile(name, options) {
+  try {
+    const elmCompiler = await localRequire('node-elm-compiler', name);
+    // compileToStringSync does not throw an Error, even when there is one.
+    // Other methods will throw an error, but do not bubble up to Parcel
+    const data = elmCompiler.compileToStringSync(name, options);
+    if (!data) {
+      throw 'You either have an error in your code, or are missing the Elm tools here https://guide.elm-lang.org/install.html.';
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
 class ElmAsset extends JSAsset {
-  getParserOptions() {
+  async getParserOptions() {
     const defaultOptions = {
       yes: true,
-      cwd: process.cwd()
+      cwd: process.cwd(),
+      verbose: true
     };
+    // Use a local copy of elm-make when in test
+    if (process.env.NODE_ENV === 'test') {
+      defaultOptions.pathToMake = require.resolve('elm/binwrappers/elm-make');
+    }
     return defaultOptions;
   }
 
   async parse() {
-    let elmCompiler = await localRequire('node-elm-compiler', this.name);
-    const options = this.getParserOptions();
-    const data = elmCompiler.compileToStringSync(this.name, options);
+    const options = await this.getParserOptions();
+    const data = await compile(this.name, options);
     this.contents = data.toString();
     return await super.parse(this.contents);
   }
