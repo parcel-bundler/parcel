ac46f9204e47b378fb722c6b3cab353444ff0f49
senze.fan <fansenze@songxiaocai.com>
2018-01-02 10:30:13 +0800

    add test

---
+++ test/resolver.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const {bundle, run, assertBundleTree} = require('./utils');
+
+describe('resolver', function() {
+  it('should transfrom "~" to "process.cwd()"', async function() {
+    let b = await bundle(__dirname + '/integration/path/index.js');
+
+    assertBundleTree(b, {
+      name: 'index.js',
+      assets: ['index.js', 'local.js'],
+      childBundles: []
+    });
+
+    let output = run(b);
+
+    assert.equal(typeof output, 'function');
+    assert.equal(output(), 7);
+  });
+});
