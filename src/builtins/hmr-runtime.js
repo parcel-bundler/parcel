e1decde85ec461fc1116c3426692e7403bb134db
arianon <arianon@openmailbox.org>
2018-01-03 05:55:20 -0400

    HMR: Access `location` from `global`

--- src/builtins/hmr-runtime.js
+++ src/builtins/hmr-runtime.js
@@ -15,7 +15,7 @@ function Module() {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent && typeof WebSocket !== 'undefined') {
-  var ws = new WebSocket('ws://' + location.hostname + ':{{HMR_PORT}}/');
+  var ws = new WebSocket('ws://' + global.location.hostname + ':{{HMR_PORT}}/');
   ws.onmessage = function(event) {
     var data = JSON.parse(event.data);
 
