d3900ba82d1ef95a1dbca37c53b0aa82429b2feb
Devon Govett <devongovett@gmail.com>
2018-01-06 13:24:47 -0500

    Simplify urlJoin
    Just use URL.parse always, and use posix path joining.

--- src/utils/urlJoin.js
+++ src/utils/urlJoin.js
@@ -1,30 +1,12 @@
-const url = require('url');
+const URL = require('url');
 const path = require('path');
-const isUrl = require('./is-url');
 
+/**
+ * Joins a path onto a URL, and normalizes Windows paths
+ * e.g. from \path\to\res.js to /path/to/res.js.
+ */
 module.exports = function(publicURL, assetPath) {
-  // Use url.format to resolve path when the publicURL is a url
-  // Use url.resolve to normalize path for windows
-  // from \path\to\res.js to /path/to/res.js
-
-  if (isUrl(publicURL)) {
-    const urlObj = url.parse(publicURL);
-    const regexp = /[?#](.)*/;
-
-    const rightUrlObj = Object.assign({}, urlObj, {
-      href: urlObj.href.replace(regexp, ''),
-      pathname: url.resolve(path.join(urlObj.pathname, assetPath), ''),
-      path: url.resolve(
-        `${path.join(
-          urlObj.path.replace(regexp, ''),
-          assetPath
-        )}${urlObj.search || ''}${urlObj.hash || ''}`,
-        ''
-      )
-    });
-
-    return url.format(rightUrlObj);
-  }
-
-  return url.resolve(path.join(publicURL, assetPath), '');
+  const url = URL.parse(publicURL);
+  url.pathname = path.posix.join(url.pathname, URL.parse(assetPath).pathname);
+  return URL.format(url);
 };
--- test/url-join.js
+++ test/url-join.js
@@ -2,27 +2,35 @@ const assert = require('assert');
 const urlJoin = require('../src/utils/urlJoin');
 
 describe('Url Join', () => {
-  it('should return url', () => {
+  it('should join a filename with a URL', () => {
     assert.equal(
       urlJoin('https://parceljs.org', 'a.js'),
       'https://parceljs.org/a.js'
     );
+  });
 
+  it('should join a path with a URL', () => {
     assert.equal(
       urlJoin('https://parceljs.org', 'bar/a.js'),
       'https://parceljs.org/bar/a.js'
     );
+  });
 
+  it('should join a paths together', () => {
     assert.equal(
       urlJoin('https://parceljs.org/foo/', 'bar/a.js'),
       'https://parceljs.org/foo/bar/a.js'
     );
+  });
 
+  it('should join an absolute path with a URL', () => {
     assert.equal(
       urlJoin('https://parceljs.org/foo/', '/bar/a.js'),
       'https://parceljs.org/foo/bar/a.js'
     );
+  });
 
+  it('should join a URL with a querystring', () => {
     assert.equal(
       urlJoin('https://parceljs.org/foo?a=123', '/bar/a.js'),
       'https://parceljs.org/foo/bar/a.js?a=123'
@@ -32,7 +40,9 @@ describe('Url Join', () => {
       urlJoin('https://parceljs.org/foo?a=123&b=456', '/bar/a.js'),
       'https://parceljs.org/foo/bar/a.js?a=123&b=456'
     );
+  });
 
+  it('should join a URL with a hash', () => {
     assert.equal(
       urlJoin('https://parceljs.org/foo#hello', '/bar/a.js'),
       'https://parceljs.org/foo/bar/a.js#hello'
@@ -42,10 +52,16 @@ describe('Url Join', () => {
       urlJoin('https://parceljs.org/foo?a=123&b=456#hello', '/bar/a.js'),
       'https://parceljs.org/foo/bar/a.js?a=123&b=456#hello'
     );
+  });
 
+  it('should join two paths together', () => {
     assert.equal(
       urlJoin('/Users/people/projects/parcel', '/bar/foo.js'),
       '/Users/people/projects/parcel/bar/foo.js'
     );
+  });
+
+  it('should support windows paths', () => {
+    assert.equal(urlJoin('dist\\foo', '\\bar\\foo.js'), 'dist/foo/bar/foo.js');
   });
 });
