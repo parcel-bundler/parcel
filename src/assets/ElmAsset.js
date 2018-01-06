8a29b5009226d25a27043bd1156a8acbd905131c
Kevin Kim <fnjoonshikkim@gmail.com>
2018-01-06 21:45:38 +0100

    Removed babel-eslint and inlined compile function

--- src/assets/ElmAsset.js
+++ src/assets/ElmAsset.js
@@ -3,22 +3,6 @@ const localRequire = require('../utils/localRequire');
 const process = require('process');
 const config = require('../utils/config');
 
-// Currently no pretty way to install Elm tools on behalf of the user :/
-async function compile(name, options) {
-  try {
-    const elmCompiler = await localRequire('node-elm-compiler', name);
-    // compileToStringSync does not throw an Error, even when there is one.
-    // Other methods will throw an error, but do not bubble up to Parcel
-    const data = elmCompiler.compileToStringSync(name, options);
-    if (!data) {
-      throw 'You either have an error in your code, or are missing the Elm tools here https://guide.elm-lang.org/install.html.';
-    }
-    return data;
-  } catch (error) {
-    throw error;
-  }
-}
-
 class ElmAsset extends JSAsset {
   async getParserOptions() {
     let defaultOptions = {
@@ -45,9 +29,25 @@ class ElmAsset extends JSAsset {
 
   async parse() {
     const options = await this.getParserOptions();
-    const data = await compile(this.name, options);
+    const data = await this.compile(this.name, options);
     this.contents = data.toString();
     return await super.parse(this.contents);
+  }
+
+  // Currently no pretty way to install Elm tools on behalf of the user :/
+  async compile(name, options) {
+    try {
+      const elmCompiler = await localRequire('node-elm-compiler', name);
+      // compileToStringSync does not throw an Error, even when there is one.
+      // Other methods will throw an error, but do not bubble up to Parcel
+      const data = elmCompiler.compileToStringSync(name, options);
+      if (!data) {
+        throw 'You either have an error in your code, or are missing the Elm tools here https://guide.elm-lang.org/install.html.';
+      }
+      return data;
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
