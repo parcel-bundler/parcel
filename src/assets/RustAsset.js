5e5a96f1aa79cdac8bbb8764ef106fb24deaf5e8
Jose Albizures <albizures3601@gmail.com>
2018-01-14 18:00:34 -0600

    use wasm-gc in build

--- src/assets/RustAsset.js
+++ src/assets/RustAsset.js
@@ -29,16 +29,13 @@ class RustAsset extends JSAsset {
   }
 
   async rustcParse() {
-    const release = process.env.NODE_ENV === 'production';
     const {dir, base, name} = path.parse(this.name);
     const wasmPath = path.format({
       dir,
       name,
       ext: '.wasm'
     });
-    const compileCmd = `rustc +nightly --target ${rustTarget} -O --crate-type=cdylib ${base} -o ${wasmPath} ${
-      release ? ' --release' : ''
-    }`;
+    const compileCmd = `rustc +nightly --target ${rustTarget} -O --crate-type=cdylib ${base} -o ${wasmPath}`;
 
     await exec(compileCmd, {cwd: dir});
     this.rustDeps = await this.getRustDeps(dir, base, name);
@@ -51,6 +48,7 @@ class RustAsset extends JSAsset {
   }
 
   async parse() {
+    const isProduction = process.env.NODE_ENV === 'production';
     try {
       await commandExists('rustc');
     } catch (e) {
@@ -60,6 +58,16 @@ class RustAsset extends JSAsset {
     }
 
     const wasmPath = await this.rustcParse();
+    if (isProduction) {
+      try {
+        await commandExists('wasm-gc');
+      } catch (e) {
+        await exec(
+          'cargo install --git https://github.com/alexcrichton/wasm-gc'
+        );
+      }
+      await exec('wasm-gc ${wasmPath} ${wasmPath}');
+    }
     const nameModule = './' + path.relative(path.dirname(this.name), wasmPath);
 
     this.contents = `module.exports = require(${JSON.stringify(nameModule)})`;
