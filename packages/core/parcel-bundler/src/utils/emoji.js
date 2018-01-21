b5773c6d4091a5486c9da2b40abfcf74d8678261
Brandon Smith <brandon93s@gmail.com>
2018-01-21 18:02:21 -0500

    Improve Windows emoji support detection (#607)

--- packages/core/parcel-bundler/src/utils/emoji.js
+++ packages/core/parcel-bundler/src/utils/emoji.js
@@ -1,4 +1,5 @@
-const supportsEmoji = process.platform !== 'win32' || process.env.VSCODE_PID;
+const supportsEmoji =
+  process.platform !== 'win32' || process.env.TERM === 'xterm-256color';
 
 // Fallback symbols for Windows from https://en.wikipedia.org/wiki/Code_page_437
 exports.progress = supportsEmoji ? '⏳' : '∞';
