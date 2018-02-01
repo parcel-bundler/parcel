896952dae2d9a1d5f8a34918bb172618ba991fed
niico <5653050+niicojs@users.noreply.github.com>
2018-02-01 05:35:15 +0100

    launch https websocket server is --https (#708)

--- packages/core/parcel-bundler/src/Bundler.js
+++ packages/core/parcel-bundler/src/Bundler.js
@@ -78,6 +78,7 @@ class Bundler extends EventEmitter {
       minify:
         typeof options.minify === 'boolean' ? options.minify : isProduction,
       hmr: typeof options.hmr === 'boolean' ? options.hmr : watch,
+      https: options.https || false,
       logLevel: typeof options.logLevel === 'number' ? options.logLevel : 3,
       mainFile: this.mainFile,
       hmrPort: options.hmrPort || 0,
@@ -248,7 +249,7 @@ class Bundler extends EventEmitter {
 
     if (this.options.hmr) {
       this.hmr = new HMRServer();
-      this.options.hmrPort = await this.hmr.start(this.options.hmrPort);
+      this.options.hmrPort = await this.hmr.start(this.options);
     }
 
     this.farm = WorkerFarm.getShared(this.options);
