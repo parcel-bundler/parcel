b26cb2867abf4bfe3aa1122564f33d2e4cfa2b62
David Nagli <davidnagli@gmail.com>
2018-03-06 12:21:47 -0500

    Changed localrequire to pass in 'basedir' instead of 'path' to install()

--- src/utils/localRequire.js
+++ src/utils/localRequire.js
@@ -13,7 +13,7 @@ async function localRequire(name, path, triedInstall = false) {
       resolved = resolve.sync(name, {basedir});
     } catch (e) {
       if (e.code === 'MODULE_NOT_FOUND' && !triedInstall) {
-        await install(path, [name]);
+        await install(basedir, [name], true, true);
         return localRequire(name, path, true);
       }
       throw e;
