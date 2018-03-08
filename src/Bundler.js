parcel-bundler/parcel

commit 88630f2a7aebf141ae060f9dc6edaa1dc2089f37

fix hmr bug (#921)

src/Bundler.js
@@ -93,7 +93,8 @@ class Bundler extends EventEmitter {
       rootDir: Path.dirname(this.mainFile),
       sourceMaps:
         typeof options.sourceMaps === 'boolean' ? options.sourceMaps : true,
-      hmrHostname: options.hmrHostname || '',
+      hmrHostname:
+        options.hmrHostname || options.target === 'electron' ? 'localhost' : '',
       detailedReport: options.detailedReport || false
     };
   }

commit 51eb4559e6156ad33f4363e8dedf43b763941625

Fix HMR hostname in browser
Fixes regression introduced in #921

src/Bundler.js
@@ -94,7 +94,8 @@ class Bundler extends EventEmitter {
       sourceMaps:
         typeof options.sourceMaps === 'boolean' ? options.sourceMaps : true,
       hmrHostname:
-        options.hmrHostname || options.target === 'electron' ? 'localhost' : '',
+        options.hmrHostname ||
+        (options.target === 'electron' ? 'localhost' : ''),
       detailedReport: options.detailedReport || false
     };
   }