be453c905e50d9e55164cb6b816bf0866e0c9c50
DominikH <dominik@my1a.de>
2018-03-09 00:44:49 +0100

    Extended worker test to proof that not all required assets are included in the worker bundle.

--- test/integration/workers/index-alternative.js
+++ test/integration/workers/index-alternative.js
@@ -1,4 +1,4 @@
-exports.commonFunction = require('./common').commonFunction;
 exports.startWorker = require('./worker-client').startWorker;
+exports.commonFunction = require('./common').commonFunction;
 exports.feature = require('./feature');
 
