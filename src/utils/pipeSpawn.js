fc654d76dc5b5691219ba441985bcf71178c1cf3
David Nagli <davidnagli@gmail.com>
2018-03-18 02:15:37 -0400

    Automatically install missing dependencies, part 2 (#805)

--- src/utils/pipeSpawn.js
+++ src/utils/pipeSpawn.js
@@ -1,9 +1,18 @@
 const spawn = require('cross-spawn');
+const logger = require('../Logger');
 
 function pipeSpawn(cmd, params, opts) {
-  const cp = spawn(cmd, params, opts);
-  cp.stdout.pipe(process.stdout);
-  cp.stderr.pipe(process.stderr);
+  const cp = spawn(cmd, params, Object.assign({
+    env: Object.assign({
+      FORCE_COLOR: logger.color,
+      npm_config_color: logger.color ? 'always': '',
+      npm_config_progress: true
+    }, process.env)
+  }, opts));
+
+  cp.stdout.setEncoding('utf8').on('data', d => logger.writeRaw(d));
+  cp.stderr.setEncoding('utf8').on('data', d => logger.writeRaw(d));
+
   return new Promise((resolve, reject) => {
     cp.on('error', reject);
     cp.on('close', function(code) {
@@ -11,6 +20,7 @@ function pipeSpawn(cmd, params, opts) {
         return reject(new Error(cmd + ' failed.'));
       }
 
+      logger.clear();
       return resolve();
     });
   });
