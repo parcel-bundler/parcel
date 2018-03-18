ce89b2a8cad7d90621ce049701af40e066fcba85
Devon Govett <devongovett@gmail.com>
2018-03-18 11:18:29 -0700

    Clean up getCertificate
    No need to check if the files exist - just try to read them and catch the errors if they fail.

--- src/utils/getCertificate.js
+++ src/utils/getCertificate.js
@@ -1,14 +1,13 @@
 const fs = require('./fs');
 
 async function getCertificate(options) {
-  const key =
-    (await fs.exists(options.key)) && (await fs.readFile(options.key));
-  const cert =
-    (await fs.exists(options.cert)) && (await fs.readFile(options.cert));
-  if (!key || !cert) {
-    throw 'Certificate and/or key not found';
+  try {
+    let cert = await fs.readFile(options.cert);
+    let key = await fs.readFile(options.key);
+    return {key, cert};
+  } catch (err) {
+    throw new Error('Certificate and/or key not found');
   }
-  return {key, cert};
 }
 
 module.exports = getCertificate;
