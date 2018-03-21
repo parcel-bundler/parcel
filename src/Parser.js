5a5ead58c53c1d4a41bededa6eec825888967970
David Nagli <davidnagli@gmail.com>
2018-03-21 16:48:10 -0400

    Add support for .jade extension
    As per @brandon93's request

--- src/Parser.js
+++ src/Parser.js
@@ -35,6 +35,7 @@ class Parser {
     this.registerExtension('html', './assets/HTMLAsset');
     this.registerExtension('htm', './assets/HTMLAsset');
     this.registerExtension('pug', './assets/PugAsset');
+    this.registerExtension('jade', './assets/PugAsset');
     this.registerExtension('rs', './assets/RustAsset');
 
     this.registerExtension('webmanifest', './assets/WebManifestAsset');
