484800ec4341d308f84198fd4150d7c1f5d85169
Devon Govett <devongovett@gmail.com>
2018-03-25 14:49:09 -0700

    Implement scoped style support

--- src/assets/VueAsset.js
+++ src/assets/VueAsset.js
@@ -1,5 +1,6 @@
 const Asset = require('../Asset');
 const localRequire = require('../utils/localRequire');
+const md5 = require('../utils/md5');
 
 class VueAsset extends Asset {
   constructor(name, pkg, options) {
@@ -7,19 +8,26 @@ class VueAsset extends Asset {
     this.type = 'js';
   }
 
-  async generate() {
+  async parse(code) {
     // Is being used in component-compiler-utils, errors if not installed...
-    await localRequire('vue-template-compiler', this.name);
-    const vue = await localRequire('@vue/component-compiler-utils', this.name);
+    this.vueTemplateCompiler = await localRequire(
+      'vue-template-compiler',
+      this.name
+    );
+    this.vue = await localRequire('@vue/component-compiler-utils', this.name);
 
-    let descriptor = vue.parse({
-      source: this.contents,
+    return this.vue.parse({
+      source: code,
       needMap: this.options.sourceMaps,
       filename: this.relativeName, // Used for sourcemaps
       sourceRoot: '' // Used for sourcemaps. Override so it doesn't use cwd
     });
+  }
 
+  async generate() {
+    let descriptor = this.ast;
     let parts = [];
+
     if (descriptor.script) {
       parts.push({
         type: descriptor.script.lang || 'js',
@@ -48,14 +56,10 @@ class VueAsset extends Asset {
   }
 
   async postProcess(generated) {
-    // Is being used in component-compiler-utils, errors if not installed...
-    const vueTemplateCompiler = await localRequire(
-      'vue-template-compiler',
-      this.name
-    );
-    const vue = await localRequire('@vue/component-compiler-utils', this.name);
-
     let result = [];
+
+    let hasScoped = this.ast.styles.some(s => s.scoped);
+    let scopeId = hasScoped ? `data-v-${md5(this.name).slice(-6)}` : null;
 
     // Combine JS output. This is because the CSS asset generates some JS for HMR.
     // TODO: deal with export for CSS modules too
@@ -63,24 +67,7 @@ class VueAsset extends Asset {
       .filter(r => r.type === 'js')
       .reduce((p, r) => (p += r.value), '');
 
-    let html = generated.find(r => r.type === 'html');
-    if (html) {
-      let template = vue.compileTemplate({
-        source: html.value,
-        filename: this.relativeName,
-        compiler: vueTemplateCompiler,
-        isProduction: this.options.production === true
-      });
-
-      if (Array.isArray(template.errors) && template.errors.length >= 1) {
-        throw new Error(template.errors[0]);
-      }
-
-      js += `\nObject.assign(exports.default || module.exports, (function () {
-        ${template.code}
-        return {render, staticRenderFns, _compiled: true};
-      })())\n`;
-    }
+    js += this.compileTemplate(generated, scopeId);
 
     if (js) {
       result.push({
@@ -96,11 +83,7 @@ class VueAsset extends Asset {
       result.push(map);
     }
 
-    // Combine CSS outputs
-    // TODO: process with vue.compileStyle for scoped CSS transform
-    let css = generated
-      .filter(r => r.type === 'css')
-      .reduce((p, r) => (p += r.value), '');
+    let css = this.compileStyle(generated, scopeId);
     if (css) {
       result.push({
         type: 'css',
@@ -109,6 +92,61 @@ class VueAsset extends Asset {
     }
 
     return result;
+  }
+
+  compileTemplate(generated, scopeId) {
+    let html = generated.find(r => r.type === 'html');
+    if (html) {
+      let template = this.vue.compileTemplate({
+        source: html.value,
+        filename: this.relativeName,
+        compiler: this.vueTemplateCompiler,
+        isProduction: this.options.production,
+        compilerOptions: {
+          scopeId
+        }
+      });
+
+      if (Array.isArray(template.errors) && template.errors.length >= 1) {
+        throw new Error(template.errors[0]);
+      }
+
+      return `
+        Object.assign(exports.default || module.exports, (function () {
+          ${template.code}
+          return {render, staticRenderFns, _compiled: true, _scopeId: ${JSON.stringify(
+            scopeId
+          )}};
+        })())
+      `;
+    }
+
+    return '';
+  }
+
+  compileStyle(generated, scopeId) {
+    return generated.filter(r => r.type === 'css').reduce((p, r, i) => {
+      let css = r.value;
+      let scoped = this.ast.styles[i].scoped;
+
+      // Process scoped styles if needed.
+      if (scoped) {
+        let {code, errors} = this.vue.compileStyle({
+          source: css,
+          filename: this.relativeName,
+          id: scopeId,
+          scoped
+        });
+
+        if (errors.length) {
+          throw errors[0];
+        }
+
+        css = code;
+      }
+
+      return p + css;
+    }, '');
   }
 }
 
