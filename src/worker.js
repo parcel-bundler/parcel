69625e057f350e5f7cbfd6e2f2b162180a289a67
Jasper De Moor <jdemo@live.be>
2018-04-15 19:53:45 +0200

    Custom workerfarm, BI-Directional IPC (#1105)

--- src/worker.js
+++ src/worker.js
@@ -1,32 +1,39 @@
 require('v8-compile-cache');
 const Pipeline = require('./Pipeline');
+const child = require('./workerfarm/child');
+const WorkerFarm = require('./workerfarm/WorkerFarm');
 
 let pipeline;
 
-exports.init = function(options, callback) {
+function init(options, isLocal = false) {
   pipeline = new Pipeline(options || {});
   Object.assign(process.env, options.env || {});
   process.env.HMR_PORT = options.hmrPort;
   process.env.HMR_HOSTNAME = options.hmrHostname;
-  callback();
-};
+  if (isLocal) {
+    process.env.WORKER_TYPE = 'parcel-worker';
+  }
+}
 
-exports.run = async function(path, pkg, options, isWarmUp, callback) {
+async function run(path, pkg, options, isWarmUp) {
   try {
     options.isWarmUp = isWarmUp;
-    var result = await pipeline.process(path, pkg, options);
-
-    callback(null, result);
-  } catch (err) {
-    let returned = err;
-    returned.fileName = path;
-    callback(returned);
+    return await pipeline.process(path, pkg, options);
+  } catch (e) {
+    e.fileName = path;
+    throw e;
   }
-};
+}
 
-process.on('unhandledRejection', function(err) {
-  // ERR_IPC_CHANNEL_CLOSED happens when the worker is killed before it finishes processing
-  if (err.code !== 'ERR_IPC_CHANNEL_CLOSED') {
-    console.error('Unhandled promise rejection:', err.stack);
+// request.location is a module path relative to src or lib
+async function addCall(request, awaitResponse = true) {
+  if (process.send && process.env.WORKER_TYPE === 'parcel-worker') {
+    return child.addCall(request, awaitResponse);
+  } else {
+    return WorkerFarm.getShared().processRequest(request);
   }
-});
+}
+
+exports.init = init;
+exports.run = run;
+exports.addCall = addCall;
