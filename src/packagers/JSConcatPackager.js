e6ea095bd38a10f5f01d65730499e0078a687af1
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2018-04-17 08:19:30 +0200

    Use same wildcard helper as babel

--- src/packagers/JSConcatPackager.js
+++ src/packagers/JSConcatPackager.js
@@ -56,14 +56,13 @@ class JSConcatPackager extends Packager {
 (function () {
   function $parcel$exportWildcard(dest, source) {
     Object.keys(source).forEach(function(key) {
-      if(key !== 'default') {
-        Object.defineProperty(dest, key, {
-          enumerable: true,
-          get() {
-            return source[key];
-          }
-        });
-      }
+      if(key === 'default' || key === "__esModule") return;
+      Object.defineProperty(dest, key, {
+        enumerable: true,
+        get: function get() {
+          return source[key];
+        }
+      });
     });
 
     return dest;
