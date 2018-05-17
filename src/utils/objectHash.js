8aa1b13331876158acd7e1252795789e0a3e9055
odykyi <24225497+odykyi@users.noreply.github.com>
2018-05-17 17:25:08 +0300

    Update objectHash.js

--- src/utils/objectHash.js
+++ src/utils/objectHash.js
@@ -4,7 +4,7 @@ function objectHash(object) {
   let hash = crypto.createHash('md5');
   for (let key of Object.keys(object).sort()) {
     let val = object[key];
-    if (typeof val === 'object' && val) {
+    if (typeof val === 'object' && val !== null) {
       hash.update(key + objectHash(val));
     } else {
       hash.update(key + val);
