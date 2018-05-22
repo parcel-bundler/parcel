0ef05f5d862b6798d2f1fc7a2fd60d081a2cbc1d
Steffen A. L <steffenl.sw@gmail.com>
2018-05-22 04:41:26 +0200

    Fix Vue asset supplemental code concatenation (#1362)

--- packages/core/parcel-bundler/src/assets/VueAsset.js
+++ packages/core/parcel-bundler/src/assets/VueAsset.js
@@ -87,6 +87,10 @@ class VueAsset extends Asset {
       supplemental = code;
     }
 
+    if (supplemental) {
+      supplemental = `\n(function(){${supplemental}})();`;
+    }
+
     js += supplemental;
 
     if (js) {
