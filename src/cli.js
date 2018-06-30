d3f11b4a26c7a012cba3e96fe02e5e3e1cf72528
Goran Gajic <fero.null@gmail.com>
2018-06-30 13:17:36 +0200

    fix build not exiting in dev env
    When NODE_ENV is set to something else instead of `production` build was
    not exiting. This bug was intruduced in
    1b6a93f3efa1d8a4e9e04beda1a5545770e9fb07 commit

--- src/cli.js
+++ src/cli.js
@@ -177,6 +177,7 @@ async function bundle(main, command) {
   const Bundler = require('../');
 
   if (command.name() === 'build') {
+    command.watch = false;
     process.env.NODE_ENV = process.env.NODE_ENV || 'production';
   } else {
     process.env.NODE_ENV = process.env.NODE_ENV || 'development';
