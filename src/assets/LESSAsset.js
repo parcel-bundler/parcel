7b3e43dfc47a52bc32ca4c3192d32b57c18b54c6
Guðjón Jónsson <gudjonj@gmail.com>
2018-07-30 02:49:35 +0000

    Resolve imports from paths also specified in LESS config

--- src/assets/LESSAsset.js
+++ src/assets/LESSAsset.js
@@ -18,8 +18,9 @@ class LESSAsset extends Asset {
     let render = promisify(less.render.bind(less));
 
     let opts =
-      (await this.getConfig(['.lessrc', '.lessrc.js'], {packageKey: 'less'})) ||
-      {};
+      (await this.getConfig(['.lessrc', '.lessrc.js'], {
+        packageKey: 'less'
+      })) || {};
     opts.filename = this.name;
     opts.plugins = (opts.plugins || []).concat(urlPlugin(this));
 
@@ -28,7 +29,9 @@ class LESSAsset extends Asset {
 
   collectDependencies() {
     for (let dep of this.ast.imports) {
-      this.addDependency(dep, {includedInParent: true});
+      this.addDependency(dep, {
+        includedInParent: true
+      });
     }
   }
 
@@ -80,16 +83,64 @@ function getFileManager(less, options) {
       return false;
     }
 
-    async loadFile(filename, currentDirectory) {
-      filename = parseCSSImport(filename);
-      let resolved = await resolver.resolve(
+    async loadFile(filename, currentDirectory, options) {
+      let resolved = await this.findResolution(
         filename,
-        path.join(currentDirectory, 'index')
+        currentDirectory,
+        options
       );
+
       return {
         contents: await fs.readFile(resolved.path, 'utf8'),
         filename: resolved.path
       };
+    }
+
+    async findResolution(filename, currentDirectory, options) {
+      const filenamesTried = [];
+      let paths = path.isAbsolute(filename) ? [] : [currentDirectory];
+
+      if (options.paths) {
+        paths = paths.concat(options.paths);
+      }
+
+      if (!path.isAbsolute(filename) && !paths.includes('.') === -1) {
+        paths.push('.');
+      }
+
+      // promise is guaranteed to be asyncronous
+      // which helps as it allows the file handle
+      // to be closed before it continues with the next file
+      return new Promise(function(resolve, reject) {
+        (async function tryPathIndex(i) {
+          if (i < paths.length) {
+            let fullFilename = resolver.resolveFilename(
+              parseCSSImport(filename),
+              paths[i]
+            );
+            try {
+              const resolution = await resolver.resolve(fullFilename, paths[i]);
+              if (resolution) {
+                resolve(resolution);
+              } else {
+                throw new Error();
+              }
+            } catch (err) {
+              filenamesTried.push(fullFilename);
+              tryPathIndex(i + 1);
+            }
+          } else {
+            reject({
+              type: 'File',
+              message:
+                "'" +
+                filename +
+                "' wasn't found. Tried - " +
+                filenamesTried.join(',')
+            });
+          }
+        })(0);
+      });
     }
   }
 
