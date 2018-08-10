59194acd1b083bd1a8a2355053f897265ec6ee70
Jasper De Moor <jasperdemoor@gmail.com>
2018-08-10 10:19:54 -0700

    Fix test

--- test/javascript.js
+++ test/javascript.js
@@ -29,7 +29,7 @@ describe('javascript', function() {
   });
 
   it('should produce a basic JS bundle with object rest spread support', async function() {
-    let b = await bundle(__dirname + '/integration/es6/object-rest-spread.js');
+    let b = await bundle(__dirname + '/integration/object-rest-spread/object-rest-spread.js');
 
     assert.equal(b.assets.size, 1);
 
