1bec001331262f88f3254593f5a24041ffd75cdd
Maia Teegarden <padmaia@me.com>
2018-08-14 15:32:51 -0700

    Orchestrator and AssetGraphBuilder wip

---
+++ packages/core/core-v2/src/AssetGraphBuilder.js
@@ -0,0 +1,35 @@
+const AssetGraph = require('./AssetGraph');
+const Queue = require('./Queue');
+const Emittery = require('emittery');
+
+
+
+export default class AssetGraphBuilder extends Emittery {
+  constructor() {
+    this.assetGraph = new AssetGraph();
+    this.transformQueue = new Queue();
+    this.resolver = new Resolver();
+  }
+
+  async build(entries) {
+    entries.forEach((moduleSpecifier) => {
+      this.processModuleRequest({
+        srcPath: cwd,
+        moduleSpecifier
+      })
+    });
+  }
+
+  async update(event) {
+    
+  }
+
+  async processModuleRequest(moduleRequest) {
+    let resolvedPath = this.resolver.resolve(moduleRequest);
+    
+  }
+
+  async transformInWorker(filePath) {
+
+  }
+}
--- packages/core/core-v2/src/Orchestrator.js
+++ packages/core/core-v2/src/Orchestrator.js
@@ -3,17 +3,32 @@
 const EventEmitter = require('events');
 
 class Orchestrator extends EventEmitter {
-  constructor() {
-    this.resolver = new Resolver();
-    this.workerFarm = new WorkerFarm();
-    this.transformerQueue = new TransformQueue();
-    this.packagerQueue = new PackagerQueue();
+  constructor(entries) {
+
     this.watcher = new Watcher();
     this.hmrServer = new HmrServer();
+
+    this.entries = entries;
+    
+    this.assetGraphBuilder = new AssetGraphBuilder();
+    this.bundleBuilder = new BundleBuilder();
+    
+    this.assetGraphBuilder.on('complete', (assetGraph) => {
+      this.bundleBuilder.build(assetGraph);
+    });
+    
   }
 
   async bundle() {
-    // ...
+    this.assetGraphBuilder.build(this.entries);
+
+    if (this.options.watch) {
+      this.watcher.on('change', this.onChange.bind(this));
+    }
+  }
+
+  async onChange(event) {
+    this.assetGraphBuilder.update(event);
   }
 }
 
