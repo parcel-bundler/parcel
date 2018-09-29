1e6fc95064ac973406a065b0f1135461b775b0ec
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2018-09-29 08:04:27 +0200

    Fix hoist of modules with multiple aliases
    Can be reproduced using the `to-function` package

--- test/scope-hoisting.js
+++ test/scope-hoisting.js
@@ -1053,5 +1053,17 @@ describe('scope hoisting', function() {
       let output = await run(b);
       assert.deepEqual(output, 9);
     });
+
+    it('should support two aliases to the same module', async function() {
+      let b = await bundle(
+        path.join(
+          __dirname,
+          '/integration/scope-hoisting/commonjs/wrap-aliases/a.js'
+        )
+      );
+
+      let output = await run(b);
+      assert.deepEqual(output, 42);
+    });
   });
 });
