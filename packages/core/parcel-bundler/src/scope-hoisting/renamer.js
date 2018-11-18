fa9ef65e8ad1dcab70c69bd5401af65fd46c982b
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2018-11-18 09:52:17 +0100

    Change back renamer and call crawl instead

--- packages/core/parcel-bundler/src/scope-hoisting/renamer.js
+++ packages/core/parcel-bundler/src/scope-hoisting/renamer.js
@@ -3,7 +3,31 @@ function rename(scope, oldName, newName) {
     return;
   }
 
-  scope.rename(oldName, newName);
+  let binding = scope.getBinding(oldName);
+
+  // Rename all constant violations
+  for (let violation of binding.constantViolations) {
+    let bindingIds = violation.getBindingIdentifierPaths(true, false);
+    for (let name in bindingIds) {
+      if (name === oldName) {
+        for (let idPath of bindingIds[name]) {
+          idPath.node.name = newName;
+        }
+      }
+    }
+  }
+
+  // Rename all references
+  for (let path of binding.referencePaths) {
+    if (path.node.name === oldName) {
+      path.node.name = newName;
+    }
+  }
+
+  // Rename binding identifier, and update scope.
+  scope.removeOwnBinding(oldName);
+  scope.bindings[newName] = binding;
+  binding.identifier.name = newName;
 }
 
 module.exports = rename;
