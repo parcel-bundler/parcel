0f0fdd2a7f6a2ee81ddf20bc0342f95117b3c0f9
Devon Govett <devongovett@gmail.com>
2018-12-16 19:20:35 -0800

    Fix ast returned by babel transformer

--- packages/transformers/babel/src/babel6.js
+++ packages/transformers/babel/src/babel6.js
@@ -1,9 +1,9 @@
 // @flow
-import type {Asset} from '@parcel/types';
+import type {Asset, AST} from '@parcel/types';
 import localRequire from '@parcel/utils/localRequire';
 import {babel6toBabel7} from './astConverter';
 
-export default async function babel6(asset: Asset, options) {
+export default async function babel6(asset: Asset, options): Promise<?AST> {
   let babel = await localRequire('babel-core', asset.filePath);
 
   let config = options.config;
@@ -27,7 +27,11 @@ export default async function babel6(asset: Asset, options) {
 
   let res = babel.transform(asset.code, config);
   if (res.ast) {
-    return babel6toBabel7(res.ast);
+    return {
+      type: 'babel',
+      version: '7.0.0',
+      program: babel6toBabel7(res.ast)
+    };
   }
 }
 
