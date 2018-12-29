9c3dfcba8b45326b03cb945516370bed4dd22da4
Devon Govett <devongovett@gmail.com>
2018-12-28 19:42:05 -0500

    Support .browserslistrc in TargetResolver

--- packages/core/core/src/TargetResolver.js
+++ packages/core/core/src/TargetResolver.js
@@ -4,10 +4,12 @@ import type {
   PackageJSON,
   Target,
   Environment,
-  EnvironmentContext
+  EnvironmentContext,
+  Engines
 } from '@parcel/types';
 import {loadConfig} from '@parcel/utils/config';
 import path from 'path';
+import browserslist from 'browserslist';
 
 const DEFAULT_ENGINES = {
   browsers: ['> 0.25%'],
@@ -21,11 +23,14 @@ export default class TargetResolver {
     // $FlowFixMe
     let pkg: PackageJSON = conf ? conf.config : {};
     let pkgTargets = pkg.targets || {};
-    let pkgEngines = pkg.engines || {};
-    let targets = [];
+    let pkgEngines = Object.assign({}, pkg.engines);
+    if (!pkgEngines.browsers) {
+      pkgEngines.browsers = browserslist.loadConfig({path: rootDir});
+    }
 
+    let targets = [];
     let node = pkgEngines.node;
-    let browsers = pkgEngines.browsers || pkg.browserslist;
+    let browsers = pkgEngines.browsers;
 
     // If there is a separate `browser` target, or an `engines.node` field but no browser targets, then
     // the `main` and `module` targets refer to node, otherwise browser.
@@ -39,7 +44,7 @@ export default class TargetResolver {
         name: 'main',
         distPath: pkg.main,
         env: Object.assign(
-          this.getEnvironment(pkg, mainContext),
+          this.getEnvironment(pkgEngines, mainContext),
           pkgTargets.main
         )
       });
@@ -50,7 +55,7 @@ export default class TargetResolver {
         name: 'module',
         distPath: pkg.module,
         env: Object.assign(
-          this.getEnvironment(pkg, mainContext),
+          this.getEnvironment(pkgEngines, mainContext),
           pkgTargets.module
         )
       });
@@ -67,7 +72,7 @@ export default class TargetResolver {
         name: 'browser',
         distPath: browser,
         env: Object.assign(
-          this.getEnvironment(pkg, 'browser'),
+          this.getEnvironment(pkgEngines, 'browser'),
           pkgTargets.browser
         )
       });
@@ -87,7 +92,7 @@ export default class TargetResolver {
         targets.push({
           name,
           distPath,
-          env: Object.assign(this.getEnvironment(pkg, context), env)
+          env: Object.assign(this.getEnvironment(pkgEngines, context), env)
         });
       }
     }
@@ -97,15 +102,17 @@ export default class TargetResolver {
       let context = browsers || !node ? 'browser' : 'node';
       targets.push({
         name: 'default',
-        env: this.getEnvironment(pkg, context)
+        env: this.getEnvironment(pkgEngines, context)
       });
     }
 
     return targets;
   }
 
-  getEnvironment(pkg: PackageJSON, context: EnvironmentContext): Environment {
-    let pkgEngines = pkg.engines || {};
+  getEnvironment(
+    pkgEngines: Engines,
+    context: EnvironmentContext
+  ): Environment {
     let env: Environment = {
       context,
       includeNodeModules: context === 'browser',
@@ -115,8 +122,7 @@ export default class TargetResolver {
     if (context === 'node') {
       env.engines.node = pkgEngines.node || DEFAULT_ENGINES.node;
     } else {
-      env.engines.browsers =
-        pkgEngines.browsers || pkg.browserslist || DEFAULT_ENGINES.browsers;
+      env.engines.browsers = pkgEngines.browsers || DEFAULT_ENGINES.browsers;
     }
 
     return env;
