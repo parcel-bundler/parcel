8322e477e638c5d6526d894bf98af074a68543c5
Abdullah <5623766+abdullahtariq1171@users.noreply.github.com>
2018-12-30 21:51:57 +0500

    fix chokidar ignored regex (#2479)

--- packages/core/watcher/src/Watcher.js
+++ packages/core/watcher/src/Watcher.js
@@ -19,7 +19,7 @@ class Watcher extends EventEmitter {
         process.platform === 'darwin' && process.env.NODE_ENV !== 'test',
       ignoreInitial: true,
       ignorePermissionErrors: true,
-      ignored: /\.cache|\.git/
+      ignored: /(^|[/\\])\.(git|cache)/
     }
   ) {
     super();
