8b4c513dbcbaafbf3c861b1c278f63b3940d5919
sainthkh <sainthkh@naver.com>
2019-01-29 16:41:53 +0900

    Skip symlink tests and show warning when you don't run tests with admin privilege in Windows.

--- packages/core/utils/src/errorUtils.js
+++ packages/core/utils/src/errorUtils.js
@@ -27,5 +27,14 @@ function jsonToError(json) {
   }
 }
 
+function symlinkPrivilegeWarning() {
+  console.error("-----------------------------------")
+  console.error("Skipping symbolic link test(s) because you don't have the privilege.");
+  console.error("Run tests with Administrator privilege.");
+  console.error("If you don't know how, check here: https://bit.ly/2UmWsbD");
+  console.error("-----------------------------------")
+}
+
 exports.errorToJson = errorToJson;
 exports.jsonToError = jsonToError;
+exports.symlinkPrivilegeWarning = symlinkPrivilegeWarning;
