83d9fd791782ce32f2226ca329977a35430697c7
Maia Teegarden <padmaia.thedev@gmail.com>
2019-02-22 11:43:34 -0800

    comment out problem code

--- packages/core/workers/src/Worker.js
+++ packages/core/workers/src/Worker.js
@@ -38,9 +38,10 @@ class Worker extends EventEmitter {
 
     this.child = childProcess.fork(childModule, process.argv, options);
 
-    // Unref the child and IPC channel so that the workers don't prevent the main process from exiting
-    this.child.unref();
-    this.child.channel.unref();
+    // TODO: This was causing odd behavior, need to fix
+    // // Unref the child and IPC channel so that the workers don't prevent the main process from exiting
+    // this.child.unref();
+    // this.child.channel.unref();
 
     this.child.on('message', data => this.receive(data));
 
