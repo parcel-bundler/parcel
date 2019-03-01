c2ef3238a6784315d5824f76bcc692cadf0d2192
tje3d <tje3d@yahoo.com>
2019-03-01 14:04:29 +0330

    Remove unnecessary return await

--- packages/core/parcel-bundler/src/Resolver.js
+++ packages/core/parcel-bundler/src/Resolver.js
@@ -251,7 +251,7 @@ class Resolver {
     }
 
     // Fall back to an index file inside the directory.
-    return await this.loadAsFile(path.join(dir, 'index'), extensions, pkg);
+    return this.loadAsFile(path.join(dir, 'index'), extensions, pkg);
   }
 
   async readPackage(dir) {
