2dea83312e59a8862dab4e366615bd410be5fda0
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2019-03-02 17:56:36 +0100

    Handle object destructuring a require call

--- packages/core/parcel-bundler/src/packagers/JSConcatPackager.js
+++ packages/core/parcel-bundler/src/packagers/JSConcatPackager.js
@@ -283,17 +283,34 @@ class JSConcatPackager extends Packager {
       // so that they can be referenced by other modules directly.
       if (t.isVariableDeclaration(node)) {
         for (let decl of node.declarations) {
-          decls.push(t.variableDeclarator(decl.id));
-          if (decl.init) {
-            body.push(
-              t.expressionStatement(
-                t.assignmentExpression(
-                  '=',
-                  t.identifier(decl.id.name),
-                  decl.init
+          if (t.isObjectPattern(decl.id)) {
+            for (let prop of decl.id.properties) {
+              decls.push(t.variableDeclarator(prop.value));
+              if (decl.init) {
+                body.push(
+                  t.expressionStatement(
+                    t.assignmentExpression(
+                      '=',
+                      t.identifier(prop.value.name),
+                      t.memberExpression(decl.init, prop.key)
+                    )
+                  )
+                );
+              }
+            }
+          } else {
+            decls.push(t.variableDeclarator(decl.id));
+            if (decl.init) {
+              body.push(
+                t.expressionStatement(
+                  t.assignmentExpression(
+                    '=',
+                    t.identifier(decl.id.name),
+                    decl.init
+                  )
                 )
-              )
-            );
+              );
+            }
           }
         }
       } else if (t.isFunctionDeclaration(node)) {
