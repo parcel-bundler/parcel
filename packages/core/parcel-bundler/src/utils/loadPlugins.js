c3921cb93368c3e0d4c24f2966931279e7fd1ae4
Will Binns-Smith <wbinnssmith@gmail.com>
2019-03-04 20:00:55 -0800

    eslint: enable no-return-await (#2707)
    This sets violations of the `no-return-await` to `error`. `return await` is redundant as values returned from an async function are effectively wrapped in a `Promise.resolve`, and awaiting another promise in addition to this will defer resolution another microtick.
    
    [eslint is good enough to detect the valid use case of `return await` in a `try`/`catch`](https://eslint.org/docs/rules/no-return-await). We also get clever with a short circuiting `||` in `Resolver` so I disabled it there, but we should probably rewrite it to be clearer in the future.
    
    Test Plan: `yarn && yarn lint && yarn test`

--- packages/core/parcel-bundler/src/utils/loadPlugins.js
+++ packages/core/parcel-bundler/src/utils/loadPlugins.js
@@ -3,13 +3,11 @@ const localRequire = require('./localRequire');
 module.exports = async function loadPlugins(plugins, relative) {
   if (Array.isArray(plugins)) {
     return Promise.all(
-      plugins.map(async p => loadPlugin(p, relative)).filter(Boolean)
+      plugins.map(p => loadPlugin(p, relative)).filter(Boolean)
     );
   } else if (typeof plugins === 'object') {
     let mapPlugins = await Promise.all(
-      Object.keys(plugins).map(
-        async p => await loadPlugin(p, relative, plugins[p])
-      )
+      Object.keys(plugins).map(p => loadPlugin(p, relative, plugins[p]))
     );
     return mapPlugins.filter(Boolean);
   } else {
