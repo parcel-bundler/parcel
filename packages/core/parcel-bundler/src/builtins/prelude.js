0c4320ae82660af39bc0e9b562f3dfa75c72835a
Tobias von Klipstein <tobias@klpstn.com>
2019-03-13 10:06:21 +0100

    Small change to restart CI

--- packages/core/parcel-bundler/src/builtins/prelude.js
+++ packages/core/parcel-bundler/src/builtins/prelude.js
@@ -8,7 +8,7 @@
 parcelRequire = (function (modules, cache, entry, globalName) {
   // Save the require from previous bundle to this closure if any
   var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
-  var isAmd = typeof define === 'function' && define.amd;
+  var isAmd = typeof define == 'function' && define.amd;
   var nodeRequire = !isAmd && typeof require === 'function' && require;
 
   function newRequire(name, jumped) {
