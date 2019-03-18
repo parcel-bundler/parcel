9d9009168e74f1bea15d968875e1a867894949a8
kale-io <cos+a@iuvo.ai>
2019-03-18 20:55:11 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- packages/core/integration-tests/test/integration/postcss-composes/index.js
+++ packages/core/integration-tests/test/integration/postcss-composes/index.js
@@ -1,6 +1,4 @@
 var map1 = require('./composes-1.css');
 var map2 = require('./composes-2.css');
 
-module.exports = function () {
-  return Object.assign({}, map1, map2);
-};
+module.exports = () => Object.assign({}, map1, map2);
