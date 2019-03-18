234ecf457c63630c963e8f0ba99936914a2ec752
kale-io <cos+a@iuvo.ai>
2019-03-18 21:03:48 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- packages/core/parcel-bundler/src/utils/urlJoin.js
+++ packages/core/parcel-bundler/src/utils/urlJoin.js
@@ -5,7 +5,7 @@ const path = require('path');
  * Joins a path onto a URL, and normalizes Windows paths
  * e.g. from \path\to\res.js to /path/to/res.js.
  */
-module.exports = function(publicURL, assetPath) {
+module.exports = (publicURL, assetPath) => {
   const url = URL.parse(publicURL, false, true);
   const assetUrl = URL.parse(assetPath);
   url.pathname = path.posix.join(url.pathname, assetUrl.pathname);
