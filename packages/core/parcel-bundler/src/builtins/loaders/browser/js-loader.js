fc75b9e4a0999edd2b76f5b72daaff9965e268c1
DomAmato <dominic.a.amato@gmail.com>
2019-03-18 17:23:01 -0500

    Revert "Convert function expression to an arrow function (per file)"
    This reverts commit 1f6fed9450759b71851bf134058d60f89bcbe643.

--- packages/core/parcel-bundler/src/builtins/loaders/browser/js-loader.js
+++ packages/core/parcel-bundler/src/builtins/loaders/browser/js-loader.js
@@ -1,16 +1,16 @@
 module.exports = function loadJSBundle(bundle) {
-  return new Promise((resolve, reject) => {
+  return new Promise(function (resolve, reject) {
     var script = document.createElement('script');
     script.async = true;
     script.type = 'text/javascript';
     script.charset = 'utf-8';
     script.src = bundle;
-    script.onerror = e => {
+    script.onerror = function (e) {
       script.onerror = script.onload = null;
       reject(e);
     };
 
-    script.onload = () => {
+    script.onload = function () {
       script.onerror = script.onload = null;
       resolve();
     };
