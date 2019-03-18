268083b76020765d78355db792022de61dc5324c
kale-io <cos+a@iuvo.ai>
2019-03-18 20:57:55 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- packages/core/integration-tests/test/integration/workers/worker-client.js
+++ packages/core/integration-tests/test/integration/workers/worker-client.js
@@ -2,12 +2,12 @@ const commonText = require('./common').commonFunction('Index');
 
 navigator.serviceWorker.register('service-worker.js', { scope: './' });
 
-exports.startWorker = function() {
+exports.startWorker = () => {
   const worker = new Worker('worker.js');
   worker.postMessage(commonText);
 };
 
-exports.startSharedWorker = function() {
+exports.startSharedWorker = () => {
   const worker = new SharedWorker('shared-worker.js');
 };
 
