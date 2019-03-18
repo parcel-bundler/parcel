2b9fb42fe9b89da51defbdf7eae7f715d21d09dc
kale-io <cos+a@iuvo.ai>
2019-03-18 21:27:57 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- packages/core/integration-tests/test/integration/process/index.js
+++ packages/core/integration-tests/test/integration/process/index.js
@@ -1,7 +1,5 @@
 process.browser = false
-module.exports = function () {
-  return process.browser && test(process.browser);
-};
+module.exports = () => process.browser && test(process.browser);
 
 function test(val) {
   return val;
