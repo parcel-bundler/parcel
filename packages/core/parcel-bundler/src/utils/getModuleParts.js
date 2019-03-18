09a924cae75e60329f2e0b7155e5f45b42ac564b
kale-io <cos+a@iuvo.ai>
2019-03-18 21:21:59 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- packages/core/parcel-bundler/src/utils/getModuleParts.js
+++ packages/core/parcel-bundler/src/utils/getModuleParts.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports = function(name) {
+module.exports = name => {
   let parts = path.normalize(name).split(path.sep);
   if (parts[0].charAt(0) === '@') {
     // Scoped module (e.g. @scope/module). Merge the first two parts back together.
