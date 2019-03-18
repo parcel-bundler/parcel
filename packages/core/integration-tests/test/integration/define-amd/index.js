e04bd9741716c4aa5ce2691c416addb8ccad9154
kale-io <cos+a@iuvo.ai>
2019-03-18 20:52:27 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- packages/core/integration-tests/test/integration/define-amd/index.js
+++ packages/core/integration-tests/test/integration/define-amd/index.js
@@ -1,7 +1,5 @@
 if (typeof define === 'function' && define.amd) {
-  define(function () {
-    return 4;
-  });
+  define(() => 4);
 } else if (typeof module === 'object') {
   module.exports = 2;
 }
