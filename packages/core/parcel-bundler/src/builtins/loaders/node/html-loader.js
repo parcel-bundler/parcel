e040c30c77c9ab6ba871611bef6e5da7a5e6c021
DomAmato <dominic.a.amato@gmail.com>
2019-03-18 17:23:11 -0500

    Revert "Convert function expression to an arrow function (per file)"
    This reverts commit d8866e4ac68555ad3c7eb6e5cf239b23761421b8.

--- packages/core/parcel-bundler/src/builtins/loaders/node/html-loader.js
+++ packages/core/parcel-bundler/src/builtins/loaders/node/html-loader.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
 
 module.exports = function loadHTMLBundle(bundle) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(__dirname + bundle, 'utf8', (err, data) => {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(__dirname + bundle, 'utf8', function(err, data) {
       if (err) {
         reject(err);
       } else {
         // wait for the next event loop iteration, so we are sure
         // the current module is fully loaded
-        setImmediate(() => {
+        setImmediate(function() {
           resolve(data);
         });
       }
