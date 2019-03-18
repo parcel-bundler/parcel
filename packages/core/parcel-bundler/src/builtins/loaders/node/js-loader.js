35bef2a222ea88d9b91cb580b66b510cc62cb5b7
kale-io <cos+a@iuvo.ai>
2019-03-18 21:05:58 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- packages/core/parcel-bundler/src/builtins/loaders/node/js-loader.js
+++ packages/core/parcel-bundler/src/builtins/loaders/node/js-loader.js
@@ -1,20 +1,20 @@
 var fs = require('fs');
 
 module.exports = function loadJSBundle(bundle) {
-  return new Promise(function(resolve, reject) {
-    fs.readFile(__dirname + bundle, 'utf8', function(err, data) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(__dirname + bundle, 'utf8', (err, data) => {
       if (err) {
         reject(err);
       } else {
         // wait for the next event loop iteration, so we are sure
         // the current module is fully loaded
-        setImmediate(function() {
+        setImmediate(() => {
           resolve(data);
         });
       }
     });
   })
-  .then(function(code) {
+  .then(code => {
     new Function('', code)();
   });
 };
