60b56c2e8ca55258ec15d4aff44c4601dce7ef1a
kale-io <cos+a@iuvo.ai>
2019-03-18 21:02:58 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- packages/core/utils/src/promisify.js
+++ packages/core/utils/src/promisify.js
@@ -1,13 +1,9 @@
-module.exports = function(fn) {
-  return function(...args) {
-    return new Promise(function(resolve, reject) {
-      fn(...args, function(err, ...res) {
-        if (err) return reject(err);
+module.exports = fn => (...args) => new Promise((resolve, reject) => {
+  fn(...args, (err, ...res) => {
+    if (err) return reject(err);
 
-        if (res.length === 1) return resolve(res[0]);
+    if (res.length === 1) return resolve(res[0]);
 
-        resolve(res);
-      });
-    });
-  };
-};
+    resolve(res);
+  });
+});
