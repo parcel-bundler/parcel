f7d4f2dd246004609a6144bc3f9f747dcbca129c
kale-io <cos+a@iuvo.ai>
2019-03-18 21:21:07 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- packages/core/parcel-bundler/src/utils/prettifyTime.js
+++ packages/core/parcel-bundler/src/utils/prettifyTime.js
@@ -1,3 +1,1 @@
-module.exports = function(time) {
-  return time < 1000 ? `${time}ms` : `${(time / 1000).toFixed(2)}s`;
-};
+module.exports = time => time < 1000 ? `${time}ms` : `${(time / 1000).toFixed(2)}s`;
