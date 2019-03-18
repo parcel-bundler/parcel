07b05cb1418b002c5d343debfb129115515a1fe0
kale-io <cos+a@iuvo.ai>
2019-03-18 21:19:36 +0000

    Convert function expression to an arrow function (per file) Converts a function expression to the new arrow function syntax. Performs the necessary safety checks. Applied over an entire file

--- packages/core/parcel-bundler/test/lineCounter.js
+++ packages/core/parcel-bundler/test/lineCounter.js
@@ -2,7 +2,7 @@ const fs = require('@parcel/fs');
 const assert = require('assert');
 const lineCounter = require('../src/utils/lineCounter');
 
-describe('line counter', async function() {
+describe('line counter', async () => {
   it('counts number of lines of a string', () => {
     const input = ` line 1
       line 2
@@ -11,7 +11,7 @@ describe('line counter', async function() {
     assert(lineCounter(input) === 3);
   });
 
-  it('counts number of lines of a file from disk', async function() {
+  it('counts number of lines of a file from disk', async () => {
     const input = (await fs.readFile('./test/lineCounter.js')).toString();
     assert(lineCounter(input) === 19);
   });
