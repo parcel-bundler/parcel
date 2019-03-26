67e80df4f0a193ad53fdd938ab49ef26f8661a41
Will Binns-Smith <wbinnssmith@gmail.com>
2019-03-26 10:15:33 -0700

    CLIReporter: Remove stdout proxy as ink now addresses this

--- packages/reporters/cli/src/CLIReporter.js
+++ packages/reporters/cli/src/CLIReporter.js
@@ -1,11 +1,10 @@
 // @flow strict-local
 
 import nullthrows from 'nullthrows';
-import {PassThrough} from 'stream';
 import {Reporter} from '@parcel/plugin';
 import {patchConsole} from '@parcel/logger';
 import React from 'react';
-import {render, type StdoutLike} from 'ink';
+import {render} from 'ink';
 import UI from './UI';
 
 // Misbehaved plugins or their dependencies can write to stdout, disrupting
@@ -13,20 +12,8 @@ import UI from './UI';
 // the main Parcel logger.
 patchConsole();
 
-// Ink expects stdout (or whatever is passed as stdout) to have a columns properly.
-// In environments like Lerna child processes, this property does not exist.
-// Create our own proxy object with `columns` on it that falls back to something sensible.
-
-// $FlowFixMe columns is added below
-const stdoutProxy: StdoutLike = new PassThrough();
-stdoutProxy.pipe(process.stdout);
-stdoutProxy.columns =
-  typeof process.stdout.columns === 'number' ? process.stdout.columns : 80;
-
 let ui: ?UI;
-render(<UI ref={u => (ui = u)} />, {
-  stdout: stdoutProxy
-});
+render(<UI ref={u => (ui = u)} />);
 
 export default new Reporter({
   report(event, options) {
