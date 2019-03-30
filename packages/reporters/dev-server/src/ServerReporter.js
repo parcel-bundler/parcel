deb46f347aa517492aaf53d4b5b2078172c2b5e7
Jasper De Moor <jasperdemoor@gmail.com>
2019-03-30 12:35:03 +0100

    minor cleanup[

--- packages/reporters/dev-server/src/ServerReporter.js
+++ packages/reporters/dev-server/src/ServerReporter.js
@@ -5,38 +5,38 @@ import Server from './Server';
 let server: Server | null = null;
 export default new Reporter({
   async report(event, options) {
-    if (options.serve) {
-      if (!server) {
-        if (options.serve === true) {
-          options.serve = {
-            host: '',
-            port: 1234,
-            https: false,
-            certificateDir: '.parcel-cert'
-          };
-        }
+    if (!options.serve) return;
 
-        let serverOptions = {
-          host: options.serve.host,
-          port: options.serve.port,
-          https: options.serve.https,
-          certificateDir: options.serve.certificateDir,
-          distDir: 'dist', //options.distDir, // ! Not sure how this works now
-          publicUrl: options.publicUrl || '/'
+    if (!server) {
+      if (options.serve === true) {
+        options.serve = {
+          host: '',
+          port: 1234,
+          https: false,
+          certificateDir: '.parcel-cert'
         };
-
-        // $FlowFixMe
-        server = new Server(serverOptions);
-        await server.start();
       }
 
-      if (event.type === 'buildSuccess') {
-        server.buildSuccess(event.bundleGraph);
-      }
+      let serverOptions = {
+        host: options.serve.host,
+        port: options.serve.port,
+        https: options.serve.https,
+        certificateDir: options.serve.certificateDir,
+        distDir: 'dist', //options.distDir, // ! Not sure how this works now
+        publicUrl: options.publicUrl || '/'
+      };
 
-      if (event.type === 'buildFailure') {
-        server.buildError(event.error);
-      }
+      // $FlowFixMe
+      server = new Server(serverOptions);
+      await server.start();
+    }
+
+    if (event.type === 'buildSuccess') {
+      server.buildSuccess(event.bundleGraph);
+    }
+
+    if (event.type === 'buildFailure') {
+      server.buildError(event.error);
     }
   }
 });
--- packages/reporters/hmr-server/src/HMRReporter.js
+++ packages/reporters/hmr-server/src/HMRReporter.js
@@ -5,34 +5,31 @@ import HMRServer from './HMRServer';
 let hmrServer: HMRServer | null = null;
 export default new Reporter({
   async report(event, options) {
-    if (options.hot) {
-      if (!hmrServer) {
-        hmrServer = new HMRServer();
+    if (!options.hot) return;
 
-        if (options.hot === true) {
-          options.hot = {
-            port: 0,
-            certificateDir: '.parcel-cert'
-          };
-        }
+    if (!hmrServer) {
+      hmrServer = new HMRServer();
 
-        await hmrServer.start(options.hot);
+      if (options.hot === true) {
+        options.hot = {
+          port: 0,
+          certificateDir: '.parcel-cert'
+        };
       }
 
-      if (
-        event.type === 'buildProgress' &&
-        event.phase === 'transformFinished'
-      ) {
-        hmrServer.addChangedAsset(event.cacheEntry);
-      }
+      await hmrServer.start(options.hot);
+    }
 
-      if (event.type === 'buildSuccess') {
-        hmrServer.emitUpdate();
-      }
+    if (event.type === 'buildProgress' && event.phase === 'transformFinished') {
+      hmrServer.addChangedAsset(event.cacheEntry);
+    }
 
-      if (event.type === 'buildFailure') {
-        hmrServer.emitError(event.error);
-      }
+    if (event.type === 'buildSuccess') {
+      hmrServer.emitUpdate();
+    }
+
+    if (event.type === 'buildFailure') {
+      hmrServer.emitError(event.error);
     }
   }
 });
