bb55112eb4d2f6c1c5ab6f069e7fa910e367d0cb
Jasper De Moor <jasperdemoor@gmail.com>
2019-03-31 14:26:31 +0200

    cleanup eslint configs

---
+++ packages/dev/eslint-config-browser/index.js
@@ -0,0 +1,49 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:flowtype/recommended',
+    'plugin:react/recommended'
+  ],
+  parser: 'babel-eslint',
+  plugins: ['flowtype', 'import', 'react'],
+  parserOptions: {
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module'
+  },
+  env: {
+    node: true,
+    es6: true
+  },
+  globals: {
+    parcelRequire: true,
+    define: true
+  },
+  // https://eslint.org/docs/user-guide/configuring#configuration-based-on-glob-patterns
+  overrides: [
+    {
+      files: ['**/test/**/*.js', '*.test.js'],
+      env: {
+        mocha: true
+      }
+    }
+  ],
+  rules: {
+    'flowtype/space-after-type-colon': 'off', // conflicts with prettier
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-extraneous-dependencies': 'error',
+    'import/no-self-import': 'error',
+    'no-return-await': 'error'
+  },
+  settings: {
+    flowtype: {
+      onlyFilesWithFlowAnnotation: true
+    },
+    react: {
+      version: 'detect'
+    }
+  }
+};
--- packages/examples/react-hmr/src/index.js
+++ packages/examples/react-hmr/src/index.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 
-// eslint-disable-next-line no-undef
 ReactDOM.render(<div>Hello world!</div>, document.getElementById('app'));
