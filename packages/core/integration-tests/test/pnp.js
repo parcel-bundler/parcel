5997d1bc2b9de4df54b30b1ef4d4e2872d36c9f0
Maël Nison <nison.mael@gmail.com>
2019-04-22 15:01:06 +0100

    Adds fixtures for pnp

---
+++ packages/core/integration-tests/test/pnp.js
@@ -0,0 +1,56 @@
+const assert = require('assert');
+const fs = require('@parcel/fs');
+const Module = require('module');
+const path = require('path');
+const {bundle, run, assertBundleTree} = require('@parcel/test-utils');
+
+describe('pnp', function() {
+  it('should defer to the pnp resolution when needed', async function() {
+    const calls = [];
+
+    const resolveToUnqualified = (request, issuer, opts) => {
+      calls.push([request, issuer, opts]);
+
+      if (request === `testmodule`) {
+        return path.join(__dirname, '/integration/pnp_require/pnp/testmodule');
+      } else {
+        throw new Error(`Shouldn't be called`);
+      }
+    };
+
+    const pnpapi = {resolveToUnqualified};
+
+    const origPnpVersion = process.versions.pnp;
+    process.versions.pnp = 42;
+
+    const origModuleLoad = Module._load;
+    Module._load = (name, ...args) =>
+      name === `pnpapi` ? pnpapi : origModuleLoad(name, ...args);
+
+    try {
+      let b = await bundle(
+        path.join(__dirname, '/integration/pnp_require/main.js')
+      );
+
+      await assertBundleTree(b, {
+        name: 'main.js',
+        assets: ['main.js', 'local.js', 'index.js']
+      });
+
+      assert.deepEqual(calls, [
+        [
+          'testmodule',
+          path.join(__dirname, '/integration/pnp_require/main.js'),
+          {considerBuiltins: false}
+        ]
+      ]);
+
+      let output = await run(b);
+      assert.equal(typeof output, 'function');
+      assert.equal(output(), 3);
+    } finally {
+      process.versions.pnp = origPnpVersion;
+      Module._load = origModuleLoad;
+    }
+  });
+});
