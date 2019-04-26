21baef216aac17136b94983abce44c957c49b32c
Will Binns-Smith <wbinnssmith@gmail.com>
2019-04-26 11:49:23 -0700

    Namer requires entry bundles first

--- packages/runtimes/js/src/JSRuntime.js
+++ packages/runtimes/js/src/JSRuntime.js
@@ -46,7 +46,16 @@ export default new Runtime({
         continue;
       }
 
-      let bundles = bundleGraph.getBundlesInBundleGroup(bundleGroup);
+      // Sort so the bundles containing the entry asset appear last
+      let bundles = bundleGraph.getBundlesInBundleGroup(bundleGroup).sort(
+        bundle =>
+          bundle
+            .getEntryAssets()
+            .map(asset => asset.id)
+            .includes(bundleGroup.entryAssetId)
+            ? 1
+            : -1
+      );
       let loaderModules = bundles.map(b => {
         let loader = loaders[b.type];
         if (!loader) {
