def2d49f633c1a5a2aefa7e7932efb2b9cb40063
Devon Govett <devongovett@gmail.com>
2019-05-06 19:27:21 -0700

    Update shake.js

--- packages/core/parcel-bundler/src/scope-hoisting/shake.js
+++ packages/core/parcel-bundler/src/scope-hoisting/shake.js
@@ -76,8 +76,7 @@ function isExportAssignment(path) {
     // match "path.any = any;"
     path.parentPath.isMemberExpression() &&
     path.parentPath.parentPath.isAssignmentExpression() &&
-    path.parentPath.parentPath.node.left === path.parentPath.node &&
-    path.parentPath.parentPath.get('right').isPure()
+    path.parentPath.parentPath.node.left === path.parentPath.node
   );
 }
 
