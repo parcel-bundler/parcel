a50ddd5ee18bbf2a70033fb118e6ec3a93a4ed97
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2019-05-08 06:22:43 +0200

    Replace module.require in scope hoisting (#2875)

--- packages/core/integration-tests/test/scope-hoisting.js
+++ packages/core/integration-tests/test/scope-hoisting.js
@@ -908,6 +908,7 @@ describe('scope hoisting', function() {
       let output = await run(b);
       assert.equal(output.id, b.entryAsset.id);
       assert.equal(output.hot, null);
+      assert.equal(output.moduleRequire, null);
       assert.equal(output.type, 'object');
       assert.deepEqual(output.exports, {});
       assert.equal(output.exportsType, 'object');
