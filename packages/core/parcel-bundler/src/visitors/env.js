b2877c2cb6784b505adcc3ec91ae4e6ae2a90654
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2019-05-12 22:20:05 +0200

    Cleanup

--- packages/core/parcel-bundler/src/visitors/env.js
+++ packages/core/parcel-bundler/src/visitors/env.js
@@ -1,13 +1,9 @@
 const types = require('@babel/types');
 
 module.exports = {
-  MemberExpression(path, asset) {
-    const {node} = path;
+  MemberExpression(node, asset) {
     // Inline environment variables accessed on process.env
-    if (
-      types.matchesPattern(node.object, 'process.env') &&
-      !types.isAssignmentExpression(path.parent)
-    ) {
+    if (types.matchesPattern(node.object, 'process.env')) {
       let key = types.toComputedKey(node);
       if (types.isStringLiteral(key)) {
         let prop = process.env[key.value];
@@ -16,24 +12,6 @@ module.exports = {
           morph(node, value);
           asset.isAstDirty = true;
           asset.cacheData.env[key.value] = process.env[key.value];
-          if (types.isUnaryExpression(path.parent, {operator: '!'})) {
-            path = path.parentPath;
-          }
-          if (
-            types.isConditionalExpression(path.parent) ||
-            types.isIfStatement(path.parent)
-          ) {
-            const result = path.evaluate();
-            if (result.confident) {
-              if (result.value && path.parent.consequent) {
-                path.parentPath.replaceWith(path.parent.consequent);
-              } else if (!result.value && path.parent.alternate) {
-                path.parentPath.replaceWith(path.parent.alternate);
-              } else {
-                path.parentPath.remove();
-              }
-            }
-          }
         }
       }
     }
--- packages/core/parcel-bundler/src/visitors/process.js
+++ packages/core/parcel-bundler/src/visitors/process.js
@@ -5,27 +5,12 @@ module.exports = {
     // Inline process.browser
     const isProcess = path.node.object.name === 'process';
     const isBrowser = path.node.property.name === 'browser';
+    const isAssignment = path.parentPath.type === 'AssignmentExpression';
     if (isProcess && isBrowser) {
-      if (t.isAssignmentExpression(path.parentPath)) {
+      if (isAssignment) {
         path.parentPath.remove();
       } else {
-        if (t.isIfStatement(path.parentPath)) {
-          path.parentPath.replaceWith(path.parent.consequent);
-        } else if (
-          t.isUnaryExpression(path.parentPath) &&
-          path.parent.operator === '!' &&
-          t.isIfStatement(path.parentPath.parentPath)
-        ) {
-          if (path.parentPath.parent.alternate) {
-            path.parentPath.parentPath.replaceWith(
-              path.parentPath.parent.alternate
-            );
-          } else {
-            path.parentPath.parentPath.remove();
-          }
-        } else {
-          path.replaceWith(t.booleanLiteral(true));
-        }
+        path.replaceWith(t.booleanLiteral(true));
       }
     }
   }
