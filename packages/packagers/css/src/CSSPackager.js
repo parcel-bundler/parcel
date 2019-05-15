e93e94ce4628f66bdae74f125834aa896c0c2214
Will Binns-Smith <wbinnssmith@atlassian.com>
2019-05-15 15:47:56 -0700

    Apply css styles in the correct  order

--- packages/packagers/css/src/CSSPackager.js
+++ packages/packagers/css/src/CSSPackager.js
@@ -5,8 +5,10 @@ import {Packager} from '@parcel/plugin';
 export default new Packager({
   async package(bundle) {
     let promises = [];
-    bundle.traverseAssets(asset => {
-      promises.push(asset.getCode());
+    bundle.traverseAssets({
+      exit: asset => {
+        promises.push(asset.getCode());
+      }
     });
     let outputs = await Promise.all(promises);
 
