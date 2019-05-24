702b80f1912054dd40e03f96047d4a53b2364a46
Will Binns-Smith <wbinnssmith@atlassian.com>
2019-05-23 23:00:55 -0700

    Add Babel typescript support

--- packages/transformers/babel/src/config.js
+++ packages/transformers/babel/src/config.js
@@ -7,6 +7,7 @@ import getFlowConfig from './flow';
 import path from 'path';
 import * as fs from '@parcel/fs';
 
+const TYPESCRIPT_EXTNAME_RE = /^\.tsx?/;
 const NODE_MODULES = `${path.sep}node_modules${path.sep}`;
 
 export default async function getBabelConfig(asset: MutableAsset) {
@@ -27,18 +28,49 @@ export default async function getBabelConfig(asset: MutableAsset) {
   let result = {};
   mergeConfigs(result, babelrc);
 
-  // Add a generated babel-preset-env config if it is not already specified in the babelrc
-  let hasEnv =
-    babelrc &&
-    hasPlugin(babelrc.config.presets, [
-      'env',
-      '@babel/env',
-      '@babel/preset-env'
-    ]);
+  // Typescript must use the plugin directly (not the typescript preset) and must
+  // come before preset env, otherwise proposals and nonstandard syntax is not
+  // transformed in time.
+  if (path.extname(asset.filePath).match(TYPESCRIPT_EXTNAME_RE)) {
+    let hasTypescript =
+      babelrc &&
+      (hasPlugin(babelrc.config.presets, [
+        '@babel/typescript',
+        '@babel/preset-typescript'
+      ]) ||
+        hasPlugin(babelrc.config.plugins, [
+          '@babel/transform-typescript',
+          '@babel/plugin-transform-typescript'
+        ]));
 
-  if (!hasEnv) {
-    let envConfig = await getEnvConfig(asset, isSource);
-    mergeConfigs(result, envConfig);
+    if (!hasTypescript) {
+      mergeConfigs(result, {
+        internal: true,
+        babelVersion: 7,
+        config: {
+          plugins: [
+            [
+              require('@babel/plugin-transform-typescript'),
+              {isTSX: path.extname(asset.filePath) === '.tsx'}
+            ]
+          ]
+        }
+      });
+    }
+  } else {
+    // Add Flow stripping config if it isn't already specified in the babelrc
+    let hasFlow =
+      babelrc &&
+      hasPlugin(babelrc.config.plugins, [
+        'transform-flow-strip-types',
+        '@babel/transform-flow-strip-types',
+        '@babel/plugin-transform-flow-strip-types'
+      ]);
+
+    if (!hasFlow) {
+      let flowConfig = await getFlowConfig(asset);
+      mergeConfigs(result, flowConfig);
+    }
   }
 
   // Add JSX config if it isn't already specified in the babelrc
@@ -60,18 +92,18 @@ export default async function getBabelConfig(asset: MutableAsset) {
     mergeConfigs(result, jsxConfig);
   }
 
-  // Add Flow stripping config if it isn't already specified in the babelrc
-  let hasFlow =
+  // Add a generated babel-preset-env config if it is not already specified in the babelrc
+  let hasEnv =
     babelrc &&
-    hasPlugin(babelrc.config.plugins, [
-      'transform-flow-strip-types',
-      '@babel/transform-flow-strip-types',
-      '@babel/plugin-transform-flow-strip-types'
+    hasPlugin(babelrc.config.presets, [
+      'env',
+      '@babel/env',
+      '@babel/preset-env'
     ]);
 
-  if (!hasFlow) {
-    let flowConfig = await getFlowConfig(asset);
-    mergeConfigs(result, flowConfig);
+  if (!hasEnv) {
+    let envConfig = await getEnvConfig(asset, isSource);
+    mergeConfigs(result, envConfig);
   }
 
   return result;
