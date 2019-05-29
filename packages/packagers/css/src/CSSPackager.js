f666b45072fc140fcf91878ae01ba342a9b6cb94
Kyle Welch <kwelch0626@gmail.com>
2019-05-28 23:08:02 -0700

    Updated packager to take media into account

--- packages/packagers/css/src/CSSPackager.js
+++ packages/packagers/css/src/CSSPackager.js
@@ -6,8 +6,24 @@ export default new Packager({
   async package({bundle}) {
     let promises = [];
     bundle.traverseAssets({
-      exit: asset => {
-        promises.push(asset.getCode());
+      exit: async asset => {
+        let parentDep;
+        bundle.traverseAncestors(asset, (node, _, {stop}) => {
+          if (node.type === 'dependency') {
+            parentDep = node.value;
+            stop();
+          }
+        });
+
+        promises.push(
+          asset.getCode().then((css: string) => {
+            if (parentDep && parentDep.meta && parentDep.meta.media) {
+              return `@media ${parentDep.meta.media} {\n${css}\n}\n`;
+            }
+
+            return css;
+          })
+        );
       }
     });
     let outputs = await Promise.all(promises);
