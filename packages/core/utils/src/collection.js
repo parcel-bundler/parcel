21b8887da693fd4422bffd11be9aa9a28da3c388
Will Binns-Smith <wbinnssmith@atlassian.com>
2019-05-31 20:00:15 -0700

    add objectSortedEntriesDeep and use it in md5

--- packages/core/utils/src/collection.js
+++ packages/core/utils/src/collection.js
@@ -5,9 +5,31 @@ export function unique<T>(array: Array<T>): Array<T> {
 }
 
 export function objectSortedEntries(obj: {
-  [string]: mixed
+  +[string]: mixed
 }): Array<[string, mixed]> {
   return Object.entries(obj).sort(([keyA], [keyB]) => keyA.localeCompare(keyB));
+}
+
+export function objectSortedEntriesDeep(object: {
+  +[string]: mixed
+}): Array<[string, mixed]> {
+  let sortedEntries = objectSortedEntries(object);
+  for (let i = 0; i < sortedEntries.length; i++) {
+    sortedEntries[i][1] = sortEntry(sortedEntries[i][1]);
+  }
+  return sortedEntries;
+}
+
+function sortEntry(entry: mixed) {
+  if (Array.isArray(entry)) {
+    return entry.map(sortEntry);
+  }
+
+  if (typeof entry === 'object' && entry != null) {
+    return objectSortedEntriesDeep(entry);
+  }
+
+  return entry;
 }
 
 // $FlowFixMe
--- packages/core/utils/test/collection.test.js
+++ packages/core/utils/test/collection.test.js
@@ -1,7 +1,11 @@
 // @flow
 
 import assert from 'assert';
-import {pick, objectSortedEntries} from '../src/collection';
+import {
+  pick,
+  objectSortedEntries,
+  objectSortedEntriesDeep
+} from '../src/collection';
 
 describe('pick', () => {
   it('returns a new object with a subset of key/value pairs', () =>
@@ -19,6 +23,19 @@ describe('objectSortedEntries', () => {
     assert.deepEqual(
       objectSortedEntries({foo: 'foo', baz: 'baz', bar: 'bar'}),
       [['bar', 'bar'], ['baz', 'baz'], ['foo', 'foo']]
+    );
+  });
+});
+
+describe('objectSortedEntriesDeep', () => {
+  it('returns a deeply sorted list of key/value tuples', () => {
+    assert.deepEqual(
+      objectSortedEntriesDeep({
+        foo: 'foo',
+        baz: ['d', 'c'],
+        bar: {g: 'g', b: 'b'}
+      }),
+      [['bar', [['b', 'b'], ['g', 'g']]], ['baz', ['d', 'c']], ['foo', 'foo']]
     );
   });
 });
