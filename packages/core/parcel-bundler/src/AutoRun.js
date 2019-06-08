af321428ebb29fd212cec42836304d194c0f27f0
Ayc0 <ayc0.benj@gmail.com>
2019-06-08 18:29:27 -0400

    use child_process.fork

--- packages/core/parcel-bundler/src/AutoRun.js
+++ packages/core/parcel-bundler/src/AutoRun.js
@@ -1,50 +1,24 @@
-const {spawn} = require('child_process');
+const {fork} = require('child_process');
 
 class AutoRun {
   constructor(bundler) {
     this.bundler = bundler;
-    this.prog = null;
-
-    this.selfStoppedPIDs = {};
 
     bundler.on('bundled', async bundle => {
-      await this.clearPrevious();
+      if (this.previousRun) {
+        this.previousRun.kill();
+      }
       this.run(bundle);
     });
 
     bundler.bundle();
   }
 
-  async clearPrevious() {
-    if (this.prog) {
-      this.prog.kill();
-      this.selfStoppedPIDs[this.prog.pid] = true;
-      await new Promise(resolve => {
-        const intervalID = setInterval(() => {
-          if (this.prog.killed) {
-            resolve(clearInterval(intervalID));
-          }
-        }, 50);
-      });
-      return;
-    }
-  }
-
   run(bundle) {
     if (!bundle.name) {
       return;
     }
-    this.prog = spawn('node', [bundle.name], {
-      stdio: [process.stdin, process.stdout, process.stderr],
-      detached: false
-    });
-    this.prog.on('close', code => {
-      if (this.selfStoppedPIDs[this.prog.pid]) {
-        delete this.selfStoppedPIDs[this.prog.pid];
-      } else {
-        process.exit(code);
-      }
-    });
+    this.previousRun = fork(bundle.name);
   }
 }
 
