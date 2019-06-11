29a234c951a9f56269c24e1d69a28c553fd4cb2e
Kyle Welch <kwelch0626@gmail.com>
2019-06-11 12:01:12 -0500

    minimize coffee transformer

--- packages/transformers/coffeescript/src/CoffeeScriptTransformer.js
+++ packages/transformers/coffeescript/src/CoffeeScriptTransformer.js
@@ -1,34 +1,11 @@
 import {Transformer} from '@parcel/plugin';
 import coffee from 'coffeescript';
-import semver from 'semver';
-import nullthrows from 'nullthrows';
 
 export default new Transformer({
-  canReuseAST({ast}) {
-    return (
-      ast.type === 'coffeescript' && semver.satisfies(ast.version, '^2.0.0')
-    );
-  },
-
-  async parse({asset}) {
-    return {
-      type: 'coffeescript',
-      version: '2.0.3',
-      program: coffee.nodes(await asset.getCode())
-    };
-  },
-
   async transform({asset}) {
     asset.type = 'js';
-
+    let output = coffee.compile(await asset.getCode());
+    asset.setCode(output);
     return [asset];
-  },
-
-  async generate({asset}) {
-    const ast = nullthrows(asset.ast);
-
-    return {
-      code: ast.program.compile()
-    };
   }
 });
