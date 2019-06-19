6a8b1f6204e77c7704ecee585a5e747536c429b4
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2019-06-19 22:08:34 +0200

    Fixup

--- packages/shared/scope-hoisting/src/prelude.js
+++ packages/shared/scope-hoisting/src/prelude.js
@@ -1,6 +1,19 @@
 var $parcel$modules = {};
 
-var parcelRequire = function(name) {
+/* eslint-disable no-undef */
+var globalObject =
+  typeof globalThis !== 'undefined'
+    ? globalThis
+    : typeof self !== 'undefined'
+    ? self
+    : typeof window !== 'undefined'
+    ? window
+    : typeof global !== 'undefined'
+    ? global
+    : {};
+/* eslint-enable no-undef */
+
+globalObject.parcelRequire = function(name) {
   if (name in $parcel$modules) {
     return $parcel$modules[name];
   }
@@ -15,6 +28,6 @@ var parcelRequire = function(name) {
   throw err;
 };
 
-parcelRequire.register = function register(id, exports) {
+globalObject.parcelRequire.register = function register(id, exports) {
   $parcel$modules[id] = exports;
 };
