429b82241adca2a8ab7c137349c26fa0e296d373
Devon Govett <devongovett@gmail.com>
2019-06-23 10:48:19 -0700

    Ensure we clear the media list of asset is imported without a media type

--- packages/packagers/css/src/CSSPackager.js
+++ packages/packagers/css/src/CSSPackager.js
@@ -7,11 +7,15 @@ export default new Packager({
     let promises = [];
     bundle.traverseAssets({
       exit: asset => {
+        // Figure out which media types this asset was imported with.
+        // We only want to import the asset once, so group them all together.
         let media = [];
         bundle.traverseAncestors(asset, (node, _, {skipChildren}) => {
           if (node.type === 'dependency') {
             let dep = node.value;
             if (!dep.meta || !dep.meta.media) {
+              // Asset was imported without a media type. Don't wrap in @media.
+              media.length = 0;
               skipChildren();
               return;
             }
@@ -30,8 +34,8 @@ export default new Packager({
         );
       }
     });
-    let outputs = await Promise.all(promises);
 
+    let outputs = await Promise.all(promises);
     return {contents: await outputs.map(output => output).join('\n')};
   }
 });
