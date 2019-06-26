18d9e9e7578622151de29ee6137bc111fcff6730
Jasper De Moor <jasperdemoor@gmail.com>
2019-06-26 20:26:09 +0200

    add a cache

--- packages/validators/ts-typechecker/src/typecheckValidator.js
+++ packages/validators/ts-typechecker/src/typecheckValidator.js
@@ -7,32 +7,34 @@ import localRequire from '@parcel/local-require';
 import formatDiagnostics from './formatDiagnostics';
 import LanguageServiceHost from './LanguageServiceHost';
 
+let langServiceCache = {};
+
 export default new Validator({
   async validate({asset, options}) {
     let ts = await localRequire('typescript', asset.filePath);
 
     let configNames = ['tsconfig.json'];
     let configPath = await resolveConfig(asset.filePath, configNames);
-    let tsconfig = (await asset.getConfig(configNames)) || {};
-    let baseDir = configPath ? path.dirname(configPath) : options.projectRoot;
 
-    let parsedCommandLine = ts.parseJsonConfigFileContent(
-      tsconfig,
-      ts.sys,
-      baseDir
+    if (!langServiceCache[configPath]) {
+      let tsconfig = (await asset.getConfig(configNames)) || {};
+      let baseDir = configPath ? path.dirname(configPath) : options.projectRoot;
+
+      let parsedCommandLine = ts.parseJsonConfigFileContent(
+        tsconfig,
+        ts.sys,
+        baseDir
+      );
+
+      langServiceCache[configPath] = ts.createLanguageService(
+        new LanguageServiceHost(parsedCommandLine, ts),
+        ts.createDocumentRegistry()
+      );
+    }
+
+    const diagnostics = langServiceCache[configPath].getSemanticDiagnostics(
+      asset.filePath
     );
-
-    let langService = ts.createLanguageService(
-      new LanguageServiceHost(parsedCommandLine, ts),
-      ts.createDocumentRegistry()
-    );
-
-    const diagnostics = [
-      ...langService.getSemanticDiagnostics(asset.filePath)
-      // We probably don't need this as it'll throw on transform...
-      // ...langService.getSyntacticDiagnostics(asset.filePath)
-    ];
-
     if (diagnostics.length > 0) {
       const formatted = formatDiagnostics(diagnostics, asset.filePath);
       throw formatted;
