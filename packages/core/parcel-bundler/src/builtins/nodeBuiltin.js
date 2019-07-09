8a91204c4cd5a7beaf705cdc551c493e642afe6b
Idan Levin <idan@ilt.co.il>
2019-07-09 18:35:03 +0300

    Update nodeBuiltin.js

--- packages/core/parcel-bundler/src/builtins/nodeBuiltin.js
+++ packages/core/parcel-bundler/src/builtins/nodeBuiltin.js
@@ -1,5 +1,11 @@
 exports.buffer = require.resolve('buffer/');
 exports.process = require.resolve('process/browser.js');
 exports.zlib = require.resolve('browserify-zlib');
+exports.stream = require.resolve('stream-browserify');
+exports._stream_duplex = require.resolve('readable-stream/duplex.js');
+exports._stream_passthrough	= require.resolve('readable-stream/passthrough.js');
+exports._stream_readable = require.resolve('readable-stream/readable.js');
+exports._stream_transform	= require.resolve('readable-stream/transform.js');
+exports._stream_writable = require.resolve('readable-stream/writable.js');
 exports.fs = null;
 
