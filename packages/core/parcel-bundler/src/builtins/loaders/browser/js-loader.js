a55e8aae10c0a017a2529114b34d69e8968c61e5
Trent Willis <twillis@netflix.com>
2019-07-11 20:55:57 -0700

    Support loading js bundles in web worker

--- packages/core/parcel-bundler/src/builtins/loaders/browser/js-loader.js
+++ packages/core/parcel-bundler/src/builtins/loaders/browser/js-loader.js
@@ -1,20 +1,33 @@
+function loadViaScript(bundle, resolve, reject) {
+  var script = document.createElement('script');
+  script.async = true;
+  script.type = 'text/javascript';
+  script.charset = 'utf-8';
+  script.src = bundle;
+  script.onerror = function (e) {
+    script.onerror = script.onload = null;
+    reject(e);
+  };
+
+  script.onload = function () {
+    script.onerror = script.onload = null;
+    resolve();
+  };
+
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+
 module.exports = function loadJSBundle(bundle) {
   return new Promise(function (resolve, reject) {
-    var script = document.createElement('script');
-    script.async = true;
-    script.type = 'text/javascript';
-    script.charset = 'utf-8';
-    script.src = bundle;
-    script.onerror = function (e) {
-      script.onerror = script.onload = null;
-      reject(e);
-    };
-
-    script.onload = function () {
-      script.onerror = script.onload = null;
-      resolve();
-    };
-
-    document.getElementsByTagName('head')[0].appendChild(script);
+    if (typeof document !== 'undefined') {
+      loadViaScript(bundle, resolve, reject);
+    } else {
+      try {
+        self.importScripts(bundle);
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    }
   });
 };
