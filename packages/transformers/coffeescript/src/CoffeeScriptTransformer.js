20d3be0d7a99ad091b355a12c7a1f5d52424f5e4
Kyle Welch <kwelch0626@gmail.com>
2019-07-29 12:26:36 -0500

    Add sourcemaps to CoffeeScript transformer

--- packages/transformers/coffeescript/src/CoffeeScriptTransformer.js
+++ packages/transformers/coffeescript/src/CoffeeScriptTransformer.js
@@ -1,11 +1,34 @@
+// @flow
 import {Transformer} from '@parcel/plugin';
+import SourceMap from '@parcel/source-map';
 import coffee from 'coffeescript';
+import {relativeUrl} from '@parcel/utils';
 
 export default new Transformer({
-  async transform({asset}) {
+  async transform({asset, options}) {
+    let sourceFileName: string = relativeUrl(
+      options.projectRoot,
+      asset.filePath
+    );
+
     asset.type = 'js';
-    let output = coffee.compile(await asset.getCode());
-    asset.setCode(output);
+    let output = coffee.compile(await asset.getCode(), {
+      filename: sourceFileName,
+      sourceMap: options.sourceMaps
+    });
+
+    // return from compile is based on sourceMaps option
+    if (options.sourceMaps) {
+      asset.setCode(output.js);
+      asset.setMap(
+        new SourceMap(output.sourceMap, {
+          [sourceFileName]: null
+        })
+      );
+    } else {
+      asset.setCode(output);
+    }
+
     return [asset];
   }
 });
