5cc18b3735aa61178b360850aae694c444b0f3d3
Will Binns-Smith <wbinnssmith@atlassian.com>
2019-08-08 12:05:49 -0700

    Don't run babel over ignore integration tests fixtures

--- packages/dev/babel-register/index.js
+++ packages/dev/babel-register/index.js
@@ -2,7 +2,13 @@ const parcelBabelPreset = require('@parcel/babel-preset');
 const path = require('path');
 
 require('@babel/register')({
-  ignore: [filepath => filepath.includes(path.sep + 'node_modules' + path.sep)],
+  ignore: [
+    filepath => filepath.includes(path.sep + 'node_modules' + path.sep),
+    // Don't run babel over ignore integration tests fixtures.
+    // These may include relative babel plugins, and running babel on those causes
+    // the plugin to be loaded to compile the plugin.
+    path.resolve(__dirname, '../../core/integration-tests/test/integration')
+  ],
   presets: [parcelBabelPreset]
 });
 
