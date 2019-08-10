511f142ffe9973dd6625e3fa33b14395d3a1a085
arianon <georgesvalera1997@gmail.com>
2019-08-10 19:40:33 -0400

    add support for asset linking with url()

--- packages/transformers/less/src/LessTransformer.js
+++ packages/transformers/less/src/LessTransformer.js
@@ -15,12 +15,33 @@ export default new Transformer({
     const less = await localRequire('less', asset.filePath);
     const code = await asset.getCode();
     const output = await less.render(code, {
-      ...config,
-      filename: asset.filePath
+      ...(config || {}),
+      filename: asset.filePath,
+      plugins: [...((config && config.plugins) || []), urlPlugin({asset})]
     });
 
     asset.type = 'css';
     asset.setCode(output.css);
+    asset.meta.hasDependencies = false;
     return [asset];
   }
 });
+
+function urlPlugin({asset}) {
+  return {
+    install(less, pluginManager) {
+      const visitor = new less.visitors.Visitor({
+        visitUrl(node) {
+          node.value.value = asset.addURLDependency(
+            node.value.value,
+            node.currentFileInfo.filename
+          );
+          return node;
+        }
+      });
+
+      visitor.run = visitor.visit;
+      pluginManager.addVisitor(visitor);
+    }
+  };
+}
