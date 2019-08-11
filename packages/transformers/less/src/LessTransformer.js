082883ac5472e7b7fed275f4c9bba5aea767ea78
arianon <georgesvalera1997@gmail.com>
2019-08-11 01:49:00 -0400

    move the config specific code to getConfig

--- packages/transformers/less/src/LessTransformer.js
+++ packages/transformers/less/src/LessTransformer.js
@@ -5,27 +5,32 @@ import localRequire from '@parcel/local-require';
 import {parseCSSImport} from '@parcel/utils';
 
 export default new Transformer({
-  async getConfig({asset}) {
-    return asset.getConfig(['.lessrc', '.lessrc.js'], {
+  async getConfig({asset, resolve}) {
+    let config = await asset.getConfig(['.lessrc', '.lessrc.js'], {
       packageKey: 'less'
     });
+
+    if (config === null) {
+      config = {};
+    }
+
+    config.filename = asset.filePath;
+    config.plugins = [
+      ...(config.plugins || []),
+      urlPlugin({asset}),
+      resolvePathPlugin({asset, resolve})
+    ];
+
+    return config;
   },
 
-  async transform({asset, config, resolve}) {
+  async transform({asset, config}) {
     const less = await localRequire('less', asset.filePath);
     const code = await asset.getCode();
-    const output = await less.render(code, {
-      ...(config || {}),
-      filename: asset.filePath,
-      plugins: [
-        ...((config && config.plugins) || []),
-        urlPlugin({asset}),
-        resolvePathPlugin({asset, resolve})
-      ]
-    });
+    const {css} = await less.render(code, config);
 
     asset.type = 'css';
-    asset.setCode(output.css);
+    asset.setCode(css);
     asset.meta.hasDependencies = false;
     return [asset];
   }
