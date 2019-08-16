174c6e124f2998b359b0aee72e7a2f47862a7c97
arianon <georgesvalera1997@gmail.com>
2019-08-16 13:20:09 -0700

    V2: Sass Transformer (#3353)
    * add sass transformer and make the tests pass
    
    * remove extensions from DefaultResolver
    
    * drop support for node-sass
    
    * only use parcel resolver for the advanced import paths
    
    * throw an error when using webpack import syntax
    
    * fix the assertion
    
    * support importing from node_modules and includePaths
    
    * update the error message
    
    * warn when node-sass is installed
    
    * add comment explaining how sass import resolution works
    
    * update
    
    * directly use resolve() function for the node-sass warning
    
    * Update package.json

---
+++ packages/transformers/sass/src/SassTransformer.js
@@ -0,0 +1,111 @@
+// @flow
+
+import {Transformer} from '@parcel/plugin';
+import {promisify, resolve} from '@parcel/utils';
+import logger from '@parcel/logger';
+import {dirname} from 'path';
+
+// E.g: ~library/file.sass
+const WEBPACK_ALIAS_RE = /^~[^/]/;
+
+let didWarnAboutNodeSass = false;
+
+async function warnAboutNodeSassBeingUnsupported(filePath) {
+  if (!didWarnAboutNodeSass) {
+    try {
+      await resolve('node-sass', {basedir: dirname(filePath)});
+      logger.warn(
+        '`node-sass` is unsupported in Parcel 2, it will use Dart Sass a.k.a. `sass`'
+      );
+    } catch (err) {
+      if (err.code !== 'MODULE_NOT_FOUND') {
+        throw err;
+      }
+    } finally {
+      didWarnAboutNodeSass = true;
+    }
+  }
+}
+
+export default new Transformer({
+  async getConfig({asset, resolve}) {
+    let config = await asset.getConfig(['.sassrc', '.sassrc.js'], {
+      packageKey: 'sass'
+    });
+
+    if (config === null) {
+      config = {};
+    }
+
+    config.file = asset.filePath;
+
+    if (config.importer === undefined) {
+      config.importer = [];
+    } else if (!Array.isArray(config.importer)) {
+      config.importer = [config.importer];
+    }
+
+    config.importer = [...config.importer, resolvePathImporter({resolve})];
+
+    return config;
+  },
+
+  async transform({asset, localRequire, config}) {
+    await warnAboutNodeSassBeingUnsupported(asset.filePath);
+    let sass = await localRequire('sass', asset.filePath);
+    const sassRender = promisify(sass.render.bind(sass));
+
+    let css;
+    try {
+      css = (await sassRender(config)).css;
+    } catch (err) {
+      // Adapt the Error object for the reporter.
+      err.fileName = err.file;
+      err.loc = {
+        line: err.line,
+        column: err.column
+      };
+
+      throw err;
+    }
+
+    asset.type = 'css';
+    asset.setCode(css);
+    return [asset];
+  }
+});
+
+function resolvePathImporter({resolve}) {
+  return function(rawUrl, prev, done) {
+    let url = rawUrl.replace(/^file:\/\//, '');
+
+    if (WEBPACK_ALIAS_RE.test(url)) {
+      const correctPath = url.replace(/^~/, '');
+      const error = new Error(
+        `The @import path "${url}" is using webpack specific syntax, which isn't supported by Parcel.\n\nTo @import files from node_modules, use "${correctPath}"`
+      );
+      done(error);
+      return;
+    }
+
+    resolve(prev, url)
+      .then(resolvedPath => {
+        done({file: resolvedPath});
+      })
+      .catch(() => {
+        /*
+         We return `null` instead of an error so that Sass' resolution algorithm can continue.
+
+         Imports are resolved by trying, in order:
+           * Loading a file relative to the file in which the `@import` appeared.
+           * Each custom importer.
+           * Loading a file relative to the current working directory.
+           * Each load path in `includePaths`
+           * Each load path specified in the `SASS_PATH` environment variable, which should be semicolon-separated on Windows and colon-separated elsewhere.
+
+         See: https://sass-lang.com/documentation/js-api#importer
+        */
+        done(null);
+      });
+  };
+}
