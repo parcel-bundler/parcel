bd5ac7c40016466b0bf542726576f5678f3cd173
Aparna Raj <araj@atlassian.com>
2019-08-16 16:34:48 +1000

    mdx test

--- packages/core/integration-tests/test/mdx.js
+++ packages/core/integration-tests/test/mdx.js
@@ -1,6 +1,12 @@
 const assert = require('assert');
 const path = require('path');
-const {bundle, assertBundleTree, outputFS} = require('@parcel/test-utils');
+const {
+  bundle,
+  assertBundles,
+  distDir,
+  outputFS,
+  run
+} = require('@parcel/test-utils');
 import {readFileSync} from 'fs';
 
 const configPath = path.join(__dirname, '/integration/mdx/.parcelrc');
@@ -18,7 +24,11 @@ describe('markdown/mdx', function() {
         config
       });
 
-      console.log(b);
+      let output = await run(b);
+      assert.equal(typeof output.default, 'function');
+
+      let js = await outputFS.readFile(path.join(distDir, 'index.js'), 'utf8');
+      assert(js.includes('This is a red heading'));
     });
   }
 });
