7b0750139b922d263b5db1ef7ed2e8a6591bd149
Taym Haddadi <bentaimia.haddadi@signify.com>
2019-08-23 14:06:26 +0200

    refactor: Add yaml transformer and make test pass (#3457)
    * refactor: Add toml transformer and make test pass
    
    * refactor: fix typo

--- packages/core/integration-tests/test/javascript.js
+++ packages/core/integration-tests/test/javascript.js
@@ -787,18 +787,20 @@ describe('javascript', function() {
     assert.equal(output(), 3);
   });
 
-  it.skip('should support requiring YAML files', async function() {
+  it('should support requiring YAML files', async function() {
     let b = await bundle(path.join(__dirname, '/integration/yaml/index.js'));
 
-    assertBundles(b, {
-      name: 'index.js',
-      assets: ['index.js', 'local.yaml'],
-      childBundles: [
-        {
-          type: 'map'
-        }
-      ]
-    });
+    assertBundles(b, [
+      {
+        name: 'index.js',
+        assets: ['index.js', 'local.yaml'],
+        childBundles: [
+          {
+            type: 'map'
+          }
+        ]
+      }
+    ]);
 
     let output = await run(b);
     assert.equal(typeof output, 'function');
