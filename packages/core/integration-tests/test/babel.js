30ab3a4ee4e7364c971a8f14dfd13d6f6afc17ad
Will Binns-Smith <wbinnssmith@atlassian.com>
2019-08-29 14:42:58 -0700

    Use useBuiltins: usage

--- packages/core/integration-tests/test/babel.js
+++ packages/core/integration-tests/test/babel.js
@@ -110,12 +110,12 @@ describe('babel', function() {
     assert(file.includes('function Bar'));
   });
 
-  // TODO: rework this test ?
-  it('should support splitting core-js using browserlist', async function() {
+  it('should only include necessary parts of core-js using browserlist', async function() {
     await bundle(path.join(__dirname, '/integration/babel-core-js/index.js'));
 
     let file = await outputFS.readFile(path.join(distDir, 'index.js'), 'utf8');
-    assert(file.includes('async function'));
+    assert(file.includes('async function Bar() {}'));
+    assert(file.includes('esnext.global-this'));
     assert(!file.includes('regenerator'));
   });
 
