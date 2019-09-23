67d341dce2d2f1c1c7e6280586fc8fc2f0d7a6e1
Quinn Blenkinsop <quinn@blenkinsop.net>
2019-09-22 17:47:22 -0700

    Support developing locally linked plugins

--- packages/core/plugin/src/PluginAPI.js
+++ packages/core/plugin/src/PluginAPI.js
@@ -12,7 +12,7 @@ import type {
   Validator as ValidatorOpts
 } from '@parcel/types';
 
-export const CONFIG = Symbol('config');
+export const CONFIG = Symbol.for('config');
 
 export class Transformer {
   constructor(opts: TransformerOpts) {
