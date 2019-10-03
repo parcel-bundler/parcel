cdeac9533a921a0c927d105002b3687ca21d328e
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2019-10-03 21:36:09 +0200

    Add plugin to use source as babel-register main field in the monorepo

---
+++ flow-resolver.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+// https://github.com/yarnpkg/yarn/blob/master/src/util/generate-pnp-map-api.tpl.js
+
+const fs = require('fs');
+const StringDecoder = require('string_decoder');
+const {
+  resolveSource
+} = require('./packages/dev/babel-register/babel-plugin-module-translate.js');
+
+function processRequest(data) {
+  try {
+    const [request, issuer] = JSON.parse(data);
+    try {
+      process.stdout.write(
+        `${JSON.stringify([null, resolveSource(request, issuer)])}\n`
+      );
+    } catch (error) {
+      process.stdout.write(
+        `${JSON.stringify([{message: error.message}, null])}\n`
+      );
+    }
+  } catch (error) {
+    process.stdout.write(
+      `${JSON.stringify([
+        {code: `INVALID_JSON`, message: error.message},
+        null
+      ])}\n`
+    );
+  }
+}
+
+let buffer = '';
+const decoder = new StringDecoder.StringDecoder();
+
+process.stdin.on('data', chunk => {
+  buffer += decoder.write(chunk);
+
+  // try {
+  do {
+    const index = buffer.indexOf('\n');
+    if (index === -1) {
+      break;
+    }
+
+    const line = buffer.slice(0, index);
+    buffer = buffer.slice(index + 1);
+
+    processRequest(line);
+  } while (true);
+  // } catch (e) {
+  //   fs.appendFileSync('/Users/niklas/Desktop/log.txt', e.message);
+  // }
+});
---
+++ packages/dev/babel-register/babel-plugin-module-translate.js
@@ -0,0 +1,58 @@
+const resolve = require('resolve');
+const path = require('path');
+
+function resolveSource(specifier, from) {
+  return resolve.sync(specifier, {
+    basedir: path.dirname(from),
+    packageFilter(pkg, x) {
+      if (pkg.name.startsWith('@parcel/') && pkg.name !== '@parcel/watcher') {
+        if (pkg.source) {
+          pkg.main = pkg.source;
+        }
+      }
+      return pkg;
+    }
+  });
+}
+
+let sourceFieldCache = new Map();
+function getSourceField(specifier, from) {
+  let key = `${specifier}:${from}`;
+  if (sourceFieldCache.has(key)) {
+    return sourceFieldCache.get(key);
+  }
+
+  let result = resolveSource(specifier, from);
+  sourceFieldCache.set(key, result);
+  return result;
+}
+
+module.exports = ({types: t}) => ({
+  name: 'module-translate',
+  visitor: {
+    ImportDeclaration({node}, state) {
+      let source = node.source;
+      if (t.isStringLiteral(source)) {
+        source.value = getSourceField(
+          source.value,
+          state.file.opts.filename || process.cwd()
+        );
+      }
+    },
+    CallExpression({node, scope}, state) {
+      if (
+        t.isIdentifier(node.callee, {name: 'require'}) &&
+        !scope.hasBinding(node.callee.value) &&
+        node.arguments.length === 1 &&
+        t.isStringLiteral(node.arguments[0])
+      ) {
+        node.arguments[0].value = getSourceField(
+          node.arguments[0].value,
+          state.file.opts.filename || process.cwd()
+        );
+      }
+    }
+  }
+});
+
+module.exports.resolveSource = resolveSource;
