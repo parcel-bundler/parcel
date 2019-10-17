a5c567b559b07d09da83e7aeb2f25f1d4e1aff95
Jasper De Moor <jasperdemoor@gmail.com>
2019-10-17 10:40:20 +0200

    create an error to diagnostic util

--- packages/core/diagnostic/src/diagnostic.js
+++ packages/core/diagnostic/src/diagnostic.js
@@ -39,6 +39,48 @@ export type Diagnostic = {|
   stack?: string
 |};
 
+// This type should represent all error formats Parcel can encounter...
+type PrintableError = Error & {
+  fileName: ?string,
+  codeFrame?: string,
+  highlightedCodeFrame?: string,
+  loc?: {
+    column: number,
+    line: number,
+    ...
+  },
+  source?: string,
+  ...
+};
+
+export function errorToDiagnostic(error: PrintableError): Diagnostic {
+  let codeframe: DiagnosticCodeFrame | void = undefined;
+
+  if (error.loc && error.source) {
+    codeframe = {
+      code: error.source,
+      codeHighlights: {
+        start: {
+          line: error.loc.line,
+          column: error.loc.column
+        },
+        end: {
+          line: error.loc.line,
+          column: error.loc.column
+        }
+      }
+    };
+  }
+
+  return {
+    origin: 'Error',
+    message: error.message,
+    filename: error.fileName,
+    stack: error.highlightedCodeFrame || error.codeFrame || error.stack,
+    codeframe
+  };
+}
+
 type ThrowableDiagnosticOpts = {
   diagnostic: Diagnostic,
   ...
