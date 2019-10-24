7129b39844edfa90a02b101b88710f0e202ddcc0
gonza <gonzalo.christobal@gmail.com>
2019-10-24 20:33:37 -0300

    Add htmlnano-optimizer and fix tests

---
+++ packages/optimizers/htmlnano/src/HTMLNanoOptimizer.js
@@ -0,0 +1,38 @@
+// @flow strict-local
+
+// $FlowFixMe this is untyped
+import htmlnano from 'htmlnano';
+import nullthrows from 'nullthrows';
+import {loadConfig} from '@parcel/utils';
+import {Optimizer} from '@parcel/plugin';
+import posthtml from 'posthtml';
+
+export default new Optimizer({
+  async optimize({contents, bundle, map, options}) {
+    if (!options.minify) {
+      return {contents, map};
+    }
+
+    if (typeof contents !== 'string') {
+      throw new Error(
+        'CSSNanoOptimizer: Only string contents are currently supported'
+      );
+    }
+
+    let userConfig = await loadConfig(
+      options.inputFS,
+      nullthrows(bundle.getMainEntry()).filePath,
+      ['.htmlnanorc', '.htmlnanorc.js']
+    );
+
+    const htmlNanoConfig = {
+      minifyJs: false,
+      ...userConfig?.config
+    };
+
+    return {
+      contents: (await posthtml([htmlnano(htmlNanoConfig)]).process(contents))
+        .html
+    };
+  }
+});
