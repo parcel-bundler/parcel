555c32cb8b77ed72a0a45efc4c2d3a7ffa43869b
Kyle Welch <kwelch0626@gmail.com>
2019-10-28 22:20:04 -0500

    Add reload config case

--- packages/transformers/posthtml/src/PostHTMLTransformer.js
+++ packages/transformers/posthtml/src/PostHTMLTransformer.js
@@ -9,6 +9,20 @@ import nullthrows from 'nullthrows';
 import semver from 'semver';
 import loadPlugins from './loadPlugins';
 
+const canSerializeConfig = config => {
+  if (!config || !config.plugins) {
+    return true;
+  }
+  if (Array.isArray(config.plugins)) {
+    return config.plugins.every(plugin => typeof plugin === 'string');
+  } else if (typeof config.plugins === 'object') {
+    return Object.keys(config.plugins).every(
+      plugin => typeof plugin === 'string'
+    );
+  }
+  return true;
+};
+
 export default new Transformer({
   async loadConfig({config, options}) {
     let configResult = await config.getConfig(
@@ -19,19 +33,25 @@ export default new Transformer({
     configResult = configResult || {};
     configResult.skipParse = true;
 
-    // if (canSerializeConfig(config)) {
-    //   config.shouldReload();
-    //   config.setResult({
-    //     internal: false
-    //   });
-    //   config.setResultHash(JSON.stringify(Date.now()));
-    // }
+    // isReload case
+    if (config.result != null) {
+      configResult.plugins = await loadPlugins(
+        configResult.plugins,
+        config.searchPath,
+        options
+      );
+      config.setResult(configResult);
+    } else if (!canSerializeConfig(configResult)) {
+      config.shouldReload();
+      config.setResult({isReload: true});
+      config.setResultHash(JSON.stringify(Date.now()));
+    } else {
+      if (configResult.plugins) {
+        config.shouldRehydrate();
+      }
 
-    if (configResult.plugins) {
-      config.shouldRehydrate();
+      config.setResult(configResult);
     }
-
-    config.setResult(configResult);
   },
 
   async rehydrateConfig({config, options}) {
