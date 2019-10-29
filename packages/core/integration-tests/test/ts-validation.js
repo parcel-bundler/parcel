a407cb7655aef28d5c60f7f65d8e7b2b0dc62be9
Jasper De Moor <jasperdemoor@gmail.com>
2019-10-29 17:04:21 +0100

    add tests for validators

---
+++ packages/core/integration-tests/test/ts-validation.js
@@ -0,0 +1,43 @@
+import assert from 'assert';
+import path from 'path';
+import {bundle} from '@parcel/test-utils';
+import defaultConfigContents from '@parcel/config-default';
+
+const config = {
+  ...defaultConfigContents,
+  validators: {
+    '*.{ts,tsx}': ['@parcel/validator-typescript']
+  },
+  reporters: ['@parcel/reporter-cli'],
+  filePath: require.resolve('@parcel/config-default')
+};
+
+describe.only('ts-validator', function() {
+  it('should throw validation error on typescript typing errors', async function() {
+    let didThrow = false;
+    let entry = path.join(
+      __dirname,
+      '/integration/ts-validation-error/index.ts'
+    );
+    try {
+      await bundle(entry, {
+        defaultConfig: config,
+        logLevel: 'info'
+      });
+    } catch (e) {
+      assert.equal(e.name, 'BuildError');
+      assert(!!Array.isArray(e.diagnostic));
+      assert(!!e.diagnostic[0].codeFrame);
+      assert.equal(e.diagnostic[0].origin, '@parcel/validator-typescript');
+      assert.equal(
+        e.diagnostic[0].message,
+        `Property 'world' does not exist on type 'Params'.`
+      );
+      assert.equal(e.diagnostic[0].filePath, entry);
+
+      didThrow = true;
+    }
+
+    assert(didThrow);
+  });
+});
