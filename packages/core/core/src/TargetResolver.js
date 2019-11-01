edba547c8f616725e094988c498d9137e0d22e2f
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2019-11-01 12:55:39 +0100

    WIP

--- packages/core/core/src/TargetResolver.js
+++ packages/core/core/src/TargetResolver.js
@@ -21,6 +21,7 @@ import {createEnvironment} from './Environment';
 import path from 'path';
 import fs from 'fs';
 import browserslist from 'browserslist';
+import levenshteinDistance from 'js-levenshtein';
 
 type TargetResolveResult = {|
   targets: Array<Target>,
@@ -129,48 +130,6 @@ function parseEngines(
   }
 }
 
-function levenshteinDistance(a: string, b: string) {
-  /*
-    Copyright (c) 2011 Andrei Mackenzie
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-  */
-  if (a.length == 0) return b.length;
-  if (b.length == 0) return a.length;
-
-  var matrix = [];
-
-  // increment along the first column of each row
-  var i;
-  for (i = 0; i <= b.length; i++) {
-    matrix[i] = [i];
-  }
-
-  // increment each column in the first row
-  var j;
-  for (j = 0; j <= a.length; j++) {
-    matrix[0][j] = j;
-  }
-
-  // Fill in the rest of the matrix
-  for (i = 1; i <= b.length; i++) {
-    for (j = 1; j <= a.length; j++) {
-      if (b.charAt(i - 1) == a.charAt(j - 1)) {
-        matrix[i][j] = matrix[i - 1][j - 1];
-      } else {
-        matrix[i][j] = Math.min(
-          matrix[i - 1][j - 1] + 1, // substitution
-          matrix[i][j - 1] + 1, // insertion
-          matrix[i - 1][j] + 1 // deletion
-        );
-      }
-    }
-  }
-
-  return matrix[b.length][a.length];
-}
-
 const DESCRIPTOR_SCHEMA = (JSON.parse(
   fs.readFileSync(path.join(__dirname, 'TargetDescriptor.schema.json'))
 ): SchemaEntity);
@@ -207,6 +166,8 @@ function parseDescriptor(
               exp,
               levenshteinDistance(exp, actualValue)
             ]): Array<[string, number]>).filter(
+              // TODO Instead use levenshtein automaton directly
+              // Remove if more than half of the string would need to be changed
               ([, d]) => d * 2 < actualValue.length
             );
             likely.sort(([, a], [, b]) => a - b);
