df1d5e30c93d11e9973e5b3942adce544fd2cff7
Will Binns-Smith <wbinnssmith@atlassian.com>
2019-11-05 14:28:19 -0800

    Remove blob-url from the default config

--- packages/core/integration-tests/test/javascript.js
+++ packages/core/integration-tests/test/javascript.js
@@ -1585,46 +1585,6 @@ describe('javascript', function() {
     await run(b);
   });
 
-  it('should inline compiled content as a blob url with `blob-url:*` imports', async () => {
-    await bundle(path.join(__dirname, '/integration/blob-url/index.js'));
-
-    let bundleContent = await outputFS.readFile(
-      path.join(distDir, 'index.js'),
-      'utf8'
-    );
-    assert(
-      bundleContent.includes(
-        'new Worker(URL.createObjectURL(new Blob(["// modules are defined as an array\\n'
-      )
-    );
-    assert(
-      bundleContent.includes(
-        'self.postMessage(\\"this should appear in the bundle\\\\n\\")'
-      )
-    );
-  });
-
-  it('should inline minified content as a blob url with `blob-url:*` imports', async () => {
-    await bundle(path.join(__dirname, '/integration/blob-url/index.js'), {
-      minify: true
-    });
-
-    let bundleContent = await outputFS.readFile(
-      path.join(distDir, 'index.js'),
-      'utf8'
-    );
-    assert(
-      bundleContent.includes(
-        "new Worker(URL.createObjectURL(new Blob(['!function("
-      )
-    );
-    assert(
-      bundleContent.includes(
-        'self.postMessage("this should appear in the bundle\\\\n")'
-      )
-    );
-  });
-
   it('should inline text content as url-encoded text and mime type with `data-url:*` imports', async () => {
     let b = await bundle(path.join(__dirname, '/integration/data-url/text.js'));
 
