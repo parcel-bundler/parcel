6a47cb584ec67297b181213639049b5760045ac7
Jasper De Moor <jasperdemoor@gmail.com>
2019-12-25 11:10:58 +0100

    Return raw html by default

--- packages/transformers/markdown/src/MarkedTransformer.js
+++ packages/transformers/markdown/src/MarkedTransformer.js
@@ -6,18 +6,7 @@ import hljs from 'highlight.js';
 import fm from 'front-matter';
 import Mustache from 'mustache';
 
-const DEFAULT_TEMPLATE = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>{{title}}</title>
-</head>
-<body>
-  {{{ body }}}
-</body>
-</html>`;
+const DEFAULT_TEMPLATE = '{{{ body }}}';
 
 const markedParse = promisify(marked.parse);
 
