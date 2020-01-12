2accf349be0c7cdd4ef21a382ab1c94cb24e87b4
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-01-12 12:38:34 +0100

    Add test

--- packages/core/integration-tests/test/scope-hoisting.js
+++ packages/core/integration-tests/test/scope-hoisting.js
@@ -483,6 +483,26 @@ describe('scope hoisting', function() {
       await subscription.unsubscribe();
     });
 
+    it('removes deferred reexports when imported from multiple asssets', async function() {
+      await bundle(
+        path.join(
+          __dirname,
+          '/integration/scope-hoisting/es6/side-effects-re-exports-multiple/a.js',
+        ),
+      );
+
+      let contents = await outputFS.readFile(
+        path.join(distDir, 'a.js'),
+        'utf8',
+      );
+
+      assert(!contents.includes('$import$'));
+
+      // can't run dynamic imports in node
+      // let output = await run(b);
+      // assert.deepEqual(output, [1234, 1234]);
+    });
+
     it('keeps side effects by default', async function() {
       let b = await bundle(
         path.join(
