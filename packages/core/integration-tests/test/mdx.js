90d6e31e3bf0a9a4c3c082e0c9be698900f1e894
Jasper De Moor <jasperdemoor@gmail.com>
2020-01-17 22:01:51 +0100

    Update mdx.js

--- packages/core/integration-tests/test/mdx.js
+++ packages/core/integration-tests/test/mdx.js
@@ -4,9 +4,7 @@ const {bundle, run} = require('@parcel/test-utils');
 
 describe('mdx', function() {
   it('should support bundling MDX', async function() {
-    let b = await bundle(path.join(__dirname, '/integration/mdx/index.mdx'), {
-      patchConsole: false,
-    });
+    let b = await bundle(path.join(__dirname, '/integration/mdx/index.mdx'));
 
     let output = await run(b);
     assert.equal(typeof output.default, 'function');
