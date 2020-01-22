5267d7678d5500b54e60ed137aefa8f0e408dd5c
Jasper De Moor <jasperdemoor@gmail.com>
2020-01-22 18:32:42 +0100

    ?

--- packages/core/workers/test/cpuCount.test.js
+++ packages/core/workers/test/cpuCount.test.js
@@ -4,7 +4,9 @@ import os from 'os';
 import getCores, {detectRealCores} from '../src/cpuCount';
 
 describe('cpuCount', function() {
-  it('Should be able to detect cpu count', () => {
+  it('Should be able to detect real cpu count', () => {
+    console.log(os.platform());
+
     // I have no clue how to detect cpu cores on windows
     if (os.platform() === 'windows') return;
 
@@ -12,7 +14,7 @@ describe('cpuCount', function() {
     assert(cores > 0);
   });
 
-  it('Should be able to detect cpu count', () => {
+  it('getCores should return more than 0', () => {
     let cores = getCores(true);
     assert(cores > 0);
   });
