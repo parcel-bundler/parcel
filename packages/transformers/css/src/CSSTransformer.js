95d135f894122988f1c5e8921738b2465d48c4ea
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-01-29 15:00:34 +0100

    Support protocol-relative URLs (for addURLDependency)

--- packages/transformers/css/src/CSSTransformer.js
+++ packages/transformers/css/src/CSSTransformer.js
@@ -3,14 +3,13 @@
 import type {FilePath} from '@parcel/types';
 
 import {Transformer} from '@parcel/plugin';
-import {createDependencyLocation} from '@parcel/utils';
+import {createDependencyLocation, isURL} from '@parcel/utils';
 import postcss from 'postcss';
 import valueParser from 'postcss-value-parser';
 import semver from 'semver';
 
 const URL_RE = /url\s*\("?(?![a-z]+:)/;
 const IMPORT_RE = /@import/;
-const PROTOCOL_RE = /^[a-z]+:/;
 
 function canHaveDependencies(filePath: FilePath, code: string) {
   return !/\.css$/.test(filePath) || IMPORT_RE.test(code) || URL_RE.test(code);
@@ -74,31 +73,35 @@ export default new Transformer({
         throw new Error('Could not find import name for ' + rule);
       }
 
-      if (PROTOCOL_RE.test(moduleSpecifier)) {
-        return;
+      if (isURL(moduleSpecifier)) {
+        name.value = asset.addURLDependency(moduleSpecifier);
+      } else {
+        // If this came from an inline <style> tag, don't inline the imported file. Replace with the correct URL instead.
+        // TODO: run CSSPackager on inline style tags.
+        // let inlineHTML =
+        //   this.options.rendition && this.options.rendition.inlineHTML;
+        // if (inlineHTML) {
+        //   name.value = asset.addURLDependency(dep, {loc: rule.source.start});
+        //   rule.params = params.toString();
+        // } else {
+        media = valueParser.stringify(media).trim();
+        let dep = {
+          moduleSpecifier,
+          // Offset by 8 as it does not include `@import `
+          loc: createDependencyLocation(
+            rule.source.start,
+            moduleSpecifier,
+            0,
+            8,
+          ),
+          meta: {
+            media,
+          },
+        };
+        asset.addDependency(dep);
+        rule.remove();
+        // }
       }
-
-      // If this came from an inline <style> tag, don't inline the imported file. Replace with the correct URL instead.
-      // TODO: run CSSPackager on inline style tags.
-      // let inlineHTML =
-      //   this.options.rendition && this.options.rendition.inlineHTML;
-      // if (inlineHTML) {
-      //   name.value = asset.addURLDependency(dep, {loc: rule.source.start});
-      //   rule.params = params.toString();
-      // } else {
-      media = valueParser.stringify(media).trim();
-      let dep = {
-        moduleSpecifier,
-        // Offset by 8 as it does not include `@import `
-        loc: createDependencyLocation(rule.source.start, moduleSpecifier, 0, 8),
-        meta: {
-          media,
-        },
-      };
-      asset.addDependency(dep);
-      rule.remove();
-      // }
-
       ast.isDirty = true;
     });
 
