9ac6d5170c221f45eb158d584dcd3e37b2c7a9c5
Jasper De Moor <jasperdemoor@gmail.com>
2020-02-08 19:05:02 +0100

    remove packager

--- packages/transformers/markdown-html/src/MarkedTransformer.js
+++ packages/transformers/markdown-html/src/MarkedTransformer.js
@@ -4,40 +4,46 @@ import {promisify} from '@parcel/utils';
 import marked from 'marked';
 import hljs from 'highlight.js';
 import fm from 'front-matter';
+import Mustache from 'mustache';
 
 const markedParse = promisify(marked.parse);
 
 export default new Transformer({
-  async transform({asset}) {
-    asset.type = 'md';
+  async transform({asset, resolve, options}) {
+    asset.type = 'html';
 
     let code = await asset.getCode();
     let {body, attributes} = fm(code);
 
-    for (let key in attributes) {
-      asset.meta[key] = attributes[key];
-    }
-
-    asset.setCode(
-      await markedParse(body, {
-        renderer: new marked.Renderer(),
-        highlight: (code: string, lang: string) => {
-          return hljs.highlight(lang, code).value;
-        },
-        pedantic: false,
-        gfm: true,
-        breaks: false,
-        sanitize: false,
-        smartLists: true,
-        smartypants: false,
-        xhtml: false,
-      }),
-    );
+    let parsedCode = await markedParse(body, {
+      renderer: new marked.Renderer(),
+      highlight: (code: string, lang: string) => {
+        return hljs.highlight(lang, code).value;
+      },
+      pedantic: false,
+      gfm: true,
+      breaks: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false,
+      xhtml: false,
+    });
 
     if (attributes.template) {
-      asset.addDependency({
-        moduleSpecifier: attributes.template,
+      let templateLocation = await resolve(asset.filePath, attributes.template);
+      let template = await options.inputFS.readFile(templateLocation, 'utf-8');
+      asset.addIncludedFile({
+        filePath: templateLocation,
       });
+
+      asset.setCode(
+        Mustache.render(template, {
+          body: parsedCode,
+          ...attributes,
+        }),
+      );
+    } else {
+      asset.setCode(parsedCode);
     }
 
     return [asset];
