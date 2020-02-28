8f23a01a7b823f6a7c39054899ed0dbc99236ef8
Jasper De Moor <jasperdemoor@gmail.com>
2020-02-28 17:58:40 +0100

    pls tests pass

--- packages/transformers/image/src/ImageResizeTransformer.js
+++ packages/transformers/image/src/ImageResizeTransformer.js
@@ -24,7 +24,7 @@ export default new Transformer({
       imagePipeline[format]();
     }
 
-    asset.setStream(imagePipeline);
+    asset.setBuffer(await imagePipeline.toBuffer());
 
     return [asset];
   },
