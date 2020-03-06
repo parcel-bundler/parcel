71a2277eb3fa2432a8e51ba1ca9d464dedf23066
fuyu <fuyu.fluffy@gmail.com>
2020-03-06 18:13:24 +0900

    Fix typos

--- packages/core/utils/test/escapeMarkdown.test.js
+++ packages/core/utils/test/escapeMarkdown.test.js
@@ -3,19 +3,27 @@ import assert from 'assert';
 import {escapeMarkdown} from '../src/';
 
 describe('escapeMarkdown', () => {
-  it('returns a escaped string 01', () => {
+  it('returns an escaped string 01', () => {
     assert.equal('\\*test\\*', escapeMarkdown('*test*'));
   });
 
-  it('returns a escaped string 02', () => {
+  it('returns an escaped string 02', () => {
     assert.equal('\\_test\\_', escapeMarkdown('_test_'));
   });
 
-  it('returns a escaped string 03', () => {
+  it('returns an escaped string 03', () => {
     assert.equal('\\~test\\~', escapeMarkdown('~test~'));
   });
 
-  it('returns a escaped string 04', () => {
+  it('returns an escaped string 04', () => {
     assert.equal('\\*\\_\\~test\\~\\_\\*', escapeMarkdown('*_~test~_*'));
+  });
+
+  it('returns an escaped string with backslash 01', () => {
+    assert.equal('\\\\test\\\\', escapeMarkdown('\\test\\'));
+  });
+
+  it('returns an escaped string with backslash 02', () => {
+    assert.equal('\\\\\\*test\\*\\\\', escapeMarkdown('\\*test*\\'));
   });
 });
