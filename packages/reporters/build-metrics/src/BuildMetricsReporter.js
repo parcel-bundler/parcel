70d73a5461542f354cf3fdcb4327b438ccde887c
Jasper De Moor <jasperdemoor@gmail.com>
2020-03-08 20:40:05 +0100

    add phase timings

--- packages/reporters/build-metrics/src/BuildMetricsReporter.js
+++ packages/reporters/build-metrics/src/BuildMetricsReporter.js
@@ -4,12 +4,25 @@ import path from 'path';
 import {Reporter} from '@parcel/plugin';
 import {generateBundleReport} from '@parcel/utils';
 
+let timings = {};
+let lastPhase;
+
 export default new Reporter({
   async report({event, options}) {
+    if (event.type === 'buildProgress') {
+      timings[event.phase] = Date.now();
+      if (lastPhase && lastPhase !== event.phase) {
+        timings[lastPhase] = Date.now() - timings[lastPhase];
+      }
+      lastPhase = event.phase;
+    }
+
     if (event.type === 'buildSuccess') {
+      timings[lastPhase] = Date.now() - timings[lastPhase];
       let metricsFilePath = path.join(process.cwd(), 'parcel-metrics.json');
 
       let metrics = {
+        phaseTimings: timings,
         buildTime: event.buildTime,
         bundles: event.bundleGraph
           ? generateBundleReport(event.bundleGraph).bundles
