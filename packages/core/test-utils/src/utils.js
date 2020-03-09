39b70ca1442e87d6ba47f9e82f24371573acec76
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-03-09 13:10:56 +0100

    Fix test?

--- packages/core/test-utils/src/utils.js
+++ packages/core/test-utils/src/utils.js
@@ -405,6 +405,9 @@ function prepareBrowserContext(
           },
         });
       },
+      atob(str) {
+        return Buffer.from(str, 'base64').toString('binary');
+      },
     },
     globals,
   );
