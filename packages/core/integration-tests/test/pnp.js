5b7b13ddf525455612826cd317b29bc8ed19a079
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2020-03-19 16:33:53 +0100

    Fix test

--- packages/core/integration-tests/test/pnp.js
+++ packages/core/integration-tests/test/pnp.js
@@ -5,21 +5,20 @@ import {bundle, run, assertBundles} from '@parcel/test-utils';
 
 describe('pnp', function() {
   it('should defer to the pnp resolution when needed', async function() {
-    let dir = path.join(__dirname, '/integration/pnp-require');
+    let dir = path.join(__dirname, 'integration/pnp-require');
 
     let origPnpVersion = process.versions.pnp;
     process.versions.pnp = 42;
 
     let origModuleResolveFilename = Module._resolveFilename;
+    Module.findPnpApi = () => require(path.join(dir, '.pnp.js'));
     Module._resolveFilename = (name, ...args) =>
       name === 'pnpapi'
         ? path.join(dir, '.pnp.js')
         : origModuleResolveFilename(name, ...args);
 
     try {
-      let b = await bundle(
-        path.join(__dirname, '/integration/pnp-require/index.js'),
-      );
+      let b = await bundle(path.join(dir, 'index.js'));
 
       await assertBundles(b, [
         {
