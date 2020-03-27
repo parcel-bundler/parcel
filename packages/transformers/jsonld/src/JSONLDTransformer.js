fc538fc5b1949d599eed995eb7929101890202e6
Isaac Adams <iadams@emoneyadvisor.com>
2020-03-27 19:11:12 -0400

    transformer is now finding and adding the images as dependencies

--- packages/transformers/jsonld/src/JSONLDTransformer.js
+++ packages/transformers/jsonld/src/JSONLDTransformer.js
@@ -17,30 +17,95 @@ const SCHEMA_ATTRS = [
   'thumbnailUrl',
   'video',
   'contentUrl',
+  'url'
 ];
 
 export default new Transformer({
-  async transform({ asset, options, resolve }) {
+  async transform({ asset, config, logger, options, resolve }) {
     /// allowing any recieved jsonld to be json5
     let rawCode = await asset.getCode();
     let jsonCode = json5.parse(rawCode);
+    console.log(jsonCode);
 
-    console.log(JSON.stringify({ asset, options, resolve }));
-    console.log(rawCode);
+    console.log(config);
+    console.log(options);
+    console.log(JSON.stringify({ asset, config, logger, options, resolve }));
 
-    Object
-      .keys(jsonCode)
-      .filter(k => SCHEMA_ATTRS.includes(k))
-      .map(k => {
-        console.log(`found key: ${k}`);
-        console.log(jsonCode[k]);
-      });
+    let parser = new JSONLDParser(asset);
+    let collectedUrls = parser.parse(jsonCode);
+    console.log(collectedUrls);
     
-    let output = JSON.stringify(JSON.stringify(jsonCode));
-    console.log(output);
+    let stringified = JSON.stringify(jsonCode);
+    let output = JSON.stringify(stringified);
 
-    asset.type = 'jsonld';
-    asset.setCode(output);
+    asset.type = 'js';
+    asset.setCode(`JSON.parse(${output})`);
     return [asset];
   },
 });
+
+class JSONLDParser {
+  asset;
+  publicURL;
+  urls;
+  constructor(asset){
+    this.asset = asset;
+    this.publicURL = "";
+    this.urls = [];
+  }
+
+  parse(jsonld) {
+    this.extractUrlsFrom(jsonld);
+    return this.urls;
+  }
+
+  extractUrlsFrom(data) {
+    if(!data) return;
+    
+    if (typeof data === 'string') {
+      this.transformString(data);
+      return;
+    }
+
+    if(Array.isArray(data)) {
+      this.iterateThroughArray(data);
+      return;
+    }
+
+    this.iterateThroughObject(data);
+  }
+
+  iterateThroughObject(jsonObject) {
+    Object
+    .keys(jsonObject)
+    .filter(k => SCHEMA_ATTRS.includes(k))
+    .forEach((k, i, arr) => {
+      let value = jsonObject[k];
+      console.log(`found key: ${k}`);      
+      console.log(value);
+      this.extractUrlsFrom(value);
+    });
+  }
+
+  iterateThroughArray(jsonArray) {
+    Object
+    .keys(jsonArray)
+    .forEach(i => {
+      let value = jsonArray[i];
+      console.log(value);
+      this.extractUrlsFrom(value);
+    });
+  }
+
+  transformString(value) {
+    let assetPath = this.asset.addURLDependency(value, {});
+    //let url = `${this.publicURL}/${value}`;
+    //this.urls.push(url);
+    /* let assetPath = this.asset.addURLDependency(currentValue);
+    if (!isURL(assetPath)) {
+      // paths aren't allowed, values must be urls
+      assetPath = urlJoin(this.publicURL, assetPath);
+    }
+    schema[schemaKey] = assetPath; */
+  }
+}
