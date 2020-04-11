a038ba256b8c2d7311ad9656d33d4fc1d14d0e2a
Jasper De Moor <jasperdemoor@gmail.com>
2020-04-11 13:14:36 +0200

    use sourcemaps to get a more accurate bundle-report

--- packages/core/utils/src/generateBundleReport.js
+++ packages/core/utils/src/generateBundleReport.js
@@ -1,51 +1,154 @@
-// @flow strict-local
+// @flow
 
-import type {Asset, BundleGraph} from '@parcel/types';
+import type {Asset, BundleGraph, Bundle, FilePath} from '@parcel/types';
+import type {FileSystem} from '@parcel/fs';
+import SourceMap from '@parcel/source-map';
 import nullthrows from 'nullthrows';
+import path from 'path';
+import {loadSourceMapUrl} from './';
 
-export type BundleReport = {|
-  bundles: Array<{|
-    filePath: string,
-    size: number,
-    time: number,
-    largestAssets: Array<{|
-      filePath: string,
-      size: number,
-      time: number,
-    |}>,
-    totalAssets: number,
-  |}>,
+export type AssetStats = {|
+  filePath: string,
+  size: number,
+  originalSize: number,
+  time: number,
 |};
 
-export default function generateBundleReport(
-  bundleGraph: BundleGraph,
-  largestAssetCount: number = 10,
-): BundleReport {
-  let bundles = [];
-  bundleGraph.traverseBundles(bundle => {
-    bundles.push(bundle);
-  });
-  bundles.sort((a, b) => b.stats.size - a.stats.size);
+export type BundleStats = {|
+  filePath: string,
+  size: number,
+  time: number,
+  assets: Array<AssetStats>,
+|};
 
-  return {
-    bundles: bundles.map(bundle => {
-      let assets: Array<Asset> = [];
-      bundle.traverseAssets(asset => {
-        assets.push(asset);
-      });
-      assets.sort((a, b) => b.stats.size - a.stats.size);
+export type BundleReport = {|
+  bundles: Array<BundleStats>,
+|};
+
+async function getSourcemapSizes(
+  filePath: FilePath,
+  fs: FileSystem,
+  projectRoot: FilePath,
+) {
+  let bundleContents = await fs.readFile(filePath, 'utf-8');
+  let mapUrlData = await loadSourceMapUrl(fs, filePath, bundleContents);
+  if (!mapUrlData) {
+    return null;
+  }
+
+  let rawMap = mapUrlData.map;
+  let sourceMap = new SourceMap();
+  sourceMap.addRawMappings(rawMap.mappings, rawMap.sources, rawMap.names);
+  let parsedMapData = sourceMap.getMap();
+
+  if (parsedMapData.mappings.length > 2) {
+    let sources = parsedMapData.sources.map(s => path.join(projectRoot, s));
+    let currLine = 1;
+    let currColumn = 0;
+    let currMappingIndex = 0;
+    let currMapping = parsedMapData.mappings[currMappingIndex];
+    let nextMapping = parsedMapData.mappings[currMappingIndex + 1];
+    let sourceContents = new Array(sources.length).fill('');
+    for (let i = 0; i < bundleContents.length; i++) {
+      // Update currMapping to be the next mapping with a source
+      while (
+        !currMapping.source ||
+        currMapping.source < 0 ||
+        nextMapping.generated.column <= currColumn
+      ) {
+        currMappingIndex++;
+        if (currMappingIndex > parsedMapData.mappings.length - 2) {
+          break;
+        }
+        currMapping = parsedMapData.mappings[currMappingIndex];
+        nextMapping = parsedMapData.mappings[currMappingIndex + 1];
+      }
+
+      let c = bundleContents[i];
+      if (
+        currMapping.generated.line === currLine &&
+        currMapping.generated.column <= currColumn &&
+        (nextMapping.generated.line > currLine ||
+          (nextMapping.generated.line === currLine &&
+            nextMapping.generated.column > currColumn))
+      ) {
+        sourceContents[currMapping.source] += c;
+      }
+
+      if (c === '\n') {
+        currColumn = 0;
+        currLine++;
+      } else {
+        currColumn++;
+      }
+    }
+
+    return sourceContents.map((content, i) => {
+      return {
+        filePath: sources[i],
+        size: Buffer.byteLength(content, 'utf8'),
+      };
+    });
+  }
+}
+
+async function createBundleStats(
+  bundle: Bundle,
+  fs: FileSystem,
+  projectRoot: FilePath,
+) {
+  let filePath = nullthrows(bundle.filePath);
+  let sourcemapSizes = await getSourcemapSizes(filePath, fs, projectRoot);
+
+  let assets: Array<Asset> = [];
+  bundle.traverseAssets(asset => {
+    assets.push(asset);
+  });
+
+  let index = {};
+  let assetsReport: Array<AssetStats> = assets
+    .filter(a => {
+      if (!index[a.filePath]) {
+        index[a.filePath] = true;
+        return true;
+      } else {
+        return false;
+      }
+    })
+    .map(asset => {
+      let foundSize =
+        sourcemapSizes &&
+        sourcemapSizes.find(s => s.filePath === asset.filePath);
 
       return {
-        filePath: nullthrows(bundle.filePath),
-        size: bundle.stats.size,
-        time: bundle.stats.time,
-        largestAssets: assets.slice(0, largestAssetCount).map(asset => ({
-          filePath: asset.filePath,
-          size: asset.stats.size,
-          time: asset.stats.time,
-        })),
-        totalAssets: assets.length,
+        filePath: asset.filePath,
+        size: foundSize ? foundSize.size : asset.stats.size,
+        originalSize: asset.stats.size,
+        time: asset.stats.time,
       };
-    }),
+    });
+
+  return {
+    filePath: nullthrows(bundle.filePath),
+    size: bundle.stats.size,
+    time: bundle.stats.time,
+    assets: assetsReport.sort((a, b) => b.size - a.size),
+  };
+}
+
+export default async function generateBundleReport(
+  bundleGraph: BundleGraph,
+  fs: FileSystem,
+  projectRoot: FilePath,
+): Promise<BundleReport> {
+  let bundles = bundleGraph
+    .getBundles()
+    .sort((a, b) => b.stats.size - a.stats.size)
+    .filter(b => !!b.filePath);
+
+  return {
+    bundles: (
+      await Promise.all(bundles.map(b => createBundleStats(b, fs, projectRoot)))
+    ).filter(e => !!e),
   };
 }
--- packages/reporters/cli/src/bundleReport.js
+++ packages/reporters/cli/src/bundleReport.js
@@ -1,5 +1,6 @@
 // @flow
-import type {BundleGraph} from '@parcel/types';
+import type {BundleGraph, FilePath} from '@parcel/types';
+import type {FileSystem} from '@parcel/fs';
 
 import {generateBundleReport, prettifyTime} from '@parcel/utils';
 import filesize from 'filesize';
@@ -16,9 +17,14 @@ const COLUMNS = [
   {align: 'right'}, // time
 ];
 
-export default function bundleReport(bundleGraph: BundleGraph) {
+export default async function bundleReport(
+  bundleGraph: BundleGraph,
+  fs: FileSystem,
+  projectRoot: FilePath,
+) {
   // Get a list of bundles sorted by size
-  let {bundles} = generateBundleReport(bundleGraph);
+  // $FlowFixMe not entirely sure why this fails?
+  let {bundles} = await generateBundleReport(bundleGraph, fs, projectRoot);
   let rows = [];
 
   for (let bundle of bundles) {
@@ -29,22 +35,22 @@ export default function bundleReport(bundleGraph: BundleGraph) {
       chalk.green.bold(prettifyTime(bundle.time)),
     ]);
 
-    for (let asset of bundle.largestAssets) {
+    let largestAssets = bundle.assets.slice(0, 10);
+    for (let asset of largestAssets) {
       // Add a row for the asset.
       rows.push([
-        (asset == bundle.largestAssets[bundle.largestAssets.length - 1]
-          ? '└── '
-          : '├── ') + formatFilename(asset.filePath, chalk.reset),
+        (asset == largestAssets[largestAssets.length - 1] ? '└── ' : '├── ') +
+          formatFilename(asset.filePath, chalk.reset),
         chalk.dim(prettifySize(asset.size)),
         chalk.dim(chalk.green(prettifyTime(asset.time))),
       ]);
     }
 
-    if (bundle.totalAssets > bundle.largestAssets.length) {
+    if (bundle.totalAssets > largestAssets.length) {
       rows.push([
         '└── ' +
           chalk.dim(
-            `+ ${bundle.totalAssets - bundle.largestAssets.length} more assets`,
+            `+ ${bundle.totalAssets - largestAssets.length} more assets`,
           ),
       ]);
     }
