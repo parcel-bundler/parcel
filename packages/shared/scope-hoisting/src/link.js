50cc528a3acc9d1fb75e72b97ed818ea212fcc29
Will Binns-Smith <wbinnssmith@atlassian.com>
2020-04-14 11:14:03 -0700

    Fix requiring a nonentry asset from a sibling (#4470)
    * Add test for requiring a nonentry asset in a sibling whose entry was a subgraph
    
    * Implement requiring a nonentry asset in a sibling whose entry was a subgraph

--- packages/shared/scope-hoisting/src/link.js
+++ packages/shared/scope-hoisting/src/link.js
@@ -73,7 +73,7 @@ export function link({
   bundleGraph: BundleGraph,
   ast: File,
   options: PluginOptions,
-|}) {
+|}): {|ast: File, referencedAssets: Set<Asset>|} {
   let format = OutputFormats[bundle.env.outputFormat];
   let replacements: Map<Symbol, Symbol> = new Map();
   let imports: Map<Symbol, ?[Asset, Symbol]> = new Map();
@@ -700,5 +700,5 @@ export function link({
     },
   });
 
-  return ast;
+  return {ast, referencedAssets};
 }
