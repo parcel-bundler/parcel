7f508f52750e83eba91e1f8647dbf6b94031789f
isaacadams <isaac.d.adams@gmail.com>
2020-04-16 10:57:35 -0400

    :white_check_mark: removing redudant test

--- packages/core/integration-tests/test/schema-jsonld.js
+++ packages/core/integration-tests/test/schema-jsonld.js
@@ -1,25 +1,15 @@
-import {bundle, assertBundles, distDir, outputFS} from '@parcel/test-utils';
-import assert from 'assert';
+import {bundle, assertBundles} from '@parcel/test-utils';
 import path from 'path';
-
-function getPathToFile(relativePathToFile) {
-  return path.join(__dirname, relativePathToFile);
-}
-
-let pathToIndex = getPathToFile('/integration/schema-jsonld/index.html');
-
-async function getBundleFile() {
-  await bundle(pathToIndex);
-  let pathToOutputFile = path.join(distDir, 'index.html');
-  return outputFS.readFile(pathToOutputFile, 'utf-8');
-}
 
 describe('jsonld', function() {
   it('Should parse a LD+JSON schema and collect dependencies', async function() {
-    let b = await bundle(pathToIndex, {
-      production: true,
-      publicURL: 'https://place.holder/',
-    });
+    let b = await bundle(
+      path.join(__dirname, '/integration/schema-jsonld/index.html'),
+      {
+        production: true,
+        publicURL: 'https://place.holder/',
+      },
+    );
 
     assertBundles(b, [
       {
@@ -44,45 +34,4 @@ describe('jsonld', function() {
       },
     ]);
   });
-
-  it('Should output the original json back into the index.html file inside the script tag', async function() {
-    /* let v1JSONLDOutputInsideScriptTag = {
-      '@context': 'http://schema.org',
-      '@type': 'LocalBusiness',
-      description: 'This is your business description.',
-      name: "Parcel's parcel",
-      telephone: '555-111-2345',
-      openingHours: 'Mo,Tu,We,Th,Fr 09:00-17:00',
-      logo: {
-        '@type': 'ImageObject',
-        url: 'images/logo.png',
-        width: 180,
-        height: 120,
-      },
-      image: ['image\\.[a-f0-9]+/\\.jpeg', 'images/image.jpeg'],
-    }; */
-
-    let file = await getBundleFile();
-    let regex = new RegExp(
-      /<\s*script \s*type="application\/ld\+json"\s*>.*<\/\s*script\s*>/gm,
-    );
-    let firstMatch = regex.exec(file).map(m => {
-      // removes script html tags to extract the json object in between them
-      return m.replace(/<\/?\s*script( \w+=".*"\s*)*\s*>/g, '');
-    })[0];
-
-    let actual = JSON.parse(firstMatch);
-    match(actual.logo.url, /logo\.[a-f0-9]+\.png/);
-    match(actual.image[0], /image\.[a-f0-9]+\.jpeg/);
-    match(actual.image[1], /image\.[a-f0-9]+\.jpeg/);
-  });
 });
-
-function match(test, pattern) {
-  let success = new RegExp(pattern).test(test);
-  if (success) {
-    assert.ok(`'${test}' matched the given pattern of '${pattern}'`);
-    return;
-  }
-  assert.fail(`'${test}' did not match the given pattern of '${pattern}'`);
-}
