3e6f25904390c5a1ebda60e69c135c5f6be47495
isaacadams <isaac.d.adams@gmail.com>
2020-04-16 11:43:17 -0400

    :white_check_mark: adding test back in that puts expectations on the image assets generated links + that the content inserted back inbetween the script tag is a valid JSON object (partial credit to @mischnic for simplifying the regex expression with group capturing)

--- packages/core/integration-tests/test/schema-jsonld.js
+++ packages/core/integration-tests/test/schema-jsonld.js
@@ -1,5 +1,6 @@
-import {bundle, assertBundles} from '@parcel/test-utils';
+import {bundle, assertBundles, distDir, outputFS} from '@parcel/test-utils';
 import path from 'path';
+import assert from 'assert';
 
 describe('jsonld', function() {
   it('Should parse a LD+JSON schema and collect dependencies', async function() {
@@ -21,7 +22,7 @@ describe('jsonld', function() {
         assets: ['other.css'],
       },
       {
-        type: 'jsonld', //this is the jsonld asset
+        type: 'jsonld',
         assets: ['index.html'],
       },
       {
@@ -33,5 +34,39 @@ describe('jsonld', function() {
         assets: ['image.jpeg'],
       },
     ]);
+
+    let file = await outputFS.readFile(
+      path.join(distDir, 'index.html'),
+      'utf-8',
+    );
+    let contentBetweenScriptTag = new RegExp(
+      /<\s*script \s*type="application\/ld\+json"\s*>(.*)<\/\s*script\s*>/gm,
+    ).exec(file)[1];
+
+    let jsonldData = assertValidJsonObject(contentBetweenScriptTag);
+    match(jsonldData.logo.url, /logo\.[a-f0-9]+\.png/);
+    match(jsonldData.image[0], /image\.[a-f0-9]+\.jpeg/);
+    match(jsonldData.image[1], /image\.[a-f0-9]+\.jpeg/);
   });
 });
+
+function match(test, pattern) {
+  let success = new RegExp(pattern).test(test);
+  if (success) {
+    assert.ok(`'${test}' matched the given pattern of '${pattern}'`);
+    return;
+  }
+  assert.fail(`'${test}' did not match the given pattern of '${pattern}'`);
+}
+
+function assertValidJsonObject(dataAsString) {
+  try {
+    let data = JSON.parse(dataAsString);
+    assert.ok('input string is a valid json object');
+    return data;
+  } catch (e) {
+    assert.fail(
+      `the given string (see below) is not a valid json object\n\terror :: ${e}\n\tinput :: ${dataAsString}`,
+    );
+  }
+}
