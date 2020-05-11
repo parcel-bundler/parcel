d9a33416e4b057d1835d039955d7bbf259d938c7
Nils Helmig <nils.helmig@web.de>
2020-05-11 22:19:50 +0200

    Integration tests for elm transformer

--- packages/core/integration-tests/test/elm.js
+++ packages/core/integration-tests/test/elm.js
@@ -1,59 +1,73 @@
 import assert from 'assert';
-import {bundle, assertBundleTree, run, outputFS} from '@parcel/test-utils';
+import path from 'path';
+import {
+  bundle,
+  distDir,
+  assertBundles,
+  run,
+  outputFS,
+} from '@parcel/test-utils';
 
-describe.skip('elm', function() {
+describe('elm', function() {
   it('should produce a basic Elm bundle', async function() {
-    let b = await bundle(__dirname + '/integration/elm/index.js');
+    let b = await bundle(path.join(__dirname, '/integration/elm/index.js'));
 
-    await assertBundleTree(b, {
-      type: 'js',
-      assets: ['Main.elm', 'index.js'],
-    });
+    await assertBundles(b, [
+      {
+        type: 'js',
+        assets: ['Main.elm', 'index.js'],
+      },
+    ]);
 
     let output = await run(b);
     assert.equal(typeof output().Elm.Main.init, 'function');
   });
   it('should produce a elm bundle with debugger', async function() {
-    let b = await bundle(__dirname + '/integration/elm/index.js');
+    let b = await bundle(path.join(__dirname, '/integration/elm/index.js'));
 
     await run(b);
-    let js = await outputFS.readFile(__dirname + '/dist/index.js', 'utf8');
+    let js = await outputFS.readFile(path.join(distDir, 'index.js'), 'utf8');
     assert(js.includes('elm$browser$Debugger'));
   });
 
   it('should apply elm-hot if HMR is enabled', async function() {
-    let b = await bundle(__dirname + '/integration/elm/index.js', {
-      hmr: true,
+    let b = await bundle(path.join(__dirname, '/integration/elm/index.js'), {
+      hot: true,
     });
 
-    await assertBundleTree(b, {
-      type: 'js',
-      assets: ['Main.elm', 'hmr-runtime.js', 'index.js'],
-    });
+    await assertBundles(b, [
+      {
+        type: 'js',
+        assets: ['HMRRuntime.js', 'Main.elm', 'index.js'],
+      },
+    ]);
 
-    let js = await outputFS.readFile(__dirname + '/dist/index.js', 'utf8');
+    let js = await outputFS.readFile(path.join(distDir, 'index.js'), 'utf8');
     assert(js.includes('[elm-hot]'));
   });
 
   it('should remove debugger in production', async function() {
-    let b = await bundle(__dirname + '/integration/elm/index.js', {
-      production: true,
+    let b = await bundle(path.join(__dirname, '/integration/elm/index.js'), {
+      mode: 'production',
     });
 
     await run(b);
-    let js = await outputFS.readFile(__dirname + '/dist/index.js', 'utf8');
+    let js = await outputFS.readFile(path.join(distDir, 'index.js'), 'utf8');
     assert(!js.includes('elm$browser$Debugger'));
   });
 
   it('should minify Elm in production mode', async function() {
-    let b = await bundle(__dirname + '/integration/elm/index.js', {
-      production: true,
+    let b = await bundle(path.join(__dirname, '/integration/elm/index.js'), {
+      mode: 'production',
+      minify: true,
     });
 
-    let output = await run(b);
-    assert.equal(typeof output().Elm.Main.init, 'function');
+    await run(b);
+    // assert.equal(typeof output().Elm.Main.init, 'function');
 
-    let js = await outputFS.readFile(__dirname + '/dist/index.js', 'utf8');
+    let js = await outputFS.readFile(path.join(distDir, 'index.js'), 'utf8');
     assert(!js.includes('elm$core'));
+    assert(js.includes('Elm'));
+    assert(js.includes('init'));
   });
 });
