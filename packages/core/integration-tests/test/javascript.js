a959c532c7875c482d0d302ba6990c06328588d6
Mike Allanson <michael.allanson@gmail.com>
2020-05-19 17:34:26 +0100

    Add output format tests

--- packages/core/integration-tests/test/javascript.js
+++ packages/core/integration-tests/test/javascript.js
@@ -2058,16 +2058,16 @@ describe('javascript', function() {
     assert(output.includes('Other page'));
   });
 
-  it.skip('should stub require.cache', async function() {
-    let b = await bundle(
-      path.join(__dirname, '/integration/node_require_cache/main.js'),
-      {
-        target: 'node',
-      },
-    );
+  // it.skip('(skipped old test) should stub require.cache', async function() {
+  //   let b = await bundle(
+  //     path.join(__dirname, '/integration/node_require_cache/main.js'),
+  //     {
+  //       target: 'node',
+  //     },
+  //   );
 
-    await run(b);
-  });
+  //   await run(b);
+  // });
 
   it('should support async importing the same module from different bundles', async () => {
     let b = await bundle(
