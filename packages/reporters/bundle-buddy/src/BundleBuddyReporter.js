8a655d4e963c3526fe7f29c5682cdd77edd80eba
Devon Govett <devongovett@gmail.com>
2020-05-22 21:06:47 -0700

    WIP: bundle buddy reporter

---
+++ packages/reporters/bundle-buddy/src/BundleBuddyReporter.js
@@ -0,0 +1,49 @@
+// @flow strict-local
+import type {NamedBundle} from '@parcel/types';
+import {Reporter} from '@parcel/plugin';
+import fs from 'fs';
+import path from 'path';
+
+export default new Reporter({
+  report({event, options}) {
+    if (event.type !== 'buildSuccess' || process.env.BUNDLE_BUDDY == null) {
+      return;
+    }
+
+    let bundlesByTarget: Map<string, Array<NamedBundle>> = new Map();
+    for (let bundle of event.bundleGraph.getBundles()) {
+      if (!bundle.isInline) {
+        let bundles = bundlesByTarget.get(bundle.target.distDir);
+        if (!bundles) {
+          bundles = [];
+          bundlesByTarget.set(bundle.target.distDir, bundles);
+        }
+
+        bundles.push(bundle);
+      }
+    }
+
+    for (let [targetDir, bundles] of bundlesByTarget) {
+      let out = [];
+
+      for (let bundle of bundles) {
+        bundle.traverseAssets(asset => {
+          let deps = event.bundleGraph.getDependencies(asset);
+          for (let dep of deps) {
+            let resolved = event.bundleGraph.getDependencyResolution(dep);
+            if (!resolved) {
+              continue;
+            }
+
+            out.push({
+              source: path.relative(options.projectRoot, asset.filePath),
+              target: path.relative(options.projectRoot, resolved.filePath),
+            });
+          }
+        });
+      }
+
+      fs.writeFileSync(`${targetDir}/bundle-buddy.json`, JSON.stringify(out));
+    }
+  },
+});
