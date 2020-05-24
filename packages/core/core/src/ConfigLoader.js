bad0318344c98178706a30968b2887b6fc30d097
Jasper De Moor <jasperdemoor@gmail.com>
2020-05-24 20:03:04 +0200

    fix parcelrc reloading?

--- packages/core/core/src/ConfigLoader.js
+++ packages/core/core/src/ConfigLoader.js
@@ -56,6 +56,7 @@ export default class ConfigLoader {
       env,
     });
     let publicConfig = new PublicConfig(config, this.options);
+    publicConfig.addIncludedFile(this.parcelConfig.filePath);
     let devDeps = [];
     switch (configRequest.meta.actionType) {
       case 'transformation':
