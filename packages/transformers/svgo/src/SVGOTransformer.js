2ead61609306fac160f3b691d97b3341101281d6
Jasper De Moor <jasperdemoor@gmail.com>
2020-06-16 20:28:18 +0200

    update svgo config

--- packages/transformers/svgo/src/SVGOTransformer.js
+++ packages/transformers/svgo/src/SVGOTransformer.js
@@ -3,14 +3,15 @@
 import {Transformer} from '@parcel/plugin';
 
 import SVGO from 'svgo';
+import path from 'path';
 
 const defaultConfig = {
   plugins: [{prefixIds: true}],
 };
 
 export default new Transformer({
-  async getConfig({asset}) {
-    let config = await asset.getConfig(
+  async loadConfig({config}) {
+    let configFile = await config.getConfig(
       [
         '.svgorc',
         '.svgorc.json',
@@ -24,14 +25,38 @@ export default new Transformer({
       },
     );
 
-    config = {...defaultConfig, ...config};
+    if (configFile) {
+      // Don't cache JS configs
+      let isJavaScript = path.extname(configFile.filePath) === '.js';
+      if (isJavaScript) {
+        config.shouldInvalidateOnStartup();
 
-    return config;
+        // Check if we can cache this at all
+        // should fail with advanced data types like functions and maps
+        try {
+          JSON.stringify(configFile.contents);
+        } catch (e) {
+          config.shouldReload();
+        }
+      }
+
+      config.setResult({contents: configFile.contents, isJavaScript});
+    }
+  },
+
+  preSerializeConfig({config}) {
+    if (!config.result) return;
+
+    // Ensure we dont try to serialise functions
+    if (config.result.isJavaScript) {
+      config.result.contents = {};
+    }
   },
 
   async transform({asset, config}) {
+    let svgoConfig = config ? config.contents : {};
     let code = await asset.getCode();
-    let svgo = new SVGO(config);
+    let svgo = new SVGO({...defaultConfig, ...svgoConfig});
     let res = await svgo.optimize(code);
 
     asset.setCode(res.data);
