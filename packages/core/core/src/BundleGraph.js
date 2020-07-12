f51045e265df48bdfa2cf1f582b47c04a5143ece
Jasper De Moor <jasperdemoor@gmail.com>
2020-07-12 14:31:12 +0200

    update hash

--- packages/core/core/src/BundleGraph.js
+++ packages/core/core/src/BundleGraph.js
@@ -6,6 +6,7 @@ import type {
   Symbol,
   TraversalActions,
 } from '@parcel/types';
+import querystring from 'querystring';
 
 import type {
   Asset,
@@ -1032,6 +1033,7 @@ export default class BundleGraph {
           this.getAssetPublicId(asset),
           asset.outputHash,
           asset.filePath,
+          querystring.stringify(asset.query),
           asset.type,
           asset.uniqueKey,
         ].join(':'),
