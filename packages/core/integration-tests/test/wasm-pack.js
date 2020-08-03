5d51534ebe48f246f614572e89998ec57b94e4f7
Matt Hayes <matt@mysterycommand.com>
2020-08-03 16:05:37 -0400

    test: this test sliding around is weird

--- packages/core/integration-tests/test/wasm-pack.js
+++ packages/core/integration-tests/test/wasm-pack.js
@@ -23,26 +23,7 @@ describe('wasm-pack', function() {
 
     await assertBundles(b, [
       {
-        // name: '',
-        // type: '',
-        assets: [
-          'browser-loader.js',
-          'bundle-manifest.js',
-          'bundle-url.js',
-          'Cargo.toml',
-          'index.js',
-          'JSRuntime.js',
-          'JSRuntime.js',
-          'relative-path.js',
-          'single_bg.js',
-        ],
-        // includedFiles: [],
-      },
-      {
-        // name: '',
-        // type: '',
-        assets: ['single_bg.wasm'],
-        // includedFiles: [],
+        assets: ['Cargo.toml', 'index.js'],
       },
     ]);
   });
