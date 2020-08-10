c285c3e97a901fc8e40546baeaa9d4de38d8622f
Will Binns-Smith <wbinnssmith@atlassian.com>
2020-08-10 16:27:50 -0700

    Implement basic graph storing JsValues

--- packages/core/core/graph/src/lib.rs
+++ packages/core/core/graph/src/lib.rs
@@ -1,16 +1,31 @@
+use petgraph::graph::NodeIndex;
+use petgraph::Graph as PetGraph;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub struct MyStruct {
-  pub foo: i32,
+pub struct Graph {
+  graph: PetGraph<JsValue, i32>,
 }
 
 #[wasm_bindgen]
-pub fn get_my_struct() -> MyStruct {
-  MyStruct { foo: 9 }
-}
+impl Graph {
+  #[wasm_bindgen(constructor)]
+  pub fn new() -> Graph {
+    Graph {
+      graph: PetGraph::new(),
+    }
+  }
 
-#[wasm_bindgen(js_name = "getMyNumber")]
-pub fn get_my_number() -> i32 {
-  7
+  #[wasm_bindgen(js_name = "addNode")]
+  pub fn add_node(&mut self, value: JsValue) -> usize {
+    self.graph.add_node(value).index()
+  }
+
+  #[wasm_bindgen(js_name = "getNodeValue")]
+  pub fn get_node_value(&self, index: usize) -> JsValue {
+    match self.graph.node_weight(NodeIndex::new(index)) {
+      Some(value) => value.clone(),
+      None => JsValue::null(),
+    }
+  }
 }
