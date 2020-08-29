dcdd2f707099dbd291aef17a002f5397df674574
Jasper De Moor <jasperdemoor@gmail.com>
2020-08-29 12:25:45 +0200

    esbuild sourcemaps

--- packages/optimizers/esbuild/src/ESBuildOptimizer.js
+++ packages/optimizers/esbuild/src/ESBuildOptimizer.js
@@ -2,7 +2,7 @@
 
 import {startService} from 'esbuild';
 import {Optimizer} from '@parcel/plugin';
-import {blobToString} from '@parcel/utils';
+import {blobToString, normalizePath} from '@parcel/utils';
 import SourceMap from '@parcel/source-map';
 import path from 'path';
 
@@ -17,21 +17,41 @@ export default (new Optimizer({
       service = await startService();
     }
 
+    let relativeBundlePath = path.relative(
+      options.projectRoot,
+      bundle.filePath,
+    );
     let code = await blobToString(contents);
+    if (map && options.sourceMaps) {
+      let vlqMappings = map.toVLQ();
+      vlqMappings = {
+        ...vlqMappings,
+        sources: vlqMappings.sources.map(source => normalizePath(source)),
+        sourcesContent: vlqMappings.sourcesContent
+          ? vlqMappings.sourcesContent.map(content =>
+              content ? content : null,
+            )
+          : [],
+        version: 3,
+        file: normalizePath(relativeBundlePath + '.map'),
+        sourceRoot: options.projectRoot,
+      };
+      code += `\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,${Buffer.from(
+        JSON.stringify(vlqMappings),
+      ).toString('base64')}`;
+    }
 
     let {js, jsSourceMap} = await service.transform(code, {
-      sourcemap: true,
-      sourcefile: path.relative(options.projectRoot, bundle.filePath),
+      sourcemap: options.sourceMaps,
+      sourcefile: relativeBundlePath,
       minify: true,
     });
 
-    let sourcemap = new SourceMap();
-    if (jsSourceMap) {
+    let sourcemap = null;
+    if (jsSourceMap && options.sourceMaps) {
+      sourcemap = new SourceMap();
       let parsedMap = JSON.parse(jsSourceMap);
       sourcemap.addRawMappings(parsedMap);
-      if (map) {
-        sourcemap.extends(map.toBuffer());
-      }
 
       let sourcemapReference = await getSourceMapReference(sourcemap);
       if (sourcemapReference) {
