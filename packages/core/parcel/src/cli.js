d4a6b78ea80ffa50bb0e392098ead9e797757524
Brian Do <bdo@atlassian.com>
2020-09-09 14:01:26 -0700

    add message for exiting with 'ctrl-c'

--- packages/core/parcel/src/cli.js
+++ packages/core/parcel/src/cli.js
@@ -234,6 +234,7 @@ async function run(entries: Array<string>, command: any) {
           // We don't use the SIGINT event for this because when run inside yarn, the parent
           // yarn process ends before Parcel and it appears that Parcel has ended while it may still
           // be cleaning up. Handling events from stdin prevents this impression.
+          INTERNAL_ORIGINAL_CONSOLE.log('Parcel is shutting down');
           await exit(1);
           break;
         case 'e':
