795fbb4579cb86942691798762c061e6d64b10ac
Jasper De Moor <jasperdemoor@gmail.com>
2020-09-13 14:49:13 +0200

    Update packages/core/utils/src/sourcemap.js
    Co-authored-by: Niklas Mischkulnig <mischnic@users.noreply.github.com>

--- packages/core/utils/src/sourcemap.js
+++ packages/core/utils/src/sourcemap.js
@@ -30,9 +30,11 @@ export async function loadSourceMapUrl(
   if (match) {
     let url = match[1].trim();
     let dataURLMatch = url.match(DATA_URL_RE);
-    let mapFilePath = dataURLMatch ? filename : url.replace(/^file:\/\//, '');
-    if (!dataURLMatch && !isAbsolute(mapFilePath)) {
-      mapFilePath = path.join(path.dirname(filename), mapFilePath);
+    let mapFilePath;
+    if (dataURLMatch) {
+        mapFilePath =  isAbsolute(mapFilePath) ? filename : path.join(path.dirname(filename), mapFilePath);
+    } else {
+        mapFilePath = url.replace(/^file:\/\//, '');
     }
 
     return {
