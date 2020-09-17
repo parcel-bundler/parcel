f5fdfa04b01f49712d937f7fa26165c125c0190d
Will Binns-Smith <wbinnssmith@atlassian.com>
2020-09-17 13:38:36 -0700

    Add prefetch loaders

---
+++ packages/runtimes/js/src/loaders/browser/preload-loader.js
@@ -0,0 +1,12 @@
+const cacheLoader = require('../../cacheLoader');
+
+module.exports = cacheLoader(function preloadJSBundle(bundle) {
+  var link = document.createElement('link');
+  link.charset = 'utf-8';
+  link.rel = 'preload';
+  link.as = 'script';
+  link.href = bundle;
+  document.getElementsByTagName('head')[0].appendChild(link);
+
+  return Promise.resolve();
+});
