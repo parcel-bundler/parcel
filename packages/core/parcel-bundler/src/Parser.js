567c399584e661e6c9fe41604703b8b5b2de8925
Mungo Dewar <mungodewar@skyscanner.net>
2020-09-19 00:01:42 +0100

    Expand ReasonAsset to include res file format

--- packages/core/parcel-bundler/src/Parser.js
+++ packages/core/parcel-bundler/src/Parser.js
@@ -15,6 +15,7 @@ class Parser {
     this.registerExtension('mjs', './assets/JSAsset');
     this.registerExtension('ml', './assets/ReasonAsset');
     this.registerExtension('re', './assets/ReasonAsset');
+    this.registerExtension('res', './assets/ReasonAsset');
     this.registerExtension('ts', './assets/TypeScriptAsset');
     this.registerExtension('tsx', './assets/TypeScriptAsset');
     this.registerExtension('coffee', './assets/CoffeeScriptAsset');
