1e8a66421df393c60fbeba20b4c0b4580ea5be58
Will Binns-Smith <wbinnssmith@atlassian.com>
2020-10-13 20:45:03 -0700

    Don't recursively check reachability when removing asset graphs

--- packages/core/core/src/BundleGraph.js
+++ packages/core/core/src/BundleGraph.js
@@ -332,11 +332,7 @@ export default class BundleGraph {
       }
 
       if (node.type === 'asset' || node.type === 'dependency') {
-        if (
-          this._graph.hasEdge(bundle.id, node.id, 'contains') &&
-          (node.type !== 'asset' ||
-            this.isAssetReachableFromBundle(node.value, bundle))
-        ) {
+        if (this._graph.hasEdge(bundle.id, node.id, 'contains')) {
           this._graph.removeEdge(
             bundle.id,
             node.id,
