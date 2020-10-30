358e47c345af94dec41331d28f514a0a7b3c76ed
Arjun Barrett <arjunbarrett@gmail.com>
2020-10-30 03:45:47 -0700

    Add full webext schema

--- packages/transformers/json/src/JSONTransformer.js
+++ packages/transformers/json/src/JSONTransformer.js
@@ -2,8 +2,24 @@
 
 import {Transformer} from '@parcel/plugin';
 import json5 from 'json5';
+import semver from 'semver';
 
 export default (new Transformer({
+  canReuseAST({ast}) {
+    return ast.type == 'json5' && semver.satisfies(ast.version, '^2.1.0');
+  },
+  async parse({asset}) {
+    // This indicates a previous transformer (e.g. WebExt) has applied special
+    // handling to this already
+    if (asset.meta.hasDependencies === false) {
+      return null;
+    }
+    return {
+      type: 'json5',
+      version: '2.1.0',
+      program: json5.parse(await asset.getCode())
+    }
+  },
   async transform({asset}) {
     asset.type = 'js';
     // Use JSON.parse("...") for faster script parsing, see
@@ -11,7 +27,7 @@ export default (new Transformer({
     // Apply `JSON.stringify` twice to make it a valid string literal.
     asset.setCode(
       `module.exports = JSON.parse(${JSON.stringify(
-        JSON.stringify(json5.parse(await asset.getCode())),
+        JSON.stringify(await asset.getAST()),
       )});`,
     );
     return [asset];
--- packages/transformers/webext/src/WebExtTransformer.js
+++ packages/transformers/webext/src/WebExtTransformer.js
@@ -2,18 +2,38 @@
 
 import {Transformer} from '@parcel/plugin';
 import json5 from 'json5';
+import {validateSchema} from '@parcel/utils';
+import {encodeJSONKeyComponent} from '@parcel/diagnostic';
+import WebExtSchema from './schema';
+
+const BASE_KEYS = ['manifest_version', 'name', 'version'];
 
 export default (new Transformer({
-  async transform({asset}) {
-    asset.type = 'js';
-    // Use JSON.parse("...") for faster script parsing, see
-    // https://v8.dev/blog/cost-of-javascript-2019#json.
-    // Apply `JSON.stringify` twice to make it a valid string literal.
-    asset.setCode(
-      `module.exports = JSON.parse(${JSON.stringify(
-        JSON.stringify(json5.parse(await asset.getCode())),
-      )});`,
+  async parse({asset}) {
+    const manifest = json5.parse(await asset.getCode());
+    if (BASE_KEYS.some(key => !manifest.hasOwnProperty(key))) {
+      // This is probably just another file that happens to be named manifest.json
+      return null;
+    }
+    validateSchema.diagnostic(
+      WebExtSchema,
+      manifest,
+      asset.filePath,
+      await asset.getCode(),
+      '@parcel/transformer-webext',
+      `/${encodeJSONKeyComponent('@parcel/transformer-webext')}`,
+      'Invalid Web Extension manifest'
     );
+    return {
+      type: 'json5',
+      version: '2.1.0',
+      program: manifest
+    }
+  },
+  async transform({asset}) {
+    const manifest = await asset.getAST();
+    
+    asset.meta.hasDependencies = false;
     return [asset];
   },
 }): Transformer);
