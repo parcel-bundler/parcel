370ee17b1510d6a8d220fb585468948d75235b62
Agnieszka Gawrys <agawrys@atlassian.com>
2020-11-19 16:32:04 -0800

    format

--- packages/core/utils/src/ansi-html.js
+++ packages/core/utils/src/ansi-html.js
@@ -1,6 +1,5 @@
 // @flow strict-local
 import ansiHTML from 'ansi-html';
-// flowlint-next-line untyped-import:off
 import {escapeHTML} from './escape-html';
 
 export function ansiHtml(ansi: string): string {
