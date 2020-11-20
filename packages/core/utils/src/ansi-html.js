da59592eb4af6ea9c7f8560ae5921837ec38b472
Agnieszka Gawrys <agawrys@atlassian.com>
2020-11-19 16:16:40 -0800

    Flowlint disable for different import

--- packages/core/utils/src/ansi-html.js
+++ packages/core/utils/src/ansi-html.js
@@ -1,5 +1,6 @@
-// @flow strict enable
+// @flow strict-local
 import ansiHTML from 'ansi-html';
+// flowlint-next-line untyped-import:off
 import {escapeHTML} from './escape-html';
 
 export function ansiHtml(ansi: string): string {
