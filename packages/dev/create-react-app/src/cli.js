ead8bccca9a851c3d16cf9436851fc9bd50d5f49
Will Binns-Smith <wbinnssmith@atlassian.com>
2020-12-10 12:58:03 -0800

    Create package directory and git repository

--- packages/dev/create-react-app/src/cli.js
+++ packages/dev/create-react-app/src/cli.js
@@ -3,6 +3,10 @@
 import program from 'commander';
 // flowlint-next-line untyped-import:off
 import {name, version} from '../package.json';
+import mkdirp from 'mkdirp';
+// flowlint-next-line untyped-import:off
+import simpleGit from 'simple-git';
+import fs from 'fs';
 
 // flowlint-next-line untyped-import:off
 require('v8-compile-cache');
@@ -14,11 +18,44 @@ program.action((command: string | typeof program) => {
     return;
   }
 
-  run(command);
+  run(command).catch(reason => {
+    // eslint-disable-next-line no-console
+    console.error(reason.message);
+    process.exit(1);
+  });
 });
 
 program.parse(process.argv);
 
-function run(packageName: string) {
-  console.log('running', packageName);
+async function run(packagePath: string) {
+  log('running path', packagePath);
+  if (await fsExists(packagePath)) {
+    throw new Error(`Package at ${packagePath} already exists`);
+  }
+
+  // Create directory
+  log('Creating package directory...');
+  await mkdirp(packagePath);
+
+  // Initialize repo
+  const git = simpleGit({baseDir: packagePath});
+  log('Initializing git repository...');
+  await git.init();
+
+  // Copy templates
+  // Install packages
+  // Print instructions
+}
+
+async function fsExists(filePath: string): Promise<boolean> {
+  try {
+    return (await fs.promises.stat(filePath)) && true;
+  } catch {
+    return false;
+  }
+}
+
+function log(...args: Array<mixed>): void {
+  // eslint-disable-next-line no-console
+  console.log(...args);
 }
