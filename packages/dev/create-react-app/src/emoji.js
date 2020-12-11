b65bf759f28b29196769b093977ac0314f491de3
Dario Novoa <darionovoa@ideartte.com>
2020-12-11 12:31:15 -0800

    Create parcel react app color (#5457)
    * Adding chalk, moving no a diff machine
    
    * Added chalk and emojis, this needs work
    
    * done with coloring
    
    * Adding an extra space for info to match the rest of the messages.
    
    * Resolved conflicts with upstream
    
    Co-authored-by: Dario Novoa <darionovoa@Darios-MacBook-Pro-13.local>

---
+++ packages/dev/create-react-app/src/emoji.js
@@ -0,0 +1,12 @@
+// @flow strict-local
+
+const supportsEmoji =
+  process.platform !== 'win32' || process.env.TERM === 'xterm-256color';
+
+// Fallback symbols for Windows from https://en.wikipedia.org/wiki/Code_page_437
+export const progress: string = supportsEmoji ? '⏳' : '∞';
+export const success: string = supportsEmoji ? '✨' : '√';
+export const error: string = supportsEmoji ? '🚨' : '×';
+export const warning: string = supportsEmoji ? '⚠️' : '‼';
+export const info: string = supportsEmoji ? 'ℹ️' : 'ℹ';
+export const hint: string = supportsEmoji ? '💡' : 'ℹ';
