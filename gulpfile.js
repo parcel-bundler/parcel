18be79522451edcb0feeccb01681f1ae1561dce6
Will Binns-Smith <wbinnssmith@atlassian.com>
2020-12-11 10:27:53 -0800

    Build @parcel/create-react-app for publishing

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,10 @@ const paths = {
     'packages/*/*/src/**/prelude.js',
     'packages/*/dev-server/src/templates/**',
   ],
-  packageJson: 'packages/core/parcel/package.json',
+  packageJson: [
+    'packages/core/parcel/package.json',
+    'packages/dev/create-react-app/package.json',
+  ],
   packages: 'packages/',
 };
 
@@ -60,7 +63,12 @@ exports.clean = function clean(cb) {
 exports.default = exports.build = gulp.series(
   gulp.parallel(buildBabel, copyOthers),
   // Babel reads from package.json so update these after babel has run
-  () => updatePackageJson(paths.packageJson),
+  paths.packageJson.map(
+    packageJsonPath =>
+      function updatePackageJson() {
+        return _updatePackageJson(packageJsonPath);
+      },
+  ),
 );
 
 function buildBabel() {
@@ -78,7 +86,7 @@ function copyOthers() {
     .pipe(gulp.dest(paths.packages));
 }
 
-function updatePackageJson(file) {
+function _updatePackageJson(file) {
   return gulp
     .src(file)
     .pipe(
