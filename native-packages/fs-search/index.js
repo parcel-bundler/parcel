f7968dffc1acc07f8525858c6a350971a886e3e5
Devon Govett <devongovett@gmail.com>
2020-12-19 13:57:22 -0500

    Load correct build

--- native-packages/fs-search/index.js
+++ native-packages/fs-search/index.js
@@ -1,1 +1,12 @@
-module.exports = require(`./index.${process.platform}.node`);
+const fs = require('fs');
+
+let parts = [process.platform, process.arch];
+if (process.platform === 'linux') {
+  parts.push(
+    fs.existsSync('/etc/alpine-release') ? 'musl' : 'gnu'
+  );
+} else if (process.platform === 'win32') {
+  parts.push('msvc');
+}
+
+module.exports = require(`./fs-search.${parts.join('-')}.node`);
