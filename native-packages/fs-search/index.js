dd6219a5ce7d1a8131896b473bb0a57ca6333aa0
Devon Govett <devongovett@gmail.com>
2020-12-19 21:17:43 -0500

    Test running arm binaries using emulator

--- native-packages/fs-search/index.js
+++ native-packages/fs-search/index.js
@@ -2,9 +2,13 @@ const fs = require('fs');
 
 let parts = [process.platform, process.arch];
 if (process.platform === 'linux') {
-  parts.push(
-    fs.existsSync('/etc/alpine-release') ? 'musl' : 'gnu'
-  );
+  if (fs.existsSync('/etc/alpine-release')) {
+    parts.push('musl');
+  } else if (process.arch === 'arm') {
+    parts.push('gnueabihf');
+  } else {
+    parts.push('gnu');
+  }
 } else if (process.platform === 'win32') {
   parts.push('msvc');
 }
