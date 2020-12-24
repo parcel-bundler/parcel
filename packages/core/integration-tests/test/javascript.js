04ceb31e94dddd22fd380a00f7e5311fffccf673
Jessie France <FranceJessieJ@gmail.com>
2020-12-24 16:32:00 -0700

    Prevent automatic naming that starts with a dot

--- packages/core/integration-tests/test/javascript.js
+++ packages/core/integration-tests/test/javascript.js
@@ -3015,6 +3015,21 @@ describe('javascript', function() {
     ]);
   });
 
+  it('should not automatically name bundle files starting with a dot', async function() {
+    await bundle(
+      path.join(__dirname, '/integration/bundle-naming/.invisible/index.js'),
+    );
+    let bundleFiles = await outputFS.readdir(distDir);
+    let renamedSomeFiles = bundleFiles.some(currFile =>
+      currFile.startsWith('invisible.'),
+    );
+    let namedWithDot = bundleFiles.some(currFile =>
+      currFile.startsWith('.invisible.'),
+    );
+    assert.equal(renamedSomeFiles, true);
+    assert.equal(namedWithDot, false);
+  });
+
   it('should support duplicate re-exports without scope hoisting', async function() {
     let b = await bundle(
       path.join(__dirname, 'integration/js-duplicate-re-exports/index.js'),
