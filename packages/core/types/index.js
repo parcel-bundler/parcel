94e531f5af5b6626149779ae8778b3e1c0dd6ab8
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2021-01-02 19:32:16 +0100

    CSS modules: remove unused classes (#5363)

--- packages/core/types/index.js
+++ packages/core/types/index.js
@@ -556,7 +556,6 @@ export interface MutableAsset extends BaseAsset {
   +symbols: MutableAssetSymbols;
 
   isASTDirty(): boolean;
-  /** Returns <code>null</code> if there is no AST. */
   getAST(): Promise<?AST>;
   setAST(AST): void;
   setBuffer(Buffer): void;
@@ -572,9 +571,6 @@ export interface MutableAsset extends BaseAsset {
  * @section transformer
  */
 export interface Asset extends BaseAsset {
-  /** Throws if there is no AST.*/
-  getAST(): Promise<?AST>;
-
   +stats: Stats;
 }
 
