0a4b950f2502f61d4fcfbb6cd42936ce6e48656c
Devon Govett <devongovett@gmail.com>
2021-02-16 23:13:38 -0500

    Dedup bundles

--- packages/core/core/src/BundleGraph.js
+++ packages/core/core/src/BundleGraph.js
@@ -658,10 +658,12 @@ export default class BundleGraph {
     let visitedBundles: Set<Bundle> = new Set();
     // Check if any of this bundle's descendants, referencers, bundles referenced
     // by referencers, or descedants of its referencers reference the asset.
-    let siblingBundles = this.getBundleGroupsContainingBundle(
-      bundle,
-    ).flatMap(bundleGroup => this.getBundlesInBundleGroup(bundleGroup));
-    return siblingBundles.some(referencer => {
+    let siblingBundles = new Set(
+      this.getBundleGroupsContainingBundle(bundle).flatMap(bundleGroup =>
+        this.getBundlesInBundleGroup(bundleGroup),
+      ),
+    );
+    return [...siblingBundles].some(referencer => {
       let isReferenced = false;
       this.traverseBundles((descendant, _, actions) => {
         if (descendant.id === bundle.id) {
