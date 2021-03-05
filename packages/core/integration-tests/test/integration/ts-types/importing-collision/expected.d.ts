66f1371297aa4955489340c23fc3a74891d1ee58
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-03-05 21:56:02 +0100

    Add test

---
+++ packages/core/integration-tests/test/integration/ts-types/importing-collision/expected.d.ts
@@ -0,0 +1,5 @@
+declare function _log1(message: string): void;
+declare function xyz(message: number): void;
+export function log(f: typeof _log1 | typeof xyz): void;
+
+//# sourceMappingURL=types.d.ts.map
