a6c07235fc952cdd153855237686c1386b4f3108
Jasper De Moor <jasperdemoor@gmail.com>
2021-03-11 10:01:13 +0100

    change limit based on platform

--- packages/core/workers/src/cpuCount.js
+++ packages/core/workers/src/cpuCount.js
@@ -1,7 +1,14 @@
 // @flow
 import os from 'os';
 
-export function getCoreCount(limit: number = 8): number {
-  let coreCount = Math.ceil(os.cpus().length / 2);
-  return Math.max(Math.min(coreCount, limit), 1);
+let cachedCoreCount = 0;
+const platform = os.platform();
+export function getCoreCount(
+  limit: number = platform === 'win32' ? 4 : 8,
+): number {
+  if (!cachedCoreCount) {
+    cachedCoreCount = Math.ceil(os.cpus().length / 2);
+  }
+
+  return Math.max(Math.min(cachedCoreCount, limit), 1);
 }
