24dde9f3f46a6d471348b65bc64e7cf0271ed095
Agnieszka Gawrys <agawrys@atlassian.com>
2021-03-25 15:51:00 -0400

    added language server client

--- packages/utils/parcelforvscode/src/extension.ts
+++ packages/utils/parcelforvscode/src/extension.ts
@@ -1,28 +1,60 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as path from 'path';
+import {workspace, ExtensionContext} from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from 'vscode-languageclient/node';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "parcelforvscode" is now active!');
+let client: LanguageClient;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('parcelforvscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+export function activate(context: ExtensionContext) {
+  // The server is implemented in node
+  let serverModule = context.asAbsolutePath(
+    path.join('server', 'out', 'server.js'),
+  );
+  // The debug options for the server
+  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+  let debugOptions = {execArgv: ['--nolazy', '--inspect=6009']};
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello from Parcel for VS Code!');
-	});
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  async function serverOptions(): Promise<MessageTransports> {
+    let [reader, writer] = await createClientPipeTransport('Parcel');
+    return {
+      reader,
+      writer,
+      //Detached true probably
+    };
+  }
 
-	context.subscriptions.push(disposable);
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{scheme: 'file', language: 'plaintext'}],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher('**/.clientrc'),
+    },
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    'languageServerExample',
+    'Language Server Example',
+    serverOptions,
+    clientOptions,
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
-\
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
