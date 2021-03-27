2ea2227d4ebbc28b8e1410f385a01df8de9add98
Devon Govett <devongovett@gmail.com>
2021-03-27 19:40:10 -0400

    Generate parcelRequire name

--- packages/packagers/swc/src/index.js
+++ packages/packagers/swc/src/index.js
@@ -2,11 +2,33 @@
 import type {Async} from '@parcel/types';
 import type SourceMap from '@parcel/source-map';
 import {Packager} from '@parcel/plugin';
-import {replaceInlineReferences} from '@parcel/utils';
+import {
+  replaceInlineReferences,
+  md5FromString,
+  loadConfig,
+} from '@parcel/utils';
+import path from 'path';
+import nullthrows from 'nullthrows';
 import {DevPackager} from './DevPackager';
 import {SWCPackager} from './SWCPackager';
 
 export default (new Packager({
+  async loadConfig({options}) {
+    // Generate a name for the global parcelRequire function that is unique to this project.
+    // This allows multiple parcel builds to coexist on the same page.
+    let pkg = await loadConfig(
+      options.inputFS,
+      path.join(options.entryRoot, 'index'),
+      ['package.json'],
+    );
+    let name = pkg?.config.name ?? '';
+    return {
+      config: {
+        parcelRequireName: 'parcelRequire' + md5FromString(name).slice(-4),
+      },
+      files: pkg?.files ?? [],
+    };
+  },
   async package({
     bundle,
     bundleGraph,
@@ -15,10 +37,19 @@ export default (new Packager({
     config,
     options,
   }) {
-    let parcelRequireName = 'parcelRequire123'; // TODO
     let packager = bundle.env.shouldScopeHoist
-      ? new SWCPackager(options, bundleGraph, bundle, parcelRequireName)
-      : new DevPackager(options, bundleGraph, bundle, parcelRequireName);
+      ? new SWCPackager(
+          options,
+          bundleGraph,
+          bundle,
+          nullthrows(config).parcelRequireName,
+        )
+      : new DevPackager(
+          options,
+          bundleGraph,
+          bundle,
+          nullthrows(config).parcelRequireName,
+        );
 
     let {contents, map} = await packager.package();
     contents += '\n' + (await getSourceMapSuffix(getSourceMapReference, map));
