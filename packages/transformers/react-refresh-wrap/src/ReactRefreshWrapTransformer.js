4f02eccb62007660c732de4bfbd57c95d314c160
Devon Govett <devongovett@gmail.com>
2021-03-27 08:27:26 -0400

    Implement React Refresh wrap transformer using strings instead of ASTs

--- packages/transformers/react-refresh-wrap/src/ReactRefreshWrapTransformer.js
+++ packages/transformers/react-refresh-wrap/src/ReactRefreshWrapTransformer.js
@@ -1,34 +1,10 @@
 // @flow
 
-import type {StringLiteral, Statement} from '@babel/types';
-
-import semver from 'semver';
 import path from 'path';
-import {generate, parse} from '@parcel/babel-ast-utils';
 import {Transformer} from '@parcel/plugin';
 import {relativePath} from '@parcel/utils';
-import template from '@babel/template';
-import * as t from '@babel/types';
 
 const WRAPPER = path.join(__dirname, 'helpers', 'helpers.js');
-
-const wrapper = template.statements<{|
-  helper: StringLiteral,
-  module: Array<Statement>,
-|}>(`
-var helpers = require(%%helper%%);
-var prevRefreshReg = window.$RefreshReg$;
-var prevRefreshSig = window.$RefreshSig$;
-helpers.prelude(module);
-
-try {
-  %%module%%
-  helpers.postlude(module);
-} finally {
-  window.$RefreshReg$ = prevRefreshReg;
-  window.$RefreshSig$ = prevRefreshSig;
-}
-`);
 
 function shouldExclude(asset, options) {
   return (
@@ -41,25 +17,8 @@ function shouldExclude(asset, options) {
 }
 
 export default (new Transformer({
-  canReuseAST({ast}) {
-    return ast.type === 'babel' && semver.satisfies(ast.version, '^7.0.0');
-  },
-
-  async parse({asset, options}) {
-    if (shouldExclude(asset, options)) {
-      return null;
-    }
-
-    return parse({
-      asset,
-      code: await asset.getCode(),
-      options,
-    });
-  },
-
   async transform({asset, options}) {
-    let ast = await asset.getAST();
-    if (!ast || shouldExclude(asset, options)) {
+    if (shouldExclude(asset, options)) {
       return [asset];
     }
 
@@ -68,11 +27,25 @@ export default (new Transformer({
       wrapperPath = './' + wrapperPath;
     }
 
-    ast.program.program.body = wrapper({
-      helper: t.stringLiteral(wrapperPath),
-      module: ast.program.program.body,
-    });
-    asset.setAST(ast);
+    let code = await asset.getCode();
+    let map = await asset.getMap();
+
+    code = `var helpers = require(${JSON.stringify(wrapperPath)});
+var prevRefreshReg = window.$RefreshReg$;
+var prevRefreshSig = window.$RefreshSig$;
+helpers.prelude(module);
+
+try {
+${code}
+  helpers.postlude(module);
+} finally {
+  window.$RefreshReg$ = prevRefreshReg;
+  window.$RefreshSig$ = prevRefreshSig;
+}`;
+
+    map.offsetLines(1, 6);
+    asset.setCode(code);
+    asset.setMap(map);
 
     // The JSTransformer has already run, do it manually
     asset.addDependency({
@@ -80,9 +53,5 @@ export default (new Transformer({
     });
 
     return [asset];
-  },
-
-  generate({asset, ast, options}) {
-    return generate({asset, ast, options});
   },
 }): Transformer);
