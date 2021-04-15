edfb2426d7a1d5a0fa3ddf65a703173db1ab1d33
Elliott Kember <elliott.kember@gmail.com>
2021-04-15 16:54:48 -0700

    Fix regular expression for file:/// URLs
    https://github.com/parcel-bundler/parcel/issues/6141

--- packages/runtimes/js/src/bundle-url.js
+++ packages/runtimes/js/src/bundle-url.js
@@ -38,6 +38,7 @@ function getBaseURL(url) {
 // TODO: Replace uses with `new URL(url).origin` when ie11 is no longer supported.
 function getOrigin(url) {
   let matches = ('' + url).match(/(https?|file|ftp):\/\/[^/]+/);
+  let matches = ('' + url).match(/(https?|file|ftp):\/\/(\/)?[^/]+/);
   if (!matches) {
     throw new Error('Origin not found');
   }
