4c7f93c2ed236d1e7a430b69ca24de462e4aff83
Joey Slater <jslater@atlassian.com>
2021-05-05 23:57:49 -0500

    remove the incremental example

--- packages/examples/incremental/src/index.js
+++
@@ -1,8 +0,0 @@
-import assetA from './asset-a';
-
-console.log('printing: index.js', 0);
-console.log('printing: ', assetA());
-// console.log('printing: ', assetA());
-// console.log('printing1: ', assetA());
-// console.log('printing1: ', assetA());
-// console.log('printing1: ', assetA());
