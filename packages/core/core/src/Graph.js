d573e7a6ac1ada1493001494223e93f194a4b3ae
thebriando <bdo@atlassian.com>
2021-05-12 10:38:31 -0700

    Add generic TEdgeType to EfficientGraph

--- packages/core/core/src/Graph.js
+++ packages/core/core/src/Graph.js
@@ -8,7 +8,7 @@ import type {TraversalActions, GraphVisitor} from '@parcel/types';
 import assert from 'assert';
 import nullthrows from 'nullthrows';
 
-type NullEdgeType = 1;
+export type NullEdgeType = 1;
 export type GraphOpts<TNode, TEdgeType: number = 1> = {|
   nodes?: Map<NodeId, TNode>,
   edges?: AdjacencyListMap<TEdgeType | NullEdgeType>,
@@ -22,7 +22,7 @@ export default class Graph<TNode: Node, TEdgeType: number = 1> {
   nodes: Map<NodeId, TNode>;
   inboundEdges: AdjacencyList<TEdgeType | NullEdgeType>;
   outboundEdges: AdjacencyList<TEdgeType | NullEdgeType>;
-  adjacencyList: EfficientGraph;
+  adjacencyList: EfficientGraph<TEdgeType>;
   rootNodeId: ?NodeId;
   nextNodeId: number = 0;
 
@@ -35,7 +35,7 @@ export default class Graph<TNode: Node, TEdgeType: number = 1> {
     if (edges != null) {
       // this.inboundEdges = new AdjacencyList();
       // this.outboundEdges = new AdjacencyList(edges);
-      this.adjacencyList = new EfficientGraph();
+      this.adjacencyList = new EfficientGraph<TEdgeType>();
       for (let [from, edgeList] of edges) {
         for (let [type, toNodes] of edgeList) {
           for (let to of toNodes) {
@@ -47,7 +47,7 @@ export default class Graph<TNode: Node, TEdgeType: number = 1> {
     } else {
       // this.inboundEdges = new AdjacencyList();
       // this.outboundEdges = new AdjacencyList();
-      this.adjacencyList = new EfficientGraph();
+      this.adjacencyList = new EfficientGraph<TEdgeType>();
     }
   }
 
@@ -141,7 +141,7 @@ export default class Graph<TNode: Node, TEdgeType: number = 1> {
   ): Array<NodeId> {
     this._assertHasNodeId(nodeId);
 
-    return [...this.adjacencyList.getNodesConnectedTo(nodeId, Number(type))];
+    return [...this.adjacencyList.getNodesConnectedTo(nodeId, type)];
     // let inboundByType = this.inboundEdges.getEdgesByType(nodeId);
     // if (inboundByType == null) {
     //   return [];
@@ -175,7 +175,7 @@ export default class Graph<TNode: Node, TEdgeType: number = 1> {
   ): Array<NodeId> {
     this._assertHasNodeId(nodeId);
 
-    return [...this.adjacencyList.getNodesConnectedFrom(nodeId, Number(type))];
+    return [...this.adjacencyList.getNodesConnectedFrom(nodeId, type)];
     // let outboundByType = this.outboundEdges.getEdgesByType(nodeId);
     // if (outboundByType == null) {
     //   return [];
