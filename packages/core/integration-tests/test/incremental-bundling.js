11970d1d8b0549ffb4a1f2f4c6002a05c54f13d5
Joey Slater <jslater@atlassian.com>
2021-05-17 09:30:14 -0500

    Adding integration tests

--- packages/core/integration-tests/test/incremental-bundling.js
+++ packages/core/integration-tests/test/incremental-bundling.js
@@ -1,88 +1,77 @@
 // @flow strict-local
-import {bundler, getNextBuild, inputFS, overlayFS} from '@parcel/test-utils';
-import invariant from 'assert';
+import {bundler, getNextBuildSuccess, overlayFS} from '@parcel/test-utils';
 import assert from 'assert';
 import path from 'path';
+import sinon from 'sinon';
+import Bundler from '@parcel/bundler-default';
+import {CONFIG} from '@parcel/plugin';
+import CustomBundler from './integration/incremental-bundling/node_modules/parcel-bundler-test';
 
-import type {PackagedBundle} from '@parcel/types';
+describe('incremental bundling', function() {
+  let defaultBundlerSpy = sinon.spy(Bundler[CONFIG], 'bundle');
+  let customBundlerSpy = sinon.spy(CustomBundler[CONFIG], 'bundle');
 
-// Where I am struggling...  I need a way to store the amount of times the bundler has run; and to check that value.
-// What I have here almost worked, uses a fake bundler under this packages node modules and stores the number of time an asset was bundled.
-// Perhaps we could output the number of times into a temporary file and check that?
+  let assertChangedAssets = (actual: number, expected: number) => {
+    assert.equal(
+      actual,
+      expected,
+      `the number of changed assets should be ${expected}, not ${actual}`,
+    );
+  };
 
-function allAssetsBundledTimes(
-  bundles: PackagedBundle[],
-  expectedNumberOfTimesBundled: number,
-) {
-  bundles.forEach(b =>
-    b.traverseAssets(asset => {
-      // run times are merged after the bundler
-      if (
-        !asset.filePath.match(/runtimes/) &&
-        asset.meta.timesBundled != null
-      ) {
-        let numberOfTimesBundledEqual =
-          asset.meta.timesBundled === expectedNumberOfTimesBundled;
-        assert(
-          numberOfTimesBundledEqual,
-          `the asset should have only been bundled ${expectedNumberOfTimesBundled} time(s)}`,
-        );
-      }
-    }),
-  );
-}
+  let assertTimesBundled = (actual: number, expected: number) => {
+    assert.equal(
+      actual,
+      expected,
+      `the bundler should have bundled ${expected} time(s), not ${actual}`,
+    );
+  };
 
-describe.only('incremental bundling', function() {
-  before(async () => {
-    await inputFS.rimraf(path.join(__dirname, 'input'));
+  beforeEach(() => {
+    defaultBundlerSpy.resetHistory();
+    customBundlerSpy.resetHistory();
   });
 
-  // beforeEach(() => {
-  //   inputDir = path.join(
-  //     __dirname,
-  //     '/input',
-  //     Math.random()
-  //       .toString(36)
-  //       .slice(2),
-  //   );
-  // });
+  after(() => {
+    defaultBundlerSpy.restore();
+    customBundlerSpy.restore();
+  });
 
   describe('non-dependency based changes', () => {
     describe('javascript', () => {
-      it.only('add a console log should not bundle', async () => {
-        this.timeout(15000);
+      it('add a console log should not bundle', async () => {
         let subscription;
-
         let fixture = path.join(__dirname, '/integration/incremental-bundling');
         try {
           let b = bundler(path.join(fixture, 'index.js'), {
             inputFS: overlayFS,
             shouldDisableCache: false,
           });
+
           await overlayFS.mkdirp(fixture);
           subscription = await b.watch();
 
-          let event = await getNextBuild(b);
-          invariant(event.type === 'buildSuccess');
-          let beforeBundles = event.bundleGraph.getBundles();
-          allAssetsBundledTimes(beforeBundles, 1);
+          let event = await getNextBuildSuccess(b);
+          assertTimesBundled(defaultBundlerSpy.callCount, 1);
 
           await overlayFS.writeFile(
             path.join(fixture, 'index.js'),
-            `
-const a = import('./a');
+            `const a = import('./a');
 
 console.log('index.js');
-console.log('adding a new console');
-          `,
+console.log(a);
+console.log('adding a new console');`,
           );
 
-          event = await getNextBuild(b);
-          invariant(event.type === 'buildSuccess');
+          event = await getNextBuildSuccess(b);
+          assertChangedAssets(event.changedAssets.size, 1);
+          assertTimesBundled(defaultBundlerSpy.callCount, 1);
 
-          let afterBundles = event.bundleGraph.getBundles();
-          allAssetsBundledTimes(afterBundles, 1);
-          assert.equal(event.changedAssets.size, 1);
+          let output = await overlayFS.readFile(
+            path.join(fixture, 'index.js'),
+            'utf8',
+          );
+          assert(output.includes(`console.log('adding a new console')`));
         } finally {
           if (subscription) {
             await subscription.unsubscribe();
@@ -91,39 +80,39 @@ console.log('adding a new console');
         }
       });
 
-      it.only('updating a string value should not bundle', async () => {
-        this.timeout(15000);
+      it('updating a string value should not bundle', async () => {
         let subscription;
-
         let fixture = path.join(__dirname, '/integration/incremental-bundling');
         try {
           let b = bundler(path.join(fixture, 'index.js'), {
             inputFS: overlayFS,
             shouldDisableCache: false,
           });
+
           await overlayFS.mkdirp(fixture);
           subscription = await b.watch();
 
-          let event = await getNextBuild(b);
-          invariant(event.type === 'buildSuccess');
-          let beforeBundles = event.bundleGraph.getBundles();
-          allAssetsBundledTimes(beforeBundles, 1);
+          let event = await getNextBuildSuccess(b);
+          assertTimesBundled(defaultBundlerSpy.callCount, 1);
 
           await overlayFS.writeFile(
             path.join(fixture, 'index.js'),
-            `
-const a = import('./a');
+            `const a = import('./a');
 
 console.log('index.js - updated string');
-          `,
+console.log(a);
+`,
           );
 
-          event = await getNextBuild(b);
-          invariant(event.type === 'buildSuccess');
+          event = await getNextBuildSuccess(b);
+          assertChangedAssets(event.changedAssets.size, 1);
+          assertTimesBundled(defaultBundlerSpy.callCount, 1);
 
-          let afterBundles = event.bundleGraph.getBundles();
-          allAssetsBundledTimes(afterBundles, 1);
-          assert.equal(event.changedAssets.size, 1);
+          let output = await overlayFS.readFile(
+            path.join(fixture, 'index.js'),
+            'utf8',
+          );
+          assert(output.includes(`console.log('index.js - updated string');`));
         } finally {
           if (subscription) {
             await subscription.unsubscribe();
@@ -131,23 +120,223 @@ console.log('index.js - updated string');
           }
         }
       });
-      it('adding a comment');
-    });
 
-    describe('non-javascript', () => {
-      it('updating css');
-      it('updating sass');
+      it('adding a comment', async () => {
+        let subscription;
+        let fixture = path.join(__dirname, '/integration/incremental-bundling');
+        try {
+          let b = bundler(path.join(fixture, 'index.js'), {
+            inputFS: overlayFS,
+            shouldDisableCache: false,
+          });
+
+          await overlayFS.mkdirp(fixture);
+          subscription = await b.watch();
+
+          let event = await getNextBuildSuccess(b);
+          assertTimesBundled(defaultBundlerSpy.callCount, 1);
+
+          await overlayFS.writeFile(
+            path.join(fixture, 'index.js'),
+            `const a = import('./a');
+// test comment
+console.log('index.js');
+console.log(a);`,
+          );
+
+          event = await getNextBuildSuccess(b);
+          assertChangedAssets(event.changedAssets.size, 1);
+          assertTimesBundled(defaultBundlerSpy.callCount, 1);
+
+          let output = await overlayFS.readFile(
+            path.join(fixture, 'index.js'),
+            'utf8',
+          );
+          assert(output.includes(`// test comment`));
+        } finally {
+          if (subscription) {
+            await subscription.unsubscribe();
+            subscription = null;
+          }
+        }
+      });
     });
   });
 
   describe('dependency based changes should run the bundler', () => {
-    it('adding a new dependency');
-    it('removing a dependency');
-    it('updating to remove a symbol');
+    it('adding a new dependency', async () => {
+      let subscription;
+      let fixture = path.join(__dirname, '/integration/incremental-bundling');
+      try {
+        let b = bundler(path.join(fixture, 'index.js'), {
+          inputFS: overlayFS,
+          shouldDisableCache: false,
+        });
+
+        await overlayFS.mkdirp(fixture);
+        subscription = await b.watch();
+
+        let event = await getNextBuildSuccess(b);
+        assertTimesBundled(defaultBundlerSpy.callCount, 1);
+
+        await overlayFS.writeFile(
+          path.join(fixture, 'index.js'),
+          `const a = import('./a');
+const b = import('./b');
+
+console.log('index.js', b);
+console.log(a);
+`,
+        );
+
+        event = await getNextBuildSuccess(b);
+        assertChangedAssets(event.changedAssets.size, 2);
+        assertTimesBundled(defaultBundlerSpy.callCount, 2);
+
+        let output = await overlayFS.readFile(
+          path.join(fixture, 'index.js'),
+          'utf8',
+        );
+        assert(output.includes(`console.log('index.js', b);`));
+      } finally {
+        if (subscription) {
+          await subscription.unsubscribe();
+          subscription = null;
+        }
+      }
+    });
+
+    it('removing a dependency', async () => {
+      let subscription;
+      let fixture = path.join(__dirname, '/integration/incremental-bundling');
+      try {
+        let b = bundler(path.join(fixture, 'index.js'), {
+          inputFS: overlayFS,
+          shouldDisableCache: false,
+        });
+
+        await overlayFS.mkdirp(fixture);
+        subscription = await b.watch();
+
+        let event = await getNextBuildSuccess(b);
+        assertTimesBundled(defaultBundlerSpy.callCount, 1);
+
+        await overlayFS.writeFile(
+          path.join(fixture, 'index.js'),
+          `// const a = import('./a');
+
+console.log('index.js');`,
+        );
+
+        event = await getNextBuildSuccess(b);
+        assertChangedAssets(event.changedAssets.size, 1);
+        assertTimesBundled(defaultBundlerSpy.callCount, 2);
+
+        let output = await overlayFS.readFile(
+          path.join(fixture, 'index.js'),
+          'utf8',
+        );
+        assert(output.includes(`// const a = import('./a')`));
+      } finally {
+        if (subscription) {
+          await subscription.unsubscribe();
+          subscription = null;
+        }
+      }
+    });
   });
 
   describe('other changes that would for a re-bundle', () => {
-    it('changing the bundler in parcel configs');
-    it('changing bundler options');
+    it('changing the bundler in parcel configs', async () => {
+      let subscription;
+      let fixture = path.join(__dirname, '/integration/incremental-bundling');
+      try {
+        let b = bundler(path.join(fixture, 'index.js'), {
+          inputFS: overlayFS,
+          shouldDisableCache: false,
+        });
+
+        await overlayFS.mkdirp(fixture);
+        subscription = await b.watch();
+
+        let event = await getNextBuildSuccess(b);
+        assertTimesBundled(defaultBundlerSpy.callCount, 1);
+        assertTimesBundled(customBundlerSpy.callCount, 0);
+
+        await overlayFS.writeFile(
+          path.join(fixture, '.parcelrc'),
+          JSON.stringify({
+            extends: '@parcel/config-default',
+            bundler: 'parcel-bundler-test',
+          }),
+        );
+
+        event = await getNextBuildSuccess(b);
+
+        // should contain all the assets
+        assertChangedAssets(event.changedAssets.size, 3);
+        // the default bundler was only called once
+        assertTimesBundled(defaultBundlerSpy.callCount, 1);
+        // calls the new bundler to rebundle
+        assertTimesBundled(customBundlerSpy.callCount, 1);
+
+        let output = await overlayFS.readFile(
+          path.join(fixture, 'index.js'),
+          'utf8',
+        );
+        assert(output.includes(`const a = import('./a')`));
+      } finally {
+        if (subscription) {
+          await subscription.unsubscribe();
+          subscription = null;
+        }
+      }
+    });
+
+    it('changing bundler options', async () => {
+      let subscription;
+      let fixture = path.join(__dirname, '/integration/incremental-bundling');
+      try {
+        let b = bundler(path.join(fixture, 'index.js'), {
+          inputFS: overlayFS,
+          shouldDisableCache: false,
+        });
+
+        await overlayFS.mkdirp(fixture);
+        subscription = await b.watch();
+
+        let event = await getNextBuildSuccess(b);
+        assertTimesBundled(defaultBundlerSpy.callCount, 1);
+
+        let pkgFile = path.join(fixture, 'package.json');
+        let pkg = JSON.parse(await overlayFS.readFile(pkgFile));
+        await overlayFS.writeFile(
+          pkgFile,
+          JSON.stringify({
+            ...pkg,
+            '@parcel/bundler-default': {
+              http: 1,
+            },
+          }),
+        );
+
+        event = await getNextBuildSuccess(b);
+
+        // should contain all the assets
+        assertChangedAssets(event.changedAssets.size, 3);
+        assertTimesBundled(defaultBundlerSpy.callCount, 2);
+
+        let output = await overlayFS.readFile(
+          path.join(fixture, 'index.js'),
+          'utf8',
+        );
+        assert(output.includes(`const a = import('./a')`));
+      } finally {
+        if (subscription) {
+          await subscription.unsubscribe();
+          subscription = null;
+        }
+      }
+    });
   });
 });
