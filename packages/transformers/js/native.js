9fe5d8a62e5b691e3ca64c4a5fc33b180eb5f231
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-06-03 16:45:12 +0200

    Switch to serde_wasm_bindgen

--- packages/transformers/js/native.js
+++ packages/transformers/js/native.js
@@ -15,6 +15,40 @@ if (process.platform === 'linux') {
 let name = `parcel-swc.${parts.join('-')}.node`;
 if (process.env.PARCEL_BUILD_ENV === 'production') {
   module.exports = require(`./${name}`);
+} else if (process.env.PARCEL_SWC_WASM) {
+  const {transform} = require('./wasm/dist-node/parcel_js_swc_wasm.js');
+
+  module.exports.transform = function(config) {
+    let result = transform(config);
+    return {
+      ...result,
+      // Hydrate Uint8Array into Buffer
+      code: Buffer.from(result.code.buffer),
+      // https://github.com/cloudflare/serde-wasm-bindgen/issues/10
+      dependencies: result.dependencies?.map(d => ({
+        ...d,
+        attributes:
+          d.attributes != null
+            ? Object.fromEntries([...d.attributes])
+            : undefined,
+      })),
+      hoist_result:
+        result.hoist_result != null
+          ? {
+              ...result.hoist_result,
+              imported_symbols: Object.fromEntries([
+                ...result.hoist_result.imported_symbols,
+              ]),
+              exported_symbols: Object.fromEntries([
+                ...result.hoist_result.exported_symbols,
+              ]),
+              dynamic_imports: Object.fromEntries([
+                ...result.hoist_result.dynamic_imports,
+              ]),
+            }
+          : undefined,
+    };
+  };
 } else if (
   require('fs').existsSync(require('path').join(__dirname, '..', name))
 ) {
--- packages/transformers/js/wasm/src/lib.rs
+++ packages/transformers/js/wasm/src/lib.rs
@@ -1,14 +1,14 @@
 extern crate parcel_js_swc_core;
 
 use js_sys::Error;
+use serde_wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn transform(config_val: JsValue) -> Result<JsValue, JsValue> {
-  let config = config_val
-    .into_serde()
-    .map_err(|e| Error::from(JsValue::from_str(&e.to_string())))?;
+  let config: parcel_js_swc_core::Config =
+    serde_wasm_bindgen::from_value(config_val).map_err(|err| JsValue::from(err))?;
   let result = parcel_js_swc_core::transform(config)
     .map_err(|e| Error::from(JsValue::from_str(&e.to_string())))?;
-  Ok(JsValue::from_serde(&result).unwrap())
+  Ok(serde_wasm_bindgen::to_value(&result).map_err(|err| JsValue::from(err))?)
 }
