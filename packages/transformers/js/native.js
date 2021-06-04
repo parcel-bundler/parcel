029e147c03273b610f410f195bd32524edd2b581
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-06-05 00:10:45 +0200

    swc wasm: convert HashMap to objects directly

--- packages/transformers/js/native.js
+++ packages/transformers/js/native.js
@@ -24,29 +24,6 @@ if (process.env.PARCEL_BUILD_ENV === 'production') {
       ...result,
       // Hydrate Uint8Array into Buffer
       code: Buffer.from(result.code.buffer),
-      // https://github.com/cloudflare/serde-wasm-bindgen/issues/10
-      dependencies: result.dependencies?.map(d => ({
-        ...d,
-        attributes:
-          d.attributes != null
-            ? Object.fromEntries([...d.attributes])
-            : undefined,
-      })),
-      hoist_result:
-        result.hoist_result != null
-          ? {
-              ...result.hoist_result,
-              imported_symbols: Object.fromEntries([
-                ...result.hoist_result.imported_symbols,
-              ]),
-              exported_symbols: Object.fromEntries([
-                ...result.hoist_result.exported_symbols,
-              ]),
-              dynamic_imports: Object.fromEntries([
-                ...result.hoist_result.dynamic_imports,
-              ]),
-            }
-          : undefined,
     };
   };
 } else if (require('fs').existsSync(require('path').join(__dirname, name))) {
