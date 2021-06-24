7eefd58f01cd449393baea42da0e5332cb0c6f42
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-06-24 21:48:03 +0200

    Fixup Flow in tests

--- packages/core/core/test/ParcelConfig.test.js
+++ packages/core/core/test/ParcelConfig.test.js
@@ -139,7 +139,7 @@ describe('ParcelConfig', () => {
         DEFAULT_OPTIONS,
       );
 
-      sinon.stub(logger, 'warn');
+      let warnStub = sinon.stub(logger, 'warn');
       let {plugin} = await config.loadPlugin({
         packageName: 'parcel-transformer-no-engines',
         resolveFrom: configFilePath,
@@ -147,16 +147,13 @@ describe('ParcelConfig', () => {
       });
       assert(plugin);
       assert.equal(typeof plugin.transform, 'function');
-      // $FlowFixMe[method-unbinding]
-      assert(logger.warn.calledOnce);
-      // $FlowFixMe[method-unbinding]
-      assert.deepEqual(logger.warn.getCall(0).args[0], {
+      assert(warnStub.calledOnce);
+      assert.deepEqual(warnStub.getCall(0).args[0], {
         origin: '@parcel/core',
         message:
           'The plugin "parcel-transformer-no-engines" needs to specify a `package.json#engines.parcel` field with the supported Parcel version range.',
       });
-      // $FlowFixMe[method-unbinding]
-      logger.warn.restore();
+      warnStub.restore();
     });
 
     it('should error if a plugin specifies an invalid engines.parcel field in package.json', async () => {
