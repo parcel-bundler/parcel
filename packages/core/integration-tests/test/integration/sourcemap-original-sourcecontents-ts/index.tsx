56eb17dbd027a223d5847b50643dfaf13e27b1ce
Gora Kong <15333808+gorakong@users.noreply.github.com>
2021-06-28 16:57:35 -0700

    Fix TypeScript and other sourcemaps by always creating an initial sourcemap (#6472)
    * convert existing test to typescript
    
    * add test for tsx and fix
    
    * fix test
    
    * add @babel/preset-typescript
    
    * remove yarn.lock
    
    * move assignment to asset.sourceContent
    
    * store sourceContent as a buffer
    
    * wip
    
    * Update for streams
    
    * Add test for large text file
    
    Co-authored-by: Will Binns-Smith <wbinnssmith@atlassian.com>

---
+++ packages/core/integration-tests/test/integration/sourcemap-original-sourcecontents-ts/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+type Props = {
+  bar: string;
+};
+
+function App(props: Props) {
+  return <div>{props.bar}</div>;
+}
+
+ReactDOM.render(<App bar="bar" />, document.getElementById("root"));
