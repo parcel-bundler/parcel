aaf0e237343eb1da40864f9cf843c0b86e35329f
Devon Govett <devongovett@gmail.com>
2021-07-05 14:32:59 -0700

    Bug fixes (#6541)

--- packages/optimizers/terser/src/TerserOptimizer.js
+++ packages/optimizers/terser/src/TerserOptimizer.js
@@ -3,30 +3,45 @@
 import nullthrows from 'nullthrows';
 import {minify} from 'terser';
 import {Optimizer} from '@parcel/plugin';
-import {blobToString, loadConfig} from '@parcel/utils';
+import {blobToString} from '@parcel/utils';
 import SourceMap from '@parcel/source-map';
 import ThrowableDiagnostic, {escapeMarkdown} from '@parcel/diagnostic';
 
 import path from 'path';
 
 export default (new Optimizer({
-  async optimize({contents, map, bundle, options, getSourceMapReference}) {
+  async loadConfig({config, options}) {
+    let userConfig = await config.getConfigFrom(
+      path.join(options.entryRoot, 'index'),
+      ['.terserrc', '.terserrc.js'],
+    );
+
+    if (userConfig) {
+      let isJavascript = path.extname(userConfig.filePath) === '.js';
+      if (isJavascript) {
+        config.invalidateOnStartup();
+      }
+    }
+
+    return userConfig?.contents;
+  },
+  async optimize({
+    contents,
+    map,
+    bundle,
+    config: userConfig,
+    options,
+    getSourceMapReference,
+  }) {
     if (!bundle.env.shouldOptimize) {
       return {contents, map};
     }
 
     let code = await blobToString(contents);
 
-    let userConfig = await loadConfig(
-      options.inputFS,
-      path.join(options.entryRoot, 'index'),
-      ['.terserrc', '.uglifyrc', '.uglifyrc.js', '.terserrc.js'],
-      options.projectRoot,
-    );
-
     let originalMap = map ? await map.stringify({}) : null;
     let config = {
-      ...userConfig?.config,
+      ...userConfig,
       sourceMap: bundle.env.sourceMap
         ? {
             filename: path.relative(
