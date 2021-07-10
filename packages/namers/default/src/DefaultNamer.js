81293fc84517f756b593a73d94da4e2630339140
Devon Govett <devongovett@gmail.com>
2021-07-10 11:07:16 -0700

    Fix bugs with web manifests (#6573)

--- packages/namers/default/src/DefaultNamer.js
+++ packages/namers/default/src/DefaultNamer.js
@@ -17,6 +17,7 @@ export default (new Namer({
   name({bundle, bundleGraph, options}) {
     let bundleGroup = bundleGraph.getBundleGroupsContainingBundle(bundle)[0];
     let bundleGroupBundles = bundleGraph.getBundlesInBundleGroup(bundleGroup);
+    let isEntry = bundleGraph.isEntryBundleGroup(bundleGroup);
 
     if (bundle.needsStableName) {
       let entryBundlesOfType = bundleGroupBundles.filter(
@@ -37,7 +38,7 @@ export default (new Namer({
 
     if (
       bundle.id === mainBundle.id &&
-      bundle.needsStableName &&
+      isEntry &&
       bundle.target &&
       bundle.target.distEntry != null
     ) {
@@ -88,6 +89,7 @@ export default (new Namer({
     //      `index.css`.
     let name = nameFromContent(
       mainBundle,
+      isEntry,
       bundleGroup.entryAssetId,
       options.entryRoot,
     );
@@ -101,6 +103,7 @@ export default (new Namer({
 
 function nameFromContent(
   bundle: Bundle,
+  isEntry: boolean,
   entryAssetId: string,
   entryRoot: FilePath,
 ): string {
@@ -112,7 +115,7 @@ function nameFromContent(
   // If this is an entry bundle, use the original relative path.
   if (bundle.needsStableName) {
     // Match name of target entry if possible, but with a different extension.
-    if (bundle.target.distEntry != null) {
+    if (isEntry && bundle.target.distEntry != null) {
       return basenameWithoutExtension(bundle.target.distEntry);
     }
 
