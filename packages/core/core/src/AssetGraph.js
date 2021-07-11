fc80597bb95056829a5b05c9142ffeded67f38c8
Devon Govett <devongovett@gmail.com>
2021-07-11 10:57:45 -0700

    Fix inline scripts inside an HTML file imported with bundle-text (#6574)

--- packages/core/core/src/AssetGraph.js
+++ packages/core/core/src/AssetGraph.js
@@ -425,35 +425,46 @@ export default class AssetGraph extends ContentGraph<AssetGraphNode> {
     invariant(assetGroupNode.type === 'asset_group');
     assetGroupNode.correspondingRequest = correspondingRequest;
 
-    let dependentAssetKeys = [];
+    let assetsByKey = new Map();
+    for (let asset of assets) {
+      if (asset.uniqueKey != null) {
+        assetsByKey.set(asset.uniqueKey, asset);
+      }
+    }
+
+    let dependentAssetKeys = new Set();
+    for (let asset of assets) {
+      for (let dep of asset.dependencies.values()) {
+        if (assetsByKey.has(dep.specifier)) {
+          dependentAssetKeys.add(dep.specifier);
+        }
+      }
+    }
+
     let assetObjects: Array<{|
-      assetNode: AssetNode,
+      assetNodeId: NodeId,
       dependentAssets: Array<Asset>,
-      isDirect: boolean,
     |}> = [];
+    let assetNodeIds = [];
     for (let asset of assets) {
       this.normalizeEnvironment(asset);
-      let isDirect = !dependentAssetKeys.includes(asset.uniqueKey);
+      let isDirect = !dependentAssetKeys.has(asset.uniqueKey);
 
       let dependentAssets = [];
       for (let dep of asset.dependencies.values()) {
-        let dependentAsset = assets.find(a => a.uniqueKey === dep.specifier);
+        let dependentAsset = assetsByKey.get(dep.specifier);
         if (dependentAsset) {
-          dependentAssetKeys.push(dependentAsset.uniqueKey);
           dependentAssets.push(dependentAsset);
         }
       }
+      let id = this.addNode(nodeFromAsset(asset));
       assetObjects.push({
-        assetNode: nodeFromAsset(asset),
+        assetNodeId: id,
         dependentAssets,
-        isDirect,
       });
-    }
 
-    const assetNodeIds = [];
-    for (let {assetNode, isDirect} of assetObjects) {
       if (isDirect) {
-        assetNodeIds.push(this.addNode(assetNode));
+        assetNodeIds.push(id);
       }
     }
 
@@ -461,10 +472,10 @@ export default class AssetGraph extends ContentGraph<AssetGraphNode> {
       this.getNodeIdByContentKey(assetGroupNode.id),
       assetNodeIds,
     );
-    for (let {assetNode, dependentAssets} of assetObjects) {
+    for (let {assetNodeId, dependentAssets} of assetObjects) {
       // replaceNodesConnectedTo has merged the value into the existing node, retrieve
       // the actual current node.
-      assetNode = nullthrows(this.getNodeByContentKey(assetNode.id));
+      let assetNode = nullthrows(this.getNode(assetNodeId));
       invariant(assetNode.type === 'asset');
       this.resolveAsset(assetNode, dependentAssets);
     }
