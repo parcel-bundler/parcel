733cc42b1a8c049c0f9d2f25816290c7bd2861ab
Devon Govett <devongovett@gmail.com>
2021-08-22 13:33:57 -0700

    Add support for more types of dependencies in SVG

--- packages/core/integration-tests/test/svg.js
+++ packages/core/integration-tests/test/svg.js
@@ -23,7 +23,58 @@ describe('svg', function() {
         name: 'other2.html',
         assets: ['other2.html'],
       },
+      {
+        type: 'svg',
+        assets: ['path.svg'],
+      },
+      {
+        type: 'svg',
+        assets: ['gradient.svg'],
+      },
+      {
+        type: 'js',
+        assets: ['script.js'],
+      },
+      {
+        type: 'js',
+        assets: ['module.js', 'script.js'],
+      },
     ]);
+
+    let file = await outputFS.readFile(
+      b.getBundles().find(b => b.type === 'svg').filePath,
+      'utf-8',
+    );
+    assert(file.includes('<a href="/other1.html">'));
+    assert(file.includes('<use href="#circle"'));
+    assert(
+      file.includes(
+        `<use xlink:href="/${path.basename(
+          b.getBundles().find(b => b.name.startsWith('square')).filePath,
+        )}#square"`,
+      ),
+    );
+    assert(
+      file.includes(
+        `fill="url('/${path.basename(
+          b.getBundles().find(b => b.name.startsWith('gradient')).filePath,
+        )}#myGradient')"`,
+      ),
+    );
+    assert(
+      file.includes(
+        `<script xlink:href="/${path.basename(
+          b.getBundles().find(b => b.name.startsWith('script')).filePath,
+        )}"`,
+      ),
+    );
+    assert(
+      file.includes(
+        `<script href="/${path.basename(
+          b.getBundles().find(b => b.name.startsWith('module')).filePath,
+        )}"`,
+      ),
+    );
   });
 
   it('should minify SVG bundles', async function() {
