cc487e3b24f8d920cb4c73c66b785dc04c495869
Devon Govett <devongovett@gmail.com>
2021-09-30 10:56:29 -0700

    Use package manager to load sharp on main thread

--- packages/transformers/image/src/ImageTransformer.js
+++ packages/transformers/image/src/ImageTransformer.js
@@ -3,6 +3,7 @@ import {validateConfig} from './validateConfig';
 import {Transformer} from '@parcel/plugin';
 import nullthrows from 'nullthrows';
 import WorkerFarm from '@parcel/workers';
+import loadSharp from './loadSharp';
 
 // from https://github.com/lovell/sharp/blob/df7b8ba73808fc494be413e88cfb621b6279218c/lib/output.js#L6-L17
 const FORMATS = new Map([
@@ -16,8 +17,6 @@ const FORMATS = new Map([
   ['heic', 'heif'],
   ['heif', 'heif'],
 ]);
-
-const SHARP_RANGE = '^0.29.1';
 
 let isSharpLoadedOnMainThread = false;
 
@@ -70,20 +69,22 @@ export default (new Transformer({
         let api = WorkerFarm.getWorkerApi();
         await api.callMaster({
           location: __dirname + '/loadSharp.js',
-          args: [],
+          args: [
+            options.packageManager,
+            asset.filePath,
+            options.shouldAutoInstall,
+          ],
         });
 
         isSharpLoadedOnMainThread = true;
       }
 
       let inputBuffer = await asset.getBuffer();
-      let sharp = await options.packageManager.require(
-        'sharp',
+      let sharp = await loadSharp(
+        options.packageManager,
         asset.filePath,
-        {
-          range: SHARP_RANGE,
-          shouldAutoInstall: options.shouldAutoInstall,
-        },
+        options.shouldAutoInstall,
+        true,
       );
 
       let imagePipeline = sharp(inputBuffer);
