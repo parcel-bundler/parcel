de5894c3ad06ad397b4bfad5aeaf4da7990cf49a
thychamp <thychamp@users.noreply.github.com>
2021-10-27 21:32:15 -0500

    test progress

--- packages/core/integration-tests/test/global-var.js
+++ packages/core/integration-tests/test/global-var.js
@@ -1,14 +1,28 @@
 import assert from 'assert';
 import path from 'path';
-import {bundle, run} from '@parcel/test-utils';
+import {bundle} from '@parcel/test-utils';
+import {DevPackager} from '../../../packagers/js/src/DevPackager';
 
 describe('global-var', function() {
   it('should product a global var', async function() {
     let b = await bundle(
       path.join(__dirname, '/integration/global-var/index.js'),
     );
-    let output = (await run(b)).default;
-    assert.equal(output.name, 'Test Module');
-    assert.equal(output.mount(), 'Hello World');
+    const packager = new DevPackager(
+      {
+        projectRoot: '',
+        globalVar: 'hello-world',
+      },
+      b,
+      b.getBundles()[0],
+      'aRequiredName',
+    );
+    let output = await packager.package();
+    assert.equal(
+      output.contents.includes(
+        '},{}]},["ePXF2"], "ePXF2", "aRequiredName", "hello-world")',
+      ),
+      true,
+    );
   });
 });
