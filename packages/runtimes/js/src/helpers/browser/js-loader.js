5c5f04cf5f88de548c414b51e250a9b2c2fa5bda
mrkldshv <markladyshev@gmail.com>
2021-11-09 09:30:56 +0000

    fix: add error message

--- packages/runtimes/js/src/helpers/browser/js-loader.js
+++ packages/runtimes/js/src/helpers/browser/js-loader.js
@@ -1,10 +1,10 @@
 const cacheLoader = require('../cacheLoader');
 
 module.exports = cacheLoader(function loadJSBundle(bundle) {
-  return new Promise(function (resolve, reject) {
+  return new Promise(function(resolve, reject) {
     // Don't insert the same script twice (e.g. if it was already in the HTML)
     let existingScripts = document.getElementsByTagName('script');
-    let isCurrentBundle = function (script) {
+    let isCurrentBundle = function(script) {
       return script.src === bundle;
     };
 
@@ -20,14 +20,14 @@ module.exports = cacheLoader(function loadJSBundle(bundle) {
     script.src = bundle;
     script.onerror = function(e) {
       var error = new TypeError(
-        `Failed to fetch dynamically imported module: ${bundle}.`,
+        `Failed to fetch dynamically imported module: ${bundle}. Error: ${e.message}`,
       );
       script.onerror = script.onload = null;
       script.remove();
       reject(error);
     };
 
-    script.onload = function () {
+    script.onload = function() {
       script.onerror = script.onload = null;
       resolve();
     };
