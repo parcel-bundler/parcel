931ecd56df70ca172825d7befad6cda3787b9bf7
Shinobu Hayashi <PhilisPaxil@gmail.com>
2021-12-20 06:46:37 +0900

    Fix: transpiling React.createElement into Preact h behavior (#7435)

--- packages/core/integration-tests/test/transpilation.js
+++ packages/core/integration-tests/test/transpilation.js
@@ -183,6 +183,19 @@ describe('transpilation', function () {
     assert(file.includes('_jsxRuntime.jsx("div"'));
   });
 
+  it('should support the automatic JSX runtime with preact with alias', async function () {
+    let b = await bundle(
+      path.join(
+        __dirname,
+        '/integration/jsx-automatic-preact-with-alias/index.js',
+      ),
+    );
+
+    let file = await outputFS.readFile(b.getBundles()[0].filePath, 'utf8');
+    assert(/\Wreact\/jsx-runtime\W/.test(file));
+    assert(file.includes('_jsxRuntime.jsx("div"'));
+  });
+
   it('should support the automatic JSX runtime with explicit tsconfig.json', async function () {
     let b = await bundle(
       path.join(__dirname, '/integration/jsx-automatic-tsconfig/index.js'),
