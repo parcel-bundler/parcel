7857d8f593253b77bdee10a1ce54499885c25b79
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2022-01-03 21:28:58 +0100

    Add test

---
+++ packages/core/integration-tests/test/integration/react-refresh-automatic/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import Foo from "./Foo";
+import { act } from "react-dom/test-utils";
+
+export const App = () => {
+  const [x] = useState(Math.random());
+
+  return (
+    <div>
+      {x} <Foo />
+    </div>
+  );
+};
