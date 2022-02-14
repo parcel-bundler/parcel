a019869d066bac6c7ac3cda8ac5cb59484f4dfb0
Shinyaigeek <Philispaxil@gmail.com>
2022-02-15 03:29:06 +0900

    Chore: add integration test for the case svg-react with jsx pipeline

--- packages/core/integration-tests/test/svg-react.js
+++ packages/core/integration-tests/test/svg-react.js
@@ -19,4 +19,21 @@ describe('svg-react', function () {
     assert(file.includes('const SvgIcon ='));
     assert(file.includes('_react.createElement("svg"'));
   });
+
+  it('should support transforming SVGs to react components with jsx pipeline', async function () {
+    let b = await bundle(
+      path.join(__dirname, '/integration/svg-react-with-jsx-pipeline/react.js'),
+      {
+        defaultConfig: path.join(
+          __dirname,
+          'integration/custom-configs/.parcelrc-svg-react-with-jsx-pipeline',
+        ),
+      },
+    );
+
+    let file = await outputFS.readFile(b.getBundles()[0].filePath, 'utf-8');
+    assert(!file.includes('inkscape'));
+    assert(file.includes('const SvgIcon ='));
+    assert(file.includes('_react.createElement("svg"'));
+  });
 });
