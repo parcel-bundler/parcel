e10c41be6cd81de28f6510e382f7c86417c554e1
Noa <coolreader18@gmail.com>
2022-02-21 12:33:09 -0600

    Deduplicate getComponentName, svgr has the same function

--- packages/transformers/svg-react/src/SvgReactTransformer.js
+++ packages/transformers/svg-react/src/SvgReactTransformer.js
@@ -2,39 +2,27 @@
 
 import {Transformer} from '@parcel/plugin';
 
-import path from 'path';
-import camelcase from 'camelcase';
 import svgoPlugin from '@svgr/plugin-svgo';
 import jsxPlugin from '@svgr/plugin-jsx';
 import {transform} from '@svgr/core';
 
-function getComponentName(filePath) {
-  let validCharacters = /[^a-zA-Z0-9_-]/g;
-  let name = path.parse(filePath).name.replace(validCharacters, '');
-  return camelcase(name, {
-    pascalCase: true,
-  });
-}
-
 export default (new Transformer({
   async loadConfig({config}) {
-    let { contents } = await config.getConfig(['.svgrrc', '.svgrrc.json']);
-    return contents;
+    let result = await config.getConfig(['.svgrrc', '.svgrrc.json']);
+    return result?.contents ?? {};
   },
 
-  async transform({asset,config}) {
+  async transform({asset, config}) {
     let code = await asset.getCode();
-    let componentName = getComponentName(asset.filePath);
 
     const jsx = await transform(
       code,
-      { ...config, runtimeConfig: false },
+      {...config, runtimeConfig: false},
       {
         caller: {
           name: '@parcel/transformer-svg-react',
           defaultPlugins: [svgoPlugin, jsxPlugin],
         },
-        componentName,
         filePath: asset.filePath,
       },
     );
