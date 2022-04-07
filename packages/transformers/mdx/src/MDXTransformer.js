a4d209c2b1faf2e1d988ce0f24e4e7a395bed47a
Jake Goulding <jake.goulding@gmail.com>
2022-04-07 14:54:01 -0400

    Upgrade to MDX 2

--- packages/transformers/mdx/src/MDXTransformer.js
+++ packages/transformers/mdx/src/MDXTransformer.js
@@ -1,19 +1,18 @@
 // @flow
 import {Transformer} from '@parcel/plugin';
-import mdx from '@mdx-js/mdx';
 
 export default (new Transformer({
   async transform({asset}) {
+    let { compile } = await import('@mdx-js/mdx');
     let code = await asset.getCode();
-    let compiled = await mdx(code);
 
-    asset.type = 'js';
-    asset.setCode(`/* @jsxRuntime classic */
-/* @jsx mdx */
-import React from 'react';
-import { mdx } from '@mdx-js/react'
-${compiled}
-`);
+    try {
+      let compiled = await compile(code);
+      asset.type = 'js';
+      asset.setCode(compiled.value);
+    } catch (e) {
+      throw e.toString(); // Adds the line and column number of errors
+    }
 
     return [asset];
   },
