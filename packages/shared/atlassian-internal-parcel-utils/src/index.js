25e186b8207aae1f8a040ca890f99c51eb3ee10e
Will Binns-Smith <wbinnssmith@atlassian.com>
2022-05-02 15:59:28 -0700

    Sentry: sanitize paths in traces and send username

--- packages/shared/atlassian-internal-parcel-utils/src/index.js
+++ packages/shared/atlassian-internal-parcel-utils/src/index.js
@@ -1,6 +1,15 @@
 // @flow strict-local
 
+import type {FilePath} from '@parcel/types';
+
 import assert from 'assert';
+import os from 'os';
+import path from 'path';
+
+const PATH_TO_NODE_MODULES = path.resolve(
+  __dirname,
+  process.env.PARCEL_BUILD_ENV === 'production' ? '../../..' : '../../../..',
+);
 
 interface ISentry {
   captureException(mixed): void;
@@ -43,8 +52,40 @@ export function getSentry(): ISentry {
       dsn: process.env.SENTRY_DSN,
       release:
         process.env.BITBUCKET_COMMIT /* Parcel repo commit on Bitbucket */,
+      beforeSend(event, hint) {
+        for (const error of event.exception.values) {
+          // Sanitize any referenced UNIX-style paths in the main error message
+          error.value = error.value.replace(/\w?(\/.*)\b/g, p =>
+            sanitizePath(p),
+          );
+          for (const frame of error.stacktrace.frames) {
+            frame.filename = sanitizePath(frame.filename);
+          }
+        }
+        return event;
+      },
     });
+    Sentry.setUser({username: os.userInfo().username});
   }
 
   return Sentry;
+}
+
+function sanitizePath(filePath: FilePath): FilePath {
+  if (!path.isAbsolute(filePath)) {
+    // If the path isn't absolute (e.g. previously sanitized, node internals
+    // beginning with internal/, etc), this is safe to skip.
+    return filePath;
+  }
+
+  const nodeModulesPos = filePath.indexOf(PATH_TO_NODE_MODULES);
+  if (nodeModulesPos >= 0) {
+    return (
+      '[NODE_MODULES]' +
+      filePath.slice(nodeModulesPos + PATH_TO_NODE_MODULES.length)
+    );
+  }
+
+  // If node_modules isn't in this path, just use the basename
+  return '[UNKNOWN]' + path.sep + path.basename(filePath);
 }
