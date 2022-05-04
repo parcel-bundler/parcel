9ff055fd3befcfcbb569678e156874140419b020
Eric Eldredge <lettertwo@gmail.com>
2022-05-04 18:59:51 -0400

    Add post-merge hook to update nightly versions

--- scripts/update-self-published.js
+++ scripts/update-self-published.js
@@ -8,6 +8,31 @@ const exec = require('child_process').execSync;
 const UPSTREAM = /github.+parcel-bundler\/parcel/;
 const NIGHTLY = /.*-nightly\..*/;
 
+if (process.argv.includes('-h') || process.argv.includes('--help')) {
+  console.log(
+    [
+      `  Usage: ${path.basename(process.argv[1])} [opts]`,
+      '',
+      '  Options:',
+      '    -h, --help       Show help',
+      '    -a               Add updated files to git index',
+      '                     (handy in a precommit hook)',
+      '',
+      '  Looks for self-published packages (e.g., @parcel/transformer-js),',
+      '  and compares their nightly version numbers to the list',
+      '  of published nightly versions (via `yarn info`).',
+      '  If it finds a version that is newer, it updates the version',
+      '  in the package.json.',
+      '',
+      '  It will use the oldest nightly version that is newer than',
+      '  the latest common commit between HEAD and the upstream default branch.',
+    ].join('\n'),
+  );
+  process.exit();
+}
+
+const shouldStage = process.argv.includes('-a');
+
 const fromVersionString = str => str.match(/npm:.+@(.+)$/)[1];
 const toVersionString = (name, version) => `npm:${name}@${version}`;
 
@@ -43,22 +68,20 @@ function getDefaultBranchName(remote) {
  * with the timestamp closest to `time` without being younger.
  */
 function getNearestNightlyVersion(pkgName, time) {
-  let candidate;
+  let candidate = null;
   let info = run(`yarn info ${pkgName} --json`);
   let versions = [...Object.entries(info.data.time)].reverse();
   for (let [version, timestamp] of versions) {
     if (NIGHTLY.test(version)) {
       let versionTime = new Date(timestamp);
-      if (versionTime >= time) {
-        candidate = version;
-      } else {
-        break;
-      }
+      if (versionTime < time) break;
+      candidate = version;
     }
   }
-  if (candidate) return candidate;
-  throw new Error('Could not determine an appropriate nightly version!');
+  return candidate;
 }
+
+console.log(`Updating self-published (nightly) versions...`);
 
 let packages = run(
   `${path.join(__dirname, '..', 'node_modules', '.bin', 'lerna')} ls --json`,
@@ -67,7 +90,7 @@ let packages = run(
 // Fetch the default upstream branch...
 let upstream = getUpstreamRemoteName();
 let branch = getDefaultBranchName(upstream);
-run(`git fetch ${upstream} ${branch}`);
+run(`git fetch -q ${upstream} ${branch}`);
 // ...so we can determine the latest common ancestor commit.
 let baseRef = run(`git merge-base HEAD ${upstream}/${branch}`).split(/\s+/)[0];
 // Get the commit time of the latest common ancestor between HEAD and upstream.
@@ -79,12 +102,11 @@ for (let {location, name} of packages) {
   if (pkg.dependencies && 'self-published' in pkg.dependencies) {
     let current = fromVersionString(pkg.dependencies['self-published']);
     let nightly = getNearestNightlyVersion(name, baseRefTime);
-    if (current === nightly) {
-      console.log(`${name} is already on nearest nightly ${nightly}`);
-    } else {
+    if (nightly && current !== nightly) {
       console.log(`updating ${name} to nearest nightly ${nightly}`);
       pkg.dependencies['self-published'] = toVersionString(name, nightly);
       fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
+      if (shouldStage) run(`git add -u ${pkgPath}`);
     }
   }
 }
