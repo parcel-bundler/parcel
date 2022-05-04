8d65fd7696c6989d5e7a5f86e316326253db0c67
Eric Eldredge <lettertwo@gmail.com>
2022-05-04 14:19:02 -0400

    Extract run function

--- scripts/update-self-published.js
+++ scripts/update-self-published.js
@@ -8,11 +8,17 @@ const exec = require('child_process').execSync;
 const fromVersionString = str => str.match(/npm:.+@(.+)$/)[1];
 const toVersionString = (name, version) => `npm:${name}@${version}`;
 
-let packages = JSON.parse(
-  exec(
-    `${path.join(__dirname, '..', 'node_modules', '.bin', 'lerna')} ls --json`,
-    {stdio: [0, 'pipe', 2]},
-  ),
+function run(cmd) {
+  let result = exec(cmd, {stdio: [0, 'pipe', 2]});
+  try {
+    return JSON.parse(result);
+  } catch {
+    return result.toString();
+  }
+}
+
+let packages = run(
+  `${path.join(__dirname, '..', 'node_modules', '.bin', 'lerna')} ls --json`,
 );
 
 for (let {location, name} of packages) {
@@ -20,9 +26,7 @@ for (let {location, name} of packages) {
   let pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
   if (pkg.dependencies && 'self-published' in pkg.dependencies) {
     let current = fromVersionString(pkg.dependencies['self-published']);
-    let info = JSON.parse(
-      exec(`yarn info ${name} --json`, {stdio: [0, 'pipe', 2]}),
-    );
+    let info = run(`yarn info ${name} --json`);
     let nightly = info.data['dist-tags'].nightly;
     if (current === nightly) {
       console.error(`${name} is already on latest nightly ${nightly}`);
