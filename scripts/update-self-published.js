979193850cd123b334f44e015a07e7532dfca3d5
Eric Eldredge <lettertwo@gmail.com>
2022-05-24 15:52:01 -0400

    Update self-published versions for upstream merges only

--- scripts/update-self-published.js
+++ scripts/update-self-published.js
@@ -17,6 +17,8 @@ if (process.argv.includes('-h') || process.argv.includes('--help')) {
       '    -h, --help       Show help',
       '    -a               Add updated files to git index',
       '                     (handy in a precommit hook)',
+      '    -o               Only run if a merge with upstream is in progress',
+      '                     (handy in a precommit hook)',
       '',
       '  Looks for self-published packages (e.g., @parcel/transformer-js),',
       '  and compares their nightly version numbers to the list',
@@ -32,6 +34,7 @@ if (process.argv.includes('-h') || process.argv.includes('--help')) {
 }
 
 const shouldStage = process.argv.includes('-a');
+const upstreamMergeOnly = process.argv.includes('-o');
 
 const fromVersionString = str => str.match(/npm:.+@(.+)$/)[1];
 const toVersionString = (name, version) => `npm:${name}@${version}`;
@@ -72,6 +75,17 @@ function getDefaultBranchName(remote) {
   }
 }
 
+function getMergeHead() {
+  try {
+    return run(
+      `git name-rev --name-only --exclude=tags/* --no-undefined MERGE_HEAD`,
+    );
+  } catch (e) {
+    // noop
+  }
+  return null;
+}
+
 /**
  * Check nightly version timestamps for the given `pkgName`
  * in reverse chronological order, returning the version
@@ -93,19 +107,61 @@ function getNearestNightlyVersion(pkgName, time) {
 
 console.log(`Updating self-published (nightly) versions...`);
 
+let upstream = getUpstreamRemoteName();
+let branch;
+
+// When restricting to upstream merges only, we are interested in two cases:
+//   1. A successful fast-forward or automatic merge with an upstream branch.
+//      In this case, we expect to run in a postmerge hook, so the reflog
+//      will show something like `merge refs/remotes/<remote>/<branch>:`.
+//   2. A merge with an upstream branch that has conflicts. In this case,
+//      we expect to run in a precommit hook, so the reflog won't reflect
+//      the merge yet. Instead, we will look for the MERGE_HEAD to see
+//      if this is a merge with an upstream branch.
+if (upstreamMergeOnly) {
+  // Check for MERGE_HEAD first, since it's possible that the reflog could show
+  // a previous merge while we are commiting a newer merge with conflicts,
+  // and if we checked the reflog first, we'd mistakenly assume
+  // that it is the merge we're interested in.
+  let mergeHead = getMergeHead();
+  if (mergeHead) {
+    if (!mergeHead.includes(`${upstream}/`)) {
+      console.log('Not an upstream merge; skipping self-published update.');
+      process.exit(1);
+    }
+    branch = mergeHead.split(`${upstream}/`).pop();
+  } else {
+    let reflogSubject = run(`git reflog -1 --format=%gs`).trim();
+    if (reflogSubject.startsWith('merge')) {
+      // In this case, the merged branch is described in the reflog subject,
+      // so we can simply check if includes the upstream remote name.
+      if (!reflogSubject.includes(`refs/remotes/${upstream}/`)) {
+        console.log('Not an upstream merge; skipping self-published update.');
+        process.exit(1);
+      }
+      branch = reflogSubject.split(`refs/remotes/${upstream}/`).pop();
+    } else {
+      console.log('No merge in progress; skipping self-published update.');
+      process.exit(1);
+    }
+  }
+} else {
+  // Fetch the default upstream branch...
+  branch = getDefaultBranchName(upstream);
+  run(`git fetch -q ${upstream} ${branch}`);
+}
+
 let packages = run(
   `${path.join(__dirname, '..', 'node_modules', '.bin', 'lerna')} ls --json`,
 );
 
-// Fetch the default upstream branch...
-let upstream = getUpstreamRemoteName();
-let branch = getDefaultBranchName(upstream);
-run(`git fetch -q ${upstream} ${branch}`);
-// ...so we can determine the latest common ancestor commit.
+// Determine the latest common ancestor commit.
 let baseRef = run(`git merge-base HEAD ${upstream}/${branch}`).split(/\s+/)[0];
 // Get the commit time of the latest common ancestor between HEAD and upstream.
 let baseRefTime = new Date(run(`git show -s --format=%cI ${baseRef}`));
 
+// This script should exit with an error if no versions were updated.
+let updated = 0;
 for (let {location, name} of packages) {
   let pkgPath = path.join(location, 'package.json');
   let pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
@@ -117,6 +173,8 @@ for (let {location, name} of packages) {
       pkg.dependencies['self-published'] = toVersionString(name, nightly);
       fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
       if (shouldStage) run(`git add -u ${pkgPath}`);
+      updated++;
     }
   }
 }
+process.exit(updated ? 0 : 1);
