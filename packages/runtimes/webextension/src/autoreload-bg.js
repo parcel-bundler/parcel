8f56bd89205c107dc879e0944ec26a18a444ac6a
101arrowz <arjunbarrett@gmail.com>
2022-05-29 14:01:43 -0700

    update runtimes

--- packages/runtimes/webextension/src/autoreload-bg.js
+++ packages/runtimes/webextension/src/autoreload-bg.js
@@ -1,10 +1,10 @@
 /* global chrome, browser, addEventListener, fetch, Response, HMR_HOST, HMR_PORT */
 var env = typeof chrome == 'undefined' ? browser : chrome;
-env.runtime.onMessage.addListener(function (msg) {
-  if (msg.__parcel_hmr_reload__) {
-    env.runtime.reload();
-  }
-});
+var origReload = env.runtime.reload;
+env.runtime.reload = function () {
+  env.runtime.sendMessage({__parcel_hmr_reload__: true});
+  origReload.call(env.runtime);
+};
 
 if (env.runtime.getManifest().manifest_version == 3) {
   var proxyLoc = env.runtime.getURL('/__parcel_hmr_proxy__?url=');
@@ -14,14 +14,13 @@ if (env.runtime.getManifest().manifest_version == 3) {
       url = new URL(decodeURIComponent(url.slice(proxyLoc.length)));
       if (url.hostname == HMR_HOST && url.port == HMR_PORT) {
         evt.respondWith(
-          fetch(url).then(
-            res =>
-              new Response(res.body, {
-                headers: {
-                  'Content-Type': res.headers.get('Content-Type'),
-                },
-              }),
-          ),
+          fetch(url).then(function (res) {
+            return new Response(res.body, {
+              headers: {
+                'Content-Type': res.headers.get('Content-Type'),
+              },
+            });
+          }),
         );
       }
     }
--- packages/transformers/webextension/src/runtime/autoreload.js
+++ packages/transformers/webextension/src/runtime/autoreload.js
@@ -1,11 +1,10 @@
-/* global chrome, browser, addEventListener */
+/* global chrome, browser */
 var env = typeof chrome == 'undefined' ? browser : chrome;
-addEventListener('beforeunload', function () {
-  try {
-    env.runtime.sendMessage({
-      __parcel_hmr_reload__: true,
-    });
-  } catch (err) {
-    // ignore throwing if extension context invalidated
+env.runtime.onMessage.addListener(function (msg) {
+  if (msg.__parcel_hmr_reload__) {
+    // Wait for background to reload
+    setTimeout(function () {
+      location.reload();
+    }, 500);
   }
 });
