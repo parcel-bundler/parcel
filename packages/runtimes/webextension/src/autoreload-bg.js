c1910570c00f430a55a63777f2a53484e9710574
101arrowz <arjunbarrett@gmail.com>
2022-05-29 14:39:13 -0700

    fix runtimes again

--- packages/runtimes/webextension/src/autoreload-bg.js
+++ packages/runtimes/webextension/src/autoreload-bg.js
@@ -1,10 +1,34 @@
 /* global chrome, browser, addEventListener, fetch, Response, HMR_HOST, HMR_PORT */
 var env = typeof chrome == 'undefined' ? browser : chrome;
+var autoReload = false;
+
 var origReload = env.runtime.reload;
 env.runtime.reload = function () {
-  env.runtime.sendMessage({__parcel_hmr_reload__: true});
-  origReload.call(env.runtime);
+  var reloadOnce = false;
+  function reloadTabs(tabs) {
+    for (let i = 0; i < tabs.length; ++i) {
+      env.tabs.sendMessage(
+        tabs[i].id,
+        {__parcel_hmr_reload__: true},
+        {},
+        function () {},
+      );
+    }
+    if (reloadOnce) {
+      origReload.call(env.runtime);
+    } else {
+      reloadOnce = true;
+    }
+  }
+  env.tabs.query({highlighted: true}, reloadTabs);
+  env.tabs.query({highlighted: false}, reloadTabs);
 };
+
+env.runtime.onMessage.addListener(function (msg) {
+  if (msg.__parcel_hmr_reload__) {
+    env.runtime.reload();
+  }
+});
 
 if (env.runtime.getManifest().manifest_version == 3) {
   var proxyLoc = env.runtime.getURL('/__parcel_hmr_proxy__?url=');
--- packages/transformers/webextension/src/runtime/autoreload.js
+++ packages/transformers/webextension/src/runtime/autoreload.js
@@ -1,10 +1,19 @@
-/* global chrome, browser */
+/* global chrome, browser, addEventListener */
 var env = typeof chrome == 'undefined' ? browser : chrome;
+addEventListener('beforeunload', function () {
+  try {
+    env.runtime.sendMessage({
+      __parcel_hmr_reload__: true,
+    });
+  } catch (err) {
+    // ignore throwing if extension context invalidated
+  }
+});
+
 env.runtime.onMessage.addListener(function (msg) {
   if (msg.__parcel_hmr_reload__) {
-    // Wait for background to reload
     setTimeout(function () {
       location.reload();
-    }, 500);
+    }, 50);
   }
 });
