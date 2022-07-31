fca5c8cdeb69be53fbbaf024c8bb42405002e04b
ChristophP <ChristophP@users.noreply.github.com>
2022-07-31 20:47:31 +0200

    Add multi module compilation for elm (#8076)

--- packages/transformers/elm/src/ElmTransformer.js
+++ packages/transformers/elm/src/ElmTransformer.js
@@ -1,16 +1,16 @@
 // @flow strict-local
 
 import {Transformer} from '@parcel/plugin';
-import commandExists from 'command-exists';
 import spawn from 'cross-spawn';
 import path from 'path';
 import {minify} from 'terser';
-import nullthrows from 'nullthrows';
 import ThrowableDiagnostic, {md} from '@parcel/diagnostic';
 // $FlowFixMe
 import elm from 'node-elm-compiler';
 // $FlowFixMe
 import elmHMR from 'elm-hot';
+
+import {load, elmBinaryPath} from './loadConfig';
 
 let isWorker;
 try {
@@ -21,24 +21,11 @@ try {
 }
 
 export default (new Transformer({
-  async loadConfig({config}) {
-    const elmConfig = await config.getConfig(['elm.json']);
-    if (!elmConfig) {
-      elmBinaryPath(); // Check if elm is even installed
-      throw new ThrowableDiagnostic({
-        diagnostic: {
-          message: "The 'elm.json' file is missing.",
-          hints: [
-            "Initialize your elm project by running 'elm init'",
-            "If you installed elm as project dependency then run 'yarn elm init' or 'npx elm init'",
-          ],
-        },
-      });
-    }
-    return elmConfig.contents;
+  loadConfig({config}) {
+    return load({config});
   },
 
-  async transform({asset, options}) {
+  async transform({asset, options, logger}) {
     const elmBinary = elmBinaryPath();
     const compilerConfig = {
       spawn,
@@ -49,9 +36,20 @@ export default (new Transformer({
       report: 'json',
     };
     asset.invalidateOnEnvChange('PARCEL_ELM_NO_DEBUG');
-    for (const filePath of await elm.findAllDependencies(asset.filePath)) {
+
+    const extraSources = resolveExtraSources({asset, logger});
+
+    extraSources.forEach(filePath => {
       asset.invalidateOnFileChange(filePath);
-    }
+    });
+    const sources = [asset.filePath, ...extraSources];
+    const dependencies = await Promise.all(
+      sources.map(source => elm.findAllDependencies(source)),
+    );
+    const uniqueDeps = new Set(dependencies.flat());
+    Array.from(uniqueDeps).forEach(filePath => {
+      asset.invalidateOnFileChange(filePath);
+    });
 
     // Workaround for `chdir` not working in workers
     // this can be removed after https://github.com/isaacs/node-graceful-fs/pull/200 was mergend and used in parcel
@@ -59,15 +57,23 @@ export default (new Transformer({
     process.chdir.disabled = isWorker;
     let code;
     try {
-      code = await compileToString(elm, elmBinary, asset, compilerConfig);
+      code = await compileToString(elm, elmBinary, sources, compilerConfig);
     } catch (e) {
       let compilerJson = e.message.split('\n')[1];
       let compilerDiagnostics = JSON.parse(compilerJson);
 
+      if (compilerDiagnostics.type === 'compile-errors') {
+        throw new ThrowableDiagnostic({
+          diagnostic: compilerDiagnostics.errors.flatMap(
+            elmCompileErrorToParcelDiagnostics,
+          ),
+        });
+      }
+
+      // compilerDiagnostics.type === "error"
+      // happens for example when compiled in prod mode with Debug.log in code
       throw new ThrowableDiagnostic({
-        diagnostic: compilerDiagnostics.errors.flatMap(
-          elmErrorToParcelDiagnostics,
-        ),
+        diagnostic: formatElmError(compilerDiagnostics, ''),
       });
     }
 
@@ -82,42 +88,24 @@ export default (new Transformer({
   },
 }): Transformer);
 
-function elmBinaryPath() {
-  let elmBinary = resolveLocalElmBinary();
+// gather extra modules that should be added to the compilation process
+function resolveExtraSources({asset, logger}) {
+  const dirname = path.dirname(asset.filePath);
+  const relativePaths = asset.query.getAll('with');
 
-  if (elmBinary == null && !commandExists.sync('elm')) {
-    throw new ThrowableDiagnostic({
-      diagnostic: {
-        message: "Can't find 'elm' binary.",
-        hints: [
-          "You can add it as an dependency for your project by running 'yarn add -D elm' or 'npm add -D elm'",
-          'If you want to install it globally then follow instructions on https://elm-lang.org/',
-        ],
-        origin: '@parcel/elm-transformer',
-      },
+  if (relativePaths.length > 0) {
+    logger.info({
+      message: md`Compiling elm with additional sources: ${md.bold(
+        JSON.stringify(relativePaths),
+      )}`,
     });
   }
 
-  return elmBinary;
+  return relativePaths.map(relPath => path.join(dirname, relPath));
 }
 
-function resolveLocalElmBinary() {
-  try {
-    let result = require.resolve('elm/package.json');
-    // $FlowFixMe
-    let pkg = require('elm/package.json');
-    let bin = nullthrows(pkg.bin);
-    return path.join(
-      path.dirname(result),
-      typeof bin === 'string' ? bin : bin.elm,
-    );
-  } catch (_) {
-    return null;
-  }
-}
-
-function compileToString(elm, elmBinary, asset, config) {
-  return elm.compileToString(asset.filePath, {
+function compileToString(elm, elmBinary, sources, config) {
+  return elm.compileToString(sources, {
     pathToElm: elmBinary,
     ...config,
   });
@@ -173,22 +161,24 @@ function formatMessagePiece(piece) {
   return md`${piece}`;
 }
 
-function elmErrorToParcelDiagnostics(error) {
+function elmCompileErrorToParcelDiagnostics(error) {
   const relativePath = path.relative(process.cwd(), error.path);
-  return error.problems.map(problem => {
-    const padLength = 80 - 5 - problem.title.length - relativePath.length;
-    const dashes = '-'.repeat(padLength);
-    const message = [
-      '',
-      `-- ${problem.title} ${dashes} ${relativePath}`,
-      '',
-      problem.message.map(formatMessagePiece).join(''),
-    ].join('\n');
+  return error.problems.map(problem => formatElmError(problem, relativePath));
+}
 
-    return {
-      message,
-      origin: '@parcel/elm-transformer',
-      stack: '', // set stack to empty since it is not useful
-    };
-  });
+function formatElmError(problem, relativePath) {
+  const padLength = 80 - 5 - problem.title.length - relativePath.length;
+  const dashes = '-'.repeat(padLength);
+  const message = [
+    '',
+    `-- ${problem.title} ${dashes} ${relativePath}`,
+    '',
+    problem.message.map(formatMessagePiece).join(''),
+  ].join('\n');
+
+  return {
+    message,
+    origin: '@parcel/elm-transformer',
+    stack: '', // set stack to empty since it is not useful
+  };
 }
