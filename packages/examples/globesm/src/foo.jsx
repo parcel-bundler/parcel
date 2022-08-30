77d12e3e44b09cc2c41fd7bfed3e6fc3b166442e
Sam Thorne <sam.thorne@electrolux.com>
2022-08-30 14:45:34 +0200

    example

--- packages/examples/globesm/src/foo.jsx
+++ packages/examples/globesm/src/foo.jsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import * as Icons from 'svg/*.svg';
+import * as Icons from './svg/*.svg';
 
 export default function () {
   const {SVG_Logo} = Icons;
-  console.log(Icons, SVG_Logo);
+  console.log(' Here : ', Icons, SVG_Logo, SVG_Logo.default);
   const Tag = SVG_Logo.default ? SVG_Logo.default : SVG_Logo;
   return (
     <>
