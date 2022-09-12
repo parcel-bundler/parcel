2bf4ff20597ea77fd9d496b1bda4f8d07d71eaca
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2022-09-12 17:49:08 +0200

    Graph explorer backend

--- packages/dev/query/src/cli.js
+++ packages/dev/query/src/cli.js
@@ -16,6 +16,9 @@ import {bundleGraphEdgeTypes} from '@parcel/core/src/BundleGraph.js';
 import {Priority} from '@parcel/core/src/types';
 
 import {loadGraphs} from './index.js';
+import * as graphExplorer from './graph-explorer';
+
+const keepAliveSymbol = Symbol('parcel-query-keepalive');
 
 let args = process.argv.slice(2);
 let cacheDir = path.join(process.cwd(), '.parcel-cache');
@@ -580,11 +583,18 @@ function stats(_) {
   }
 }
 
+async function startGraphExplorer(frontend: string) {
+  await graphExplorer.startGraphExplorer(bundleGraph, frontend);
+  return keepAliveSymbol;
+}
+function stopGraphExplorer() {
+  return graphExplorer.stopGraphExplorer();
+}
+
 // -------------------------------------------------------
 
 if (initialCmd != null) {
   eval(initialCmd);
-  process.exit(0);
 } else {
   console.log(
     'See .help. The graphs can be accessed via `assetGraph`, `bundleGraph` and `requestTracker`.',
@@ -601,12 +611,21 @@ if (initialCmd != null) {
     () => {},
   );
 
+  server.on('exit', async () => {
+    await graphExplorer.stopGraphExplorer();
+  });
+
   // $FlowFixMe[prop-missing]
   server.context.bundleGraph = bundleGraph;
   // $FlowFixMe[prop-missing]
   server.context.assetGraph = assetGraph;
   // $FlowFixMe[prop-missing]
   server.context.requestTracker = requestTracker;
+
+  // $FlowFixMe[prop-missing]
+  server.context.startGraphExplorer = startGraphExplorer;
+  // $FlowFixMe[prop-missing]
+  server.context.stopGraphExplorer = stopGraphExplorer;
 
   for (let [name, cmd] of new Map([
     [
