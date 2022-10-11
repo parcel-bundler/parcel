b59ea01989a9321f9fac9ccd31f4f03c56c8b295
Eric Eldredge <lettertwo@gmail.com>
2022-10-11 19:20:35 -0400

    Export link and unlink from module
    I guess in case they would be useful in another script...

--- packages/dev/atlassian-parcel-link/src/index.js
+++ packages/dev/atlassian-parcel-link/src/index.js
@@ -1,1 +1,7 @@
 // @flow strict-local
+
+export type {LinkOptions} from './link';
+export type {UnlinkOptions} from './unlink';
+
+export {default as link} from './link';
+export {default as unlink} from './unlink';
