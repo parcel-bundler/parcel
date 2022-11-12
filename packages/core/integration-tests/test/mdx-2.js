39802ebca213ab370b65846d99a842833eb40ae6
Ayc0 <ayc0.benj@gmail.com>
2022-11-12 15:58:07 +0100

    [mdx-2][test] use mdx 2 transformer

--- packages/core/integration-tests/test/mdx-2.js
+++ packages/core/integration-tests/test/mdx-2.js
@@ -1,10 +1,14 @@
+// @flow
+
 const assert = require('assert');
 const path = require('path');
 const {bundle, run} = require('@parcel/test-utils');
 
 describe('mdx', function () {
   it('should support bundling MDX', async function () {
-    let b = await bundle(path.join(__dirname, '/integration/mdx/index.mdx'));
+    let b = await bundle(path.join(__dirname, '/integration/mdx/index.mdx'), {
+      config: require.resolve('./mdx-2-config.json'),
+    });
 
     let output = await run(b);
     assert.equal(typeof output.default, 'function');
@@ -14,6 +18,9 @@ describe('mdx', function () {
   it('should support bundling MDX with React 17', async function () {
     let b = await bundle(
       path.join(__dirname, '/integration/mdx-react-17/index.mdx'),
+      {
+        config: require.resolve('./mdx-2-config.json'),
+      },
     );
 
     let output = await run(b);
