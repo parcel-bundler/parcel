4ec91ba2f661415b51dc5a4e230e4b1676c9d07d
Ayc0 <ayc0.benj@gmail.com>
2022-11-13 02:05:35 +0100

    Patch mdx-2 loader

--- packages/transformers/mdx-2/src/MDXTransformer.js
+++ packages/transformers/mdx-2/src/MDXTransformer.js
@@ -1,28 +1,41 @@
-// @flow
-import {Transformer} from '@parcel/plugin';
+const {Transformer} = require('@parcel/plugin');
 
-export default (new Transformer({
+module.exports = new Transformer({
   async loadConfig({config}) {
+    // config.getConfig only works with CJS
     let configFile = await config.getConfig(['.mdxrc.js', '.mdxrc.cjs'], {
       packageKey: 'mdx',
     });
+    if (!configFile) {
+      return () => {};
+    }
 
-    if (configFile) {
-      config.invalidateOnStartup();
-
+    config.invalidateOnFileChange(configFile.filePath);
+    // .mdxrc can either exports an object (MDX config), or a function that returns the config
+    if (typeof configFile.contents === 'function') {
       return configFile.contents;
     }
+    return () => configFile.contents;
   },
+
   async transform({asset, config}) {
     let code = await asset.getCode();
-    // @mdx-js/mdx is a ESM package, so we need to use `import()` to make it work in CJS envs
+
     const {compile} = await import('@mdx-js/mdx');
 
-    let compiled = await compile(code, config);
+    // @mdx-js/mdx is a ESM package, so we need to use `import()` to make it work in CJS envs
+    let compiled = await compile(code, {
+      ...(await config()),
 
+      // Those don't have to be configurable
+      useDynamicImport: false,
+      format: 'mdx',
+      outputFormat: 'program',
+    });
+
+    asset.setCode(compiled.toString('utf-8'));
     asset.type = 'js';
-    asset.setCode(compiled.value);
 
     return [asset];
   },
-}): Transformer);
+});
