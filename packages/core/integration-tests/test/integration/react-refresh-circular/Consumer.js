74e5e78ffacb9152fa74982a707e22699faa1478
Devon Govett <devongovett@gmail.com>
2023-01-05 14:07:26 -0800

    Fix import

--- packages/core/integration-tests/test/integration/react-refresh-circular/Consumer.js
+++ packages/core/integration-tests/test/integration/react-refresh-circular/Consumer.js
@@ -1,5 +1,5 @@
 import {useContext} from 'react';
-import {Context} from './provider';
+import {Context} from './Provider';
 
 // This prevents the module from being self accepting
 // (not all exports are react components).
