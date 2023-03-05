81a87ca1bc0fecf09f085c660fe8d6301bbc8ff0
Marcin Szczepanski <mszczepanski@atlassian.com>
2023-03-06 10:38:15 +1100

    Add unit tests for application profiler

---
+++ packages/core/profiler/test/ApplicationProfiler.test.js
@@ -0,0 +1,78 @@
+import {
+  applicationProfiler,
+  PluginApplicationProfiler,
+} from '../src/ApplicationProfiler';
+import sinon from 'sinon';
+import assert from 'assert';
+
+describe('ApplicationProfiler', () => {
+  let onTrace;
+  let traceDisposable;
+  beforeEach(() => {
+    onTrace = sinon.spy();
+    traceDisposable = applicationProfiler.onTrace(onTrace);
+    applicationProfiler.enable();
+  });
+  afterEach(() => {
+    traceDisposable.dispose();
+  });
+
+  it('returns no measurement when disabled', () => {
+    applicationProfiler.disable();
+    const measurement = applicationProfiler.createMeasurement('test');
+    assert(measurement == null);
+    assert(onTrace.notCalled);
+  });
+  it('emits a basic trace event', () => {
+    const measurement = applicationProfiler.createMeasurement('test');
+    measurement.end();
+    sinon.assert.calledWith(
+      onTrace,
+      sinon.match({
+        type: 'trace',
+        name: 'test',
+        args: undefined,
+        duration: sinon.match.number,
+      }),
+    );
+  });
+  it('emits a complex trace event', () => {
+    const measurement = applicationProfiler.createMeasurement(
+      'test',
+      'myPlugin',
+      'aaargh',
+      {extra: 'data'},
+    );
+    measurement.end();
+    sinon.assert.calledWith(
+      onTrace,
+      sinon.match({
+        type: 'trace',
+        name: 'test',
+        categories: ['myPlugin'],
+        args: {extra: 'data', name: 'aaargh'},
+        duration: sinon.match.number,
+      }),
+    );
+  });
+
+  describe('PluginApplicationProfiler', () => {
+    it('emits events with proper origin/category', () => {
+      const pluginProfiler = new PluginApplicationProfiler({
+        origin: 'origin',
+        category: 'cat',
+      });
+      const measurement = pluginProfiler.createMeasurement('test', 'customCat');
+      measurement.end();
+      sinon.assert.calledWith(
+        onTrace,
+        sinon.match({
+          type: 'trace',
+          name: 'test',
+          categories: ['cat:origin:customCat'],
+          duration: sinon.match.number,
+        }),
+      );
+    });
+  });
+});
