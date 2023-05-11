0a260c277c3577b1d2bb0c07f6924d9431a7f86c
Agnieszka Gawrys <agawrys@atlassian.com>
2023-05-11 10:55:45 -0700

    make repro smaller

--- packages/core/integration-tests/test/integration/circular-import-bug/esm/colors.js
+++ packages/core/integration-tests/test/integration/circular-import-bug/esm/colors.js
@@ -2,16 +2,9 @@
 
 import themed from './utils/themed';
 
-// Each tint is made of N900 and an alpha channel
-export var N0 = 'rgba(9, 30, 66, 0.02)';
-export var N20A = 'rgba(9, 30, 66, 0.04)';
-export var B100 = 'rgba(9, 30, 66, 0.04)';
-export var N30A = 'rgba(9, 30, 66, 0.04)';
-export var DN30 = '#8C9CB8';
-
 export var background = themed({
-  light: "var(--ds-surface, ".concat(N0, ")"),
-  dark: "var(--ds-surface, ".concat(DN30, ")")
+  light: "var(--ds-surface, ".concat('b', ")"),
+  dark: "var(--ds-surface, ".concat('b', ")")
 });
 
 
--- packages/core/integration-tests/test/integration/circular-import-bug/esm/utils/themed.js
+++ packages/core/integration-tests/test/integration/circular-import-bug/esm/utils/themed.js
@@ -1,38 +1,11 @@
 /* eslint-disable prefer-rest-params */
 
 import getTheme from './get-theme';
-// Unpack custom variants, and get correct value for the current theme
-function themedVariants(variantProp, variants) {
-  return function (props) {
-    var theme = getTheme(props);
-    if (props && props[variantProp] && variants) {
-      var modes = variants[props[variantProp]];
-      if (modes && modes[theme.mode]) {
-        var value = modes[theme.mode];
-        if (value) {
-          return value;
-        } // TS believes value can be undefined
-      }
-    }
 
-    return '';
-  };
-}
 export default function themed(modesOrVariant, variantModes) {
-  if (typeof modesOrVariant === 'string') {
-    return themedVariants(modesOrVariant, variantModes);
-  }
-  var modes = modesOrVariant;
+  
   return function (props) {
-    // Get theme from the user's props
-    var theme = getTheme(props);
-    // User isn't required to provide both light and dark values
-    if (theme.mode in modes) {
-      var value = modes[theme.mode]; // TS believes value can be undefined
-      if (value) {
-        return value;
-      }
-    }
-    return '';
+    var a = getTheme(props);
+    return a;
   };
 }
