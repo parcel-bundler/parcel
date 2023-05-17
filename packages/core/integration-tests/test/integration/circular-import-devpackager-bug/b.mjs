d4c9d84b8cad02094b77d0e57f5d34b1e8598673
Agnieszka Gawrys <agawrys@atlassian.com>
2023-05-16 17:44:31 -0700

    add smaller repro

---
+++ packages/core/integration-tests/test/integration/circular-import-devpackager-bug/b.mjs
@@ -0,0 +1,23 @@
+import { c } from "./c.mjs"; // imports and calls b1 (circular)
+
+// works fine in the Node native ESM
+// fails in Parcel because we don't hoist it before the imports
+export default function b1() {
+	return "b1";
+}
+
+// fails in Node native ESM
+// fails in Parcel because it isn't hoisted
+// function b1() {
+// 	return "b1";
+// };
+// export default b1;
+
+
+// works in Node native ESM
+// works in Parcel because we first do `parcelHelpers.export("b1")` and then `require(c)`
+// export function b1() {
+// 	return "b1";
+// };
+
+console.log("b: c =", c);
