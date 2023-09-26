31b22c00ca0f61efa6f41a467766813ee56ce1cd
achan3 <achan3@atlassian.com>
2023-09-26 19:45:01 -0400

    move minimal test repo into parcel

---
+++ packages/examples/test-sourcemaps/index.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import { render } from 'react-dom';
+import { FunctionComponent } from 'react';
+
+const App: FunctionComponent = () => {
+    return <></>;
+}
+
+render(<App />, document.getElementById('root'));
