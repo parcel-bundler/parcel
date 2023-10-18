6cbb16d30a42c6b5c7224b864c81326ebebe495d
mattcompiles <mattjones701@gmail.com>
2023-10-18 13:45:13 +1100

    Fix missing type in bundle uniquekey

--- packages/bundlers/default/src/DefaultBundler.js
+++ packages/bundlers/default/src/DefaultBundler.js
@@ -661,7 +661,7 @@ function createIdealGraph(
                     manualSharedMap.set(manualSharedBundleKey, bundleId);
                   }
                   bundle.manualSharedBundle = manualSharedObject.name;
-                  bundle.uniqueKey = manualSharedObject.name;
+                  bundle.uniqueKey = manualSharedObject.name + childAsset.type;
                 }
               }
 
