01528190be70a5fe45fb51733ec9ae964090c31a
Gora Kong <gora.kong1@gmail.com>
2023-11-02 14:33:38 -0700

    remove opaque type

--- packages/core/core/src/projectPath.js
+++ packages/core/core/src/projectPath.js
@@ -6,7 +6,7 @@ import {relativePath, normalizeSeparators} from '@parcel/utils';
 /**
  * A path that's relative to the project root.
  */
-export opaque type ProjectPath = string;
+export type ProjectPath = string;
 
 function toProjectPath_(projectRoot: FilePath, p: FilePath): ProjectPath {
   if (p == null) {
