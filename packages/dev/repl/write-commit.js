c619ccfdbe5ba21cebcfc455500238da882cc466
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2023-11-06 15:20:54 +0100

    Build REPL on CI

--- packages/dev/repl/write-commit.js
+++ packages/dev/repl/write-commit.js
@@ -8,8 +8,8 @@ let file = path.join(__dirname, 'commit');
 let oldCommit = fs.existsSync(file) && fs.readFileSync(file, 'utf8').trim();
 
 const newCommit = child_process
-  .execSync('git merge-base v2 HEAD', {encoding: 'utf8'})
-  // .execSync('git rev-parse HEAD', {encoding: 'utf8'})
+  // .execSync('git merge-base v2 HEAD', {encoding: 'utf8'})
+  .execSync('git rev-parse HEAD', {encoding: 'utf8'})
   .trim();
 
 if (oldCommit !== newCommit) {
