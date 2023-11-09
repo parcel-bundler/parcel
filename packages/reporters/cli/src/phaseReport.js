988eb0ce5a8873aa287afce759b1282c17c7a68c
Iris Moini <imoini@atlassian.com>
2023-11-09 18:30:49 -0500

    Add phase report func

---
+++ packages/reporters/cli/src/phaseReport.js
@@ -0,0 +1,31 @@
+// @flow
+import {prettifyTime} from '@parcel/utils';
+import chalk from 'chalk';
+import {writeOut} from './render';
+
+export default function phaseReport(phaseStartTimes) {
+  let phaseTimes = {};
+  if (phaseStartTimes['transforming'] && phaseStartTimes['bundling']) {
+    phaseTimes['Transforming'] =
+      phaseStartTimes['bundling'] - phaseStartTimes['transforming'];
+  }
+
+  let packagingAndOptimizing =
+    phaseStartTimes['packaging'] && phaseStartTimes['optimizing']
+      ? Math.min(phaseStartTimes['packaging'], phaseStartTimes['optimizing'])
+      : phaseStartTimes['packaging'] || phaseStartTimes['optimizing'];
+
+  if (phaseStartTimes['bundling'] && packagingAndOptimizing) {
+    phaseTimes['Bundling'] =
+      packagingAndOptimizing - phaseStartTimes['bundling'];
+  }
+
+  if (packagingAndOptimizing && phaseStartTimes['buildSuccess']) {
+    phaseTimes['Packaging & Optimizing'] =
+      phaseStartTimes['buildSuccess'] - packagingAndOptimizing;
+  }
+
+  for (let [phase, time] of Object.entries(phaseTimes)) {
+    writeOut(chalk.green.bold(`${phase} finished in ${prettifyTime(time)}`));
+  }
+}
