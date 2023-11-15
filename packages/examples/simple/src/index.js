c46eabfa1628c66fdb29a055eb11ac1e45e6e359
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2023-11-15 22:10:54 +0100

    Update example

--- packages/examples/simple/src/index.js
+++ packages/examples/simple/src/index.js
@@ -1,4 +1,9 @@
 import foo from './foo';
 import './bar';
+import {button} from './button.module.css';
 
 console.log(foo(122222222));
+console.log(button);
+
+let asset = new URL('./x.txt', import.meta.url);
+console.log(asset);
