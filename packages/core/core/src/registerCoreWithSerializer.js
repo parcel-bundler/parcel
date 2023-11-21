bc20c504928accbc94c1a31a0f39cbb99974fab1
Brian Do <thebriando@gmail.com>
2023-11-20 16:14:35 -0800

    Change coreRegister check back

--- packages/core/core/src/registerCoreWithSerializer.js
+++ packages/core/core/src/registerCoreWithSerializer.js
@@ -1,6 +1,6 @@
 // @flow
 import {Graph} from '@parcel/graph';
-import {ctorToName, registerSerializableClass} from './serializer';
+import {registerSerializableClass} from './serializer';
 import AssetGraph from './AssetGraph';
 import BundleGraph from './BundleGraph';
 import ParcelConfig from './ParcelConfig';
@@ -8,7 +8,11 @@ import {RequestGraph} from './RequestTracker';
 import Config from './public/Config';
 import packageJson from '../package.json';
 
+let coreRegistered;
 export function registerCoreWithSerializer() {
+  if (coreRegistered) {
+    return;
+  }
   const packageVersion: mixed = packageJson.version;
   if (typeof packageVersion !== 'string') {
     throw new Error('Expected package version to be a string');
@@ -24,9 +28,7 @@ export function registerCoreWithSerializer() {
     RequestGraph,
     // $FlowFixMe[unclear-type]
   }): Array<[string, Class<any>]>)) {
-    if (ctorToName.has(ctor)) {
-      return;
-    }
     registerSerializableClass(packageVersion + ':' + name, ctor);
   }
+  coreRegistered = true;
 }
