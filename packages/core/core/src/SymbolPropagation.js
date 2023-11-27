dc3d92b0231f8e196761a46baf5bdca657c11fc6
Gora Kong <15333808+gorakong@users.noreply.github.com>
2023-11-27 10:23:02 -0800

    Optimize Symbol Propagation (propagateSymbolsUp) (#9337)
    Co-authored-by: Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
    Co-authored-by: Celina Peralta <cperalta@atlassian.com>

--- packages/core/core/src/SymbolPropagation.js
+++ packages/core/core/src/SymbolPropagation.js
@@ -639,21 +639,18 @@ function propagateSymbolsUp(
       assetGraph.rootNodeId,
       'A root node is required to traverse',
     );
-    let visited = new Set([rootNodeId]);
-    const walk = (nodeId: NodeId) => {
+
+    const nodeVisitor = nodeId => {
       let node = nullthrows(assetGraph.getNode(nodeId));
       let outgoing = assetGraph.getNodeIdsConnectedFrom(nodeId);
+
       for (let childId of outgoing) {
-        if (!visited.has(childId)) {
-          visited.add(childId);
-          walk(childId);
-          let child = nullthrows(assetGraph.getNode(childId));
-          if (node.type === 'asset') {
-            invariant(child.type === 'dependency');
-            if (child.usedSymbolsUpDirtyUp) {
-              node.usedSymbolsUpDirty = true;
-              child.usedSymbolsUpDirtyUp = false;
-            }
+        let child = nullthrows(assetGraph.getNode(childId));
+        if (node.type === 'asset') {
+          invariant(child.type === 'dependency');
+          if (child.usedSymbolsUpDirtyUp) {
+            node.usedSymbolsUpDirty = true;
+            child.usedSymbolsUpDirtyUp = false;
           }
         }
       }
@@ -698,7 +695,7 @@ function propagateSymbolsUp(
         }
       }
     };
-    walk(rootNodeId);
+    assetGraph.postOrderDfsFast(nodeVisitor, rootNodeId);
   }
 
   let queue = dirtyDeps ?? changedDepsUsedSymbolsUpDirtyDownAssets;
