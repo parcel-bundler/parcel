bf9a03f640bebd98dfa0d293675f4cbc28dc9308
Agnieszka Gawrys <agawrys@atlassian.com>
2023-12-12 16:44:23 -0800

    revert weird extension.ts change

--- packages/utils/parcelforvscode/src/extension.ts
+++ packages/utils/parcelforvscode/src/extension.ts
@@ -52,15 +52,11 @@ export function deactivate(): Thenable<void> | undefined {
     return undefined;
   }
 
-  const LSP_SENTINEL_FILEPATH = path.join(
-    fs.realpathSync(os.tmpdir()),
-    'parcel-lsp',
-    'lsp-server',
-  );
+  const LSP_SENTINEL_FILEPATH = path.join(fs.realpathSync(os.tmpdir()), 'parcel-lsp', 'lsp-server');
 
   if (fs.existsSync(LSP_SENTINEL_FILEPATH)) {
     fs.rmSync(LSP_SENTINEL_FILEPATH);
-  }
+  } 
 
   return client.stop();
 }
