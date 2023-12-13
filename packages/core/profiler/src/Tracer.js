3896a9b62884f9325ca4de0043cc38e7daae68fe
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2023-12-13 18:00:56 +0100

    Changes to make TS happy

--- packages/core/profiler/src/Tracer.js
+++ packages/core/profiler/src/Tracer.js
@@ -9,6 +9,7 @@ import type {
   TraceMeasurement as ITraceMeasurement,
   TraceMeasurementData,
 } from './types';
+// @ts-ignore
 import {ValueEmitter} from '@parcel/events';
 
 // $FlowFixMe
@@ -21,16 +22,18 @@ try {
   tid = 0;
 }
 
+// @ts-ignore
 const performance: Performance = _performance;
 const pid = process.pid;
 
 class TraceMeasurement implements ITraceMeasurement {
-  #active /* boolean */ = true;
-  #name /* string */;
-  #pid /* number */;
-  #tid /* number */;
-  #start /* number */;
-  #data /* any */;
+  #active: boolean = true;
+  #name: string;
+  #pid: number;
+  #tid: number;
+  #start: number;
+  // $FlowFixMe
+  #data: any;
   constructor(tracer: Tracer, name, pid, tid, data) {
     this.#name = name;
     this.#pid = pid;
@@ -56,9 +59,9 @@ class TraceMeasurement implements ITraceMeasurement {
 }
 
 export default class Tracer {
-  #traceEmitter /* ValueEmitter<TraceEvent> */ = new ValueEmitter();
+  #traceEmitter: ValueEmitter<TraceEvent> = new ValueEmitter();
 
-  #enabled /* boolean */ = false;
+  #enabled: boolean = false;
 
   onTrace(cb: (event: TraceEvent) => mixed): IDisposable {
     return this.#traceEmitter.addListener(cb);
@@ -75,7 +78,7 @@ export default class Tracer {
 
   createMeasurement(
     name: string,
-    category?: string = 'Core',
+    category: string = 'Core',
     argumentName?: string,
     otherArgs?: {[key: string]: mixed},
   ): ITraceMeasurement | null {
