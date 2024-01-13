ec304b3acd0ae00fc4dbcc8b78daed14433120a3
Devon Govett <devongovett@gmail.com>
2024-01-13 12:01:19 -0500

    enable typescript resolution features in node package manager

--- packages/core/integration-tests/test/macros.js
+++ packages/core/integration-tests/test/macros.js
@@ -18,7 +18,7 @@ describe('macros', function () {
   it('should support named imports', async function () {
     await fsFixture(overlayFS, dir)`
       index.js:
-        import { hash } from "./macro.js" with { type: "macro" };
+        import { hash } from "./macro" with { type: "macro" };
         output = hash('hi');
 
       macro.js:
@@ -410,6 +410,27 @@ describe('macros', function () {
       index.js:
         import { test } from "./macro.ts" with { type: "macro" };
         output = test(1, 2);
+          return a + b;
+        }
+    `;
+
+    let b = await bundle(path.join(dir, '/index.js'), {
+      inputFS: overlayFS,
+      mode: 'production',
+    });
+
+    let res = await overlayFS.readFile(b.getBundles()[0].filePath, 'utf8');
+    assert(res.includes('output=3'));
+  });
+
+  it('should support macros written in typescript without extension', async function () {
+    await fsFixture(overlayFS, dir)`
+      index.js:
+        import { test } from "./macro" with { type: "macro" };
+        output = test(1, 2);
+
+      macro.ts:
+        export function test(a: number, b: number) {
 
       macro.ts:
         export function test(a: number, b: number) {
