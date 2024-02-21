d42930dd0593aea7dbb5b905a989745dd4cebe33
Devon Govett <devongovett@gmail.com>
2024-02-20 22:16:01 -0500

    remove old code

--- packages/examples/react-server-components/react.js
+++
@@ -1,5 +0,0 @@
-if (globalThis.process) {
-  module.exports = __parcel__require__('react');
-} else {
-  module.exports = require('./node_modules/react/index.js');
-}
--- packages/examples/react-server-components/src/serverClient.js
+++
@@ -1,6 +0,0 @@
-import {createFromReadableStream} from 'react-server-dom-parcel/client.edge';
-import {renderToReadableStream as renderHTMLToReadableStream} from 'react-dom/server.edge';
-import ReactClient from 'react';
-import 'react/jsx-runtime';
-
-export {createFromReadableStream, renderHTMLToReadableStream, ReactClient};
