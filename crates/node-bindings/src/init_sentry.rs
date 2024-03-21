4e7cf31f3e2d86225f25c94e978bea4b77d7cb82
ngrujic <ngrujic@atlassian.com>
2024-03-21 16:08:04 +1100

    added tag support via env vars

--- crates/node-bindings/src/init_sentry.rs
+++ crates/node-bindings/src/init_sentry.rs
@@ -1,5 +1,7 @@
 use napi_derive::napi;
 use once_cell::sync::Lazy;
+use querystring::querify;
+use sentry::configure_scope;
 use sentry::{init, ClientInitGuard};
 use std::{
   sync::{Arc, Mutex},
@@ -12,33 +14,47 @@ static SENTRY_GUARD: Lazy<Arc<Mutex<Option<ClientInitGuard>>>> =
 const TIMEOUT: Duration = Duration::from_secs(2);
 
 #[napi]
-fn init_sentry(release: napi::JsString) {
+fn init_sentry() {
+  println!("Initialising Sentry in rust...");
+
   if SENTRY_GUARD.lock().unwrap().is_some() {
+    println!("Sentry guard already set, skipping initialisation.");
     return;
   }
 
   let Ok(sentry_dsn) = std::env::var("PARCEL_SENTRY_DSN") else {
+    println!("PARCEL_SENTRY_DSN environment variable not provided.");
     return;
   };
-  let release_utf8: napi::JsStringUtf8 = release.into_utf8().unwrap();
-  let release_str: String = release_utf8.into_owned().unwrap();
 
   let guard = init((
     sentry_dsn,
     sentry::ClientOptions {
-      release: Some(release_str.into()),
-      debug: true,
-      environment: Some("local".into()),
+      environment: Some("test".into()),
       ..Default::default()
     },
   ));
+
   SENTRY_GUARD.lock().unwrap().replace(guard);
+
   sentry::configure_scope(|scope| {
     scope.set_user(Some(sentry::User {
       username: Some(username()),
       ..Default::default()
     }));
   });
+
+  if let Ok(sentry_tags_raw) = std::env::var("PARCEL_SENTRY_TAGS") {
+    let url = querify(&sentry_tags_raw);
+    for (key, val) in url.iter() {
+      if *key == "" || *val == "" {
+        continue;
+      }
+      configure_scope(|scope| scope.set_tag(key, val));
+    }
+  }
+  println!("Parcel Sentry for rust setup done!");
+  panic!("Please ignore");
 }
 
 #[napi]
