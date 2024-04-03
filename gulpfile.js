7b5fc73bbc980546844984aea7a12cc0e24fc1b6
Pedro Yamada <pyamada@atlassian.com>
2024-04-03 11:30:24 +1100

    Remove src to lib rewrites on package.json files
    This commit modifies the strategy to local src/lib rewrites, so that the
    package.json files are never modified.
    
    Intead, the package.json files always point at `bin/...`; which in turn
    requires `lib/bin.js`.
    
    During development, `lib/bin.js` can just require `src/bin.js` and rely on
    require.extensions register. But on distribution `lib/**` can be pre-built.
    
    By maintaining a stable `bin/...` entry-point, the following benefits are
    achieved:
    
    * The git workspace does not become dirty after a build
    * It's not possible to get into a state where `node_modules/.bin/parcel` points
      at a non existant `lib/` file, which can only be fixed by removing
      node_modules and yarn installing again

--- gulpfile.js
+++ gulpfile.js
@@ -28,12 +28,6 @@ const paths = {
     ...IGNORED_PACKAGES,
   ],
   packageOther: ['packages/*/*/src/**/dev-prelude.js'],
-  packageJson: [
-    'packages/core/parcel/package.json',
-    'packages/utils/create-react-app/package.json',
-    'packages/dev/query/package.json',
-    'packages/dev/bundle-stats-cli/package.json',
-  ],
   packages: 'packages/',
 };
 
@@ -64,16 +58,7 @@ exports.clean = function clean(cb) {
   );
 };
 
-exports.default = exports.build = gulp.series(
-  gulp.parallel(buildBabel, copyOthers),
-  // Babel reads from package.json so update these after babel has run
-  paths.packageJson.map(
-    packageJsonPath =>
-      function updatePackageJson() {
-        return _updatePackageJson(packageJsonPath);
-      },
-  ),
-);
+exports.default = exports.build = gulp.parallel(buildBabel, copyOthers);
 
 function buildBabel() {
   return gulp
@@ -88,28 +73,6 @@ function copyOthers() {
     .src(paths.packageOther)
     .pipe(renameStream(relative => relative.replace('src', 'lib')))
     .pipe(gulp.dest(paths.packages));
-}
-
-function _updatePackageJson(file) {
-  return gulp
-    .src(file)
-    .pipe(
-      new TapStream(vinyl => {
-        let json = JSON.parse(vinyl.contents);
-        // Replace all references to `src` in package.json bin entries
-        // `lib` equivalents.
-        if (typeof json.bin === 'object' && json.bin != null) {
-          for (let [binName, binPath] of Object.entries(json.bin)) {
-            json.bin[binName] = binPath.replace('src', 'lib');
-          }
-        } else if (typeof json.bin === 'string') {
-          json.bin = json.bin.replace('src', 'lib');
-        }
-
-        vinyl.contents = Buffer.from(JSON.stringify(json, null, 2));
-      }),
-    )
-    .pipe(gulp.dest(path.dirname(file)));
 }
 
 function renameStream(fn) {
---
+++ packages/utils/create-react-app/bin/dev-bin.js
@@ -0,0 +1,6 @@
+/**
+ * This file is copied into /lib/ by `yarn run dev:prepare`
+ *
+ * When babel build runs it is overwritten by another asset.
+ */
+require('../src/bin');
