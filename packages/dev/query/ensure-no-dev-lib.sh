4d14f160fae5b841c31b28dec06ef9ddce6399ab
Pedro Yamada <tacla.yamada@gmail.com>
2024-04-04 18:57:45 +1100

    Don't duplicate scripts, instead symlink

--- packages/dev/query/ensure-no-dev-lib.sh
+++ packages/dev/query/ensure-no-dev-lib.sh
@@ -1,12 +1,1 @@
-#!/bin/bash
-
-set -e
-
-if grep -q "DEV BIN - DO NOT PUBLISH" lib/bin.js
-then
-  echo "found dev files in lib"
-  exit 1
-else
-  echo "no dev files in lib"
-  exit 0
-fi
+../../../scripts/ensure-no-dev-lib.sh
