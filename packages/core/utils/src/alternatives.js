9712c28c6ea8c9a520b1c1335e3ebd635724e295
mattcompiles <mattjones701@gmail.com>
2024-04-08 10:43:46 +1000

    Sort potential, rather than fuzzy results

--- packages/core/utils/src/alternatives.js
+++ packages/core/utils/src/alternatives.js
@@ -142,5 +142,5 @@ export async function findAlternativeFiles(
   }
 
   // Order is not consistent which can make integration tests flaky so we sort the result.
-  return fuzzySearch(potentialFiles, fileSpecifier).slice(0, 2).sort();
+  return fuzzySearch(potentialFiles.sort(), fileSpecifier).slice(0, 2);
 }
