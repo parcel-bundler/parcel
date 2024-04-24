f1abafb2c6f2072689aff4f7091400a887240189
Pedro Yamada <tacla.yamada@gmail.com>
2024-04-24 16:15:09 +1000

    Update entry-point

--- crates/node-bindings/src/core/js_requests/asset_request/mod.rs
+++ crates/node-bindings/src/core/js_requests/asset_request/mod.rs
@@ -1,1 +1,13 @@
+use crate::core::requests::asset_request::AssetRequest;
+use napi::{Env, JsObject};
+use napi_derive::napi;
 
+#[napi]
+fn napi_run_asset_request(
+  env: Env,
+  asset_request: AssetRequest,
+  api: JsObject,
+  options: JsObject,
+) -> napi::Result<()> {
+  todo!("RUN ASSET REQUEST")
+}
