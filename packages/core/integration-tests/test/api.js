7d74822545e73019fe6d728cd3a62fd859ccb67d
Ben Jervis <bjervis@atlassian.com>
2024-04-26 14:23:26 +1000

    Add a test first

--- packages/core/integration-tests/test/api.js
+++ packages/core/integration-tests/test/api.js
@@ -1,7 +1,16 @@
 // @flow strict-local
 import path from 'path';
 import assert from 'assert';
-import {distDir, bundle, assertBundles, outputFS} from '@parcel/test-utils';
+import {
+  distDir,
+  bundle,
+  assertBundles,
+  outputFS,
+  overlayFS,
+  fsFixture,
+} from '@parcel/test-utils';
+
+import {PARCEL_VERSION} from '../../core/src/constants';
 
 describe('JS API', function () {
   it('should respect distEntry', async function () {
@@ -48,5 +57,53 @@ describe('JS API', function () {
     ]);
 
     assert(await outputFS.exists(path.join(distDir, 'bundle-buddy.json')));
+  });
+
+  describe('Reporter API', () => {
+    it('should pass the parcel version to plugins', async () => {
+      const dir = path.join(__dirname, 'plugin-parcel-version');
+      const versionFileLocation = path.join(dir, 'parcel-version.txt');
+
+      overlayFS.mkdirp(dir);
+
+      await fsFixture(overlayFS, dir)`
+      index.js:
+        export default 'Hi';
+      
+      .parcelrc:
+        {
+          extends: "@parcel/config-default",
+          reporters: ["./reporter-plugin.js", "..."],
+        }
+
+      package.json:
+        {
+          "version": "1234"
+        }
+
+      yarn.lock:
+      
+      reporter-plugin.js:
+        import {Reporter} from '@parcel/plugin';
+
+        export default new Reporter({
+          async report({event, options}) {
+            if (event.type === 'buildSuccess') {
+              await options.outputFS.writeFile("${versionFileLocation}", options.parcelVersion);
+            }
+          }
+        })
+      `;
+
+      await bundle(path.join(dir, 'index.js'), {
+        inputFS: overlayFS,
+        outputFS: overlayFS,
+      });
+
+      assert.equal(
+        await overlayFS.readFile(versionFileLocation),
+        PARCEL_VERSION,
+      );
+    });
   });
 });
