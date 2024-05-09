9c5f75e77ef40302592b6f7bf0b589a60574bb8a
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2024-05-09 13:47:31 +0200

    Fix imports used in member props

--- packages/transformers/js/core/src/hoist.rs
+++ packages/transformers/js/core/src/hoist.rs
@@ -1356,7 +1356,7 @@ mod tests {
 
     let (collect, _code, _hoist) = parse(
       r#"
-    import { a, b, c, d, e } from "other";
+    import { a, b, c, d, e, f } from "other";
     import * as x from "other";
     import * as y from "other";
 
@@ -1366,11 +1366,12 @@ mod tests {
     log(x);
     y.foo();
     e.foo.bar();
+    global[f] = 1;
     "#,
     );
     assert_eq_set!(
       collect.used_imports,
-      set! { w!("a"), w!("b"), w!("c"), w!("e"), w!("x"), w!("y") }
+      set! { w!("a"), w!("b"), w!("c"), w!("e"), w!("f"), w!("x"), w!("y") }
     );
     assert_eq_imports!(
       collect.imports,
@@ -1380,6 +1381,7 @@ mod tests {
         w!("c") => (w!("other"), w!("c"), false),
         w!("d") => (w!("other"), w!("d"), false),
         w!("e") => (w!("other"), w!("e"), false),
+        w!("f") => (w!("other"), w!("f"), false),
         w!("x") => (w!("other"), w!("*"), false),
         w!("y") => (w!("other"), w!("*"), false)
       }
