64082bcb2e47b849251f1af318426b0183302555
Monica Olejniczak <molejniczak@atlassian.com>
2024-05-14 14:20:04 +1000

    Reexport core types

--- crates/parcel_core/src/types.rs
+++ crates/parcel_core/src/types.rs
@@ -1,10 +1,29 @@
-pub mod asset;
-pub mod bundle;
-pub mod dependency;
-pub mod environment;
-pub mod file_type;
-pub mod json;
-pub mod parcel_options;
-pub mod source;
-pub mod symbol;
-pub mod target;
+mod asset;
+pub use self::asset::*;
+
+mod bundle;
+pub use self::bundle::*;
+
+mod dependency;
+pub use self::dependency::*;
+
+mod environment;
+pub use self::environment::*;
+
+mod file_type;
+pub use self::file_type::*;
+
+mod json;
+pub use self::json::*;
+
+mod parcel_options;
+pub use self::parcel_options::*;
+
+mod source;
+pub use self::source::*;
+
+mod symbol;
+pub use self::symbol::*;
+
+mod target;
+pub use self::target::*;
