97a85efb9c281ad64e0b1cfafe4f01e9c712748d
David Alsh <dalsh@atlassian.com>
2024-05-20 06:00:42 +0000

    Added eventual public API for Parcel crate, moved napi binding code to node_bindings crate

--- crates/parcel_core/src/lib.rs
+++ crates/parcel_core/src/lib.rs
@@ -1,3 +1,4 @@
+#![deny(unused_crate_dependencies)]
 //! Core re-implementation in Rust
 
 pub mod bundle_graph;
@@ -5,8 +6,4 @@ pub mod hash;
 pub mod plugin;
 pub mod types;
 
-/// New-type for paths relative to a project-root
-pub mod project_path;
-
-/// Request types and run functions
-pub mod requests;
+// pub use parcel::*;
---
+++ crates/parcel_core/src/parcel/parcel.rs
@@ -0,0 +1,12 @@
+// TODO eventual public API for Parcel
+pub struct Parcel {}
+
+impl Parcel {
+  pub fn run() {
+    todo!();
+  }
+
+  pub fn watch() {
+    todo!();
+  }
+}
