8f07a1c7a9a402ffcdfef7b0fccd64bf8aa115e9
Monica Olejniczak <molejniczak@atlassian.com>
2024-05-23 14:59:09 +1000

    Add cli handling

--- packages/core/integration-tests/test/reporters.js
+++ packages/core/integration-tests/test/reporters.js
@@ -1,107 +1,73 @@
 // @flow
 
 import assert from 'assert';
+import {execSync} from 'child_process';
 import path from 'path';
 
 import {INTERNAL_ORIGINAL_CONSOLE} from '@parcel/logger';
-import {bundle, fsFixture, overlayFS} from '@parcel/test-utils';
+import {bundle} from '@parcel/test-utils';
 import sinon from 'sinon';
 
-describe.only('reporters', () => {
-  let consoleError;
-  let processExitCode;
-  let dir = path.join(__dirname, 'reporters');
+describe('reporters', () => {
+  let successfulEntry = path.join(
+    __dirname,
+    'integration',
+    'reporters-success',
+    'index.js',
+  );
 
-  beforeEach(async () => {
-    processExitCode = process.exitCode;
-    consoleError = sinon.stub(INTERNAL_ORIGINAL_CONSOLE, 'error');
-    await overlayFS.mkdirp(dir);
-  });
+  let failingEntry = path.join(
+    __dirname,
+    'integration',
+    'reporters-failure',
+    'index.js',
+  );
 
-  afterEach(async () => {
-    process.exitCode = processExitCode;
-    sinon.restore();
-  });
-
-  after(async () => {
-    await overlayFS.rimraf(dir);
-  });
-
-  async function createReporterFixture(name: string, reporter: string) {
-    let cwd = path.join(dir, name);
-
-    await overlayFS.mkdirp(cwd);
-
-    await fsFixture(overlayFS, cwd)`
-      index.js:
-        export function main() {}
-
-      test-reporter/index.js:
-        ${reporter}
-
-      test-reporter/package.json:
-        {
-          "name": "test-reporter",
-          "main": "index.js",
-          "version": "1.0.0"
-        }
-
-      .parcelrc:
-        {
-          "extends": "@parcel/config-default",
-          "reporters": ["./test-reporter"]
-        }
-
-      yarn.lock: {}
-    `;
-
-    return cwd;
-  }
-
-  it('exit successfully when no errors are emitted', async () => {
-    let cwd = await createReporterFixture(
-      'success',
-      `
-      test-reporter/index.js:
-        const { Reporter } = require('@parcel/plugin');
-
-        module.exports = new Reporter({
-          async report({ event }) {}
-        });
-    `,
-    );
-
-    await bundle(path.join(cwd, 'index.js'), {
-      inputFS: overlayFS,
+  describe('running on the cli', () => {
+    it('exit successfully when no errors are emitted', () => {
+      assert.doesNotThrow(() =>
+        execSync(`parcel build --no-cache ${successfulEntry}`, {
+          stdio: 'ignore',
+        }),
+      );
     });
 
-    assert(!process.exitCode);
+    it('exit with an error code when an error is emitted', () => {
+      assert.throws(() =>
+        execSync(`parcel build --no-cache ${failingEntry}`, {stdio: 'ignore'}),
+      );
+    });
   });
 
-  it('exit with an error code when an error is emitted', async () => {
-    let cwd = await createReporterFixture(
-      'error',
-      `
-      test-reporter/index.js:
-        const { Reporter } = require('@parcel/plugin');
+  describe('running on the programmatic api', () => {
+    let consoleError;
+    let processExitCode;
 
-        module.exports = new Reporter({
-          async report({ event }) {
-            if (event.type === 'buildSuccess') {
-              throw new Error('Failed to report buildSuccess');
-            }
-          }
-        });
-    `,
-    );
-
-    await bundle(path.join(cwd, 'index.js'), {
-      inputFS: overlayFS,
+    beforeEach(() => {
+      processExitCode = process.exitCode;
+      consoleError = sinon.stub(INTERNAL_ORIGINAL_CONSOLE, 'error');
     });
 
-    assert.equal(process.exitCode, 1);
-    assert(
-      consoleError.calledWithMatch({message: 'Failed to report buildSuccess'}),
-    );
+    afterEach(() => {
+      process.exitCode = processExitCode;
+      sinon.restore();
+    });
+
+    it('exit successfully when no errors are emitted', async () => {
+      await bundle(successfulEntry);
+
+      assert(!process.exitCode);
+    });
+
+    it('exit with an error code when an error is emitted', async () => {
+      await bundle(failingEntry);
+
+      assert.equal(process.exitCode, 1);
+      assert(
+        consoleError.calledWithMatch({
+          message: 'Failed to report buildSuccess',
+        }),
+      );
+    });
   });
 });
