421f6453f1e1de35f5d89090fb5f6f7f10ec64ca
pyamada (Remote Dev Environment) <pyamada@atlassian.com>
2024-06-03 03:20:52 +0000

    Refactor to use thread-safe functions directly

--- crates/node-bindings/src/file_system/file_system_napi.rs
+++ crates/node-bindings/src/file_system/file_system_napi.rs
@@ -4,6 +4,8 @@ use std::sync::mpsc::channel;
 use std::sync::mpsc::Sender;
 use std::thread;
 
+use napi::bindgen_prelude::FromNapiValue;
+use napi::threadsafe_function::ThreadsafeFunction;
 use napi::threadsafe_function::ThreadsafeFunctionCallMode;
 use napi::Env;
 use napi::JsFunction;
@@ -15,19 +17,30 @@ use serde::Serialize;
 
 // TODO error handling
 
-#[derive(Clone)]
 pub struct FileSystemNapi {
-  tx_read_file_sync: Sender<(PathBuf, Sender<String>)>,
-  tx_is_file_sync: Sender<(PathBuf, Sender<bool>)>,
-  tx_is_dir_sync: Sender<(PathBuf, Sender<bool>)>,
+  read_file_fn: Box<dyn Fn(PathBuf) -> String + Send + Sync>,
+  is_file_fn: Box<dyn Fn(PathBuf) -> bool + Send + Sync>,
+  is_dir_fn: Box<dyn Fn(PathBuf) -> bool + Send + Sync>,
 }
 
 impl FileSystemNapi {
   pub fn new(env: &Env, js_file_system: JsObject) -> napi::Result<Self> {
     Ok(Self {
-      tx_read_file_sync: create_js_callback(&env, &js_file_system, "readFileSync")?,
-      tx_is_file_sync: create_js_callback(&env, &js_file_system, "isFile")?,
-      tx_is_dir_sync: create_js_callback(&env, &js_file_system, "isDir")?,
+      read_file_fn: Box::new(create_js_thread_safe_method(
+        &env,
+        &js_file_system,
+        "readFileSync",
+      )?),
+      is_file_fn: Box::new(create_js_thread_safe_method(
+        &env,
+        &js_file_system,
+        "isFile",
+      )?),
+      is_dir_fn: Box::new(create_js_thread_safe_method(
+        &env,
+        &js_file_system,
+        "isDir",
+      )?),
     })
   }
 }
@@ -36,71 +49,49 @@ impl FileSystemNapi {
 // thread or they will cause JavaScript to deadlock
 impl FileSystem for FileSystemNapi {
   fn read_to_string(&self, path: &Path) -> std::io::Result<String> {
-    let (tx, rx) = channel();
-
-    self
-      .tx_read_file_sync
-      .send((path.to_path_buf(), tx))
-      .unwrap();
-
-    Ok(rx.recv().unwrap())
+    Ok((*self.read_file_fn)(path.to_path_buf()))
   }
 
   fn is_file(&self, path: &Path) -> bool {
-    let (tx, rx) = channel();
-
-    self.tx_is_file_sync.send((path.to_path_buf(), tx)).unwrap();
-
-    rx.recv().unwrap()
+    (*self.is_file_fn)(path.to_path_buf())
   }
 
   fn is_dir(&self, path: &Path) -> bool {
-    let (tx, rx) = channel();
-
-    self.tx_is_dir_sync.send((path.to_path_buf(), tx)).unwrap();
-
-    rx.recv().unwrap()
+    (*self.is_dir_fn)(path.to_path_buf())
   }
 }
 
-fn create_js_callback<
+fn create_js_thread_safe_method<
   Params: Send + Serialize + 'static,
-  Response: Send + DeserializeOwned + 'static,
+  Response: Send + DeserializeOwned + 'static + FromNapiValue,
 >(
   env: &Env,
   js_file_system: &JsObject,
   method_name: &str,
-) -> napi::Result<Sender<(Params, Sender<Response>)>> {
+) -> Result<impl Fn(Params) -> Response, napi::Error> {
   let (tx, rx) = channel::<(Params, Sender<Response>)>();
 
   let jsfn: JsFunction = js_file_system.get_property(env.create_string(method_name)?)?;
 
-  let tsfn = env.create_threadsafe_function(
+  let threadsafe_function = env.create_threadsafe_function(
     &jsfn,
     0,
     |ctx: napi::threadsafe_function::ThreadSafeCallContext<Params>| {
       Ok(vec![ctx.env.to_js_value(&ctx.value)?])
     },
   )?;
+  let result = move |params| {
+    let (tx, mut rx) = std::sync::mpsc::sync_channel(1);
+    threadsafe_function.call_with_return_value(
+      Ok(params),
+      ThreadsafeFunctionCallMode::Blocking,
+      move |result: Response| {
+        let _ = tx.send(result);
+        Ok(())
+      },
+    );
+    rx.recv().unwrap()
+  };
 
-  let unsafe_env = env.raw() as usize;
-
-  thread::spawn(move || {
-    while let Ok((path, rx_result)) = rx.recv() {
-      tsfn.call_with_return_value(
-        Ok(path),
-        ThreadsafeFunctionCallMode::Blocking,
-        move |result: JsUnknown| {
-          let env = unsafe { Env::from_raw(unsafe_env as _) };
-
-          let result = env.from_js_value::<Response, JsUnknown>(result)?;
-          rx_result.send(result).unwrap();
-
-          return Ok(());
-        },
-      );
-    }
-  });
-
-  Ok(tx)
+  Ok(result)
 }
