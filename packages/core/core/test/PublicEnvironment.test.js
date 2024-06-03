d34f4b32bb69568ef9114bf065a1ad6b661192fa
Ryan Hitchman <hitchmanr@gmail.com>
2024-06-03 00:58:29 -0600

    Fix env.supports to match browserslist for es6-modules etc.
    Critically, this ensures that a browserslist of "supports es6-modules"
    will *not* produce differential bundling <script nomodule> tags.
    
    Minimum supported browser versions are largely sourced from caniuse.com,
    plus some data pulled from old user agent strings for the Chromium clones.

--- packages/core/core/test/PublicEnvironment.test.js
+++ packages/core/core/test/PublicEnvironment.test.js
@@ -9,11 +9,9 @@ describe('Public Environment', () => {
   it('has correct support data for ChromeAndroid', () => {
     let env = new PublicEnvironment(
       createEnvironment({
-        context: 'browser',
         engines: {
           browsers: ['last 1 Chrome version', 'last 1 ChromeAndroid version'],
         },
-        outputFormat: 'esmodule',
       }),
       DEFAULT_OPTIONS,
     );
@@ -22,6 +20,33 @@ describe('Public Environment', () => {
     assert(env.supports('dynamic-import'));
     assert(env.supports('worker-module'));
     assert(env.supports('import-meta-url'));
+    assert(env.supports('arrow-functions'));
+  });
+  it('matches browserslist for es6-modules support', () => {
+    let env = new PublicEnvironment(
+      createEnvironment({
+        engines: {browsers: 'fully supports es6-module'},
+      }),
+      DEFAULT_OPTIONS,
+    );
+    assert(env.supports('esmodules'));
+  });
+  it('matches browserslist for dynamic-imports support', () => {
+    let env = new PublicEnvironment(
+      createEnvironment({
+        engines: {browsers: 'fully supports es6-module-dynamic-import'},
+      }),
+      DEFAULT_OPTIONS,
+    );
+    assert(env.supports('dynamic-import'));
+  });
+  it('matches browserslist for arrow-functions support', () => {
+    let env = new PublicEnvironment(
+      createEnvironment({
+        engines: {browsers: 'fully supports arrow-functions'},
+      }),
+      DEFAULT_OPTIONS,
+    );
     assert(env.supports('arrow-functions'));
   });
 });
