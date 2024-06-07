5c3eef958e1198a404b749ac3a26964eafbe3214
David Alsh <alshdavid@gmail.com>
2024-06-07 16:59:19 +1000

    doesn't need to be in a thead because the async call is using a Tokio thread

--- crates/node-bindings/src/parcel/parcel.rs
+++ crates/node-bindings/src/parcel/parcel.rs
@@ -1,9 +1,5 @@
-use std::io;
 use std::path::PathBuf;
-use std::sync::mpsc::channel;
-use std::sync::mpsc::Sender;
 use std::sync::Arc;
-use std::thread;
 
 use napi::Env;
 use napi::JsObject;
@@ -14,29 +10,9 @@ use parcel::ParcelOptions;
 
 use crate::file_system::FileSystemNapi;
 
-enum ParcelMessage {
-  RpcPing {
-    response: Sender<()>,
-  },
-  FsReadToString {
-    path: PathBuf,
-    response: Sender<io::Result<String>>,
-  },
-  FsIsFile {
-    path: PathBuf,
-    response: Sender<bool>,
-  },
-  FsIsDir {
-    path: PathBuf,
-    response: Sender<bool>,
-  },
-}
-
-enum ParcelResponse {}
-
 #[napi]
 pub struct ParcelNapi {
-  tx_parcel: Sender<ParcelMessage>,
+  parcel: Arc<Parcel>,
 }
 
 #[napi]
@@ -60,79 +36,32 @@ impl ParcelNapi {
       rpc: Some(Arc::new(rpc_host_nodejs)),
     });
 
-    // Run Parcel within its own thread
-    let (tx_parcel, rx_parcel) = channel::<ParcelMessage>();
-    thread::spawn(move || {
-      while let Ok(msg) = rx_parcel.recv() {
-        if match msg {
-          ParcelMessage::FsReadToString { path, response } => {
-            response.send(parcel.fs.read_to_string(&path)).is_err()
-          }
-          ParcelMessage::FsIsFile { path, response } => {
-            response.send(parcel.fs.is_file(&path)).is_err()
-          }
-          ParcelMessage::FsIsDir { path, response } => {
-            response.send(parcel.fs.is_dir(&path)).is_err()
-          }
-          ParcelMessage::RpcPing { response } => response
-            .send(parcel.rpc.as_ref().unwrap().ping().unwrap())
-            .is_err(),
-        } {
-          return;
-        }
-      }
-    });
-
-    Ok(Self { tx_parcel })
+    Ok(Self {
+      parcel: Arc::new(parcel),
+    })
   }
 
   // Temporary, for testing
   #[napi]
   pub async fn _testing_temp_fs_read_to_string(&self, path: String) -> napi::Result<String> {
-    let (tx, rx) = channel();
-    self
-      .tx_parcel
-      .send(ParcelMessage::FsReadToString {
-        path: PathBuf::from(path),
-        response: tx,
-      })
-      .unwrap();
-    Ok(rx.recv().unwrap()?)
+    Ok(self.parcel.fs.read_to_string(&PathBuf::from(path))?)
   }
 
   #[napi]
   pub async fn _testing_temp_fs_is_file(&self, path: String) -> napi::Result<bool> {
-    let (tx, rx) = channel();
-    self
-      .tx_parcel
-      .send(ParcelMessage::FsIsFile {
-        path: PathBuf::from(path),
-        response: tx,
-      })
-      .unwrap();
-    Ok(rx.recv().unwrap())
+    Ok(self.parcel.fs.is_file(&PathBuf::from(path)))
   }
 
   #[napi]
   pub async fn _testing_temp_fs_is_dir(&self, path: String) -> napi::Result<bool> {
-    let (tx, rx) = channel();
-    self
-      .tx_parcel
-      .send(ParcelMessage::FsIsDir {
-        path: PathBuf::from(path),
-        response: tx,
-      })
-      .unwrap();
-    Ok(rx.recv().unwrap())
+    Ok(self.parcel.fs.is_dir(&PathBuf::from(path)))
   }
 
   #[napi]
   pub async fn _testing_rpc_ping(&self) -> napi::Result<()> {
-    let (tx, rx) = channel();
-    self
-      .tx_parcel
-      .send(ParcelMessage::RpcPing { response: tx })
-      .unwrap();
-    Ok(rx.recv().unwrap())
+    if self.parcel.rpc.as_ref().unwrap().ping().is_err() {
+      return Err(napi::Error::from_reason("Failed to run"));
+    }
+    Ok(())
   }
 }
