552e6c49cc047c5e97a480d0e4b1f02dd9afa00b
David Alsh <alshdavid@gmail.com>
2024-06-20 17:23:18 +1000

    Don't need unsafe

--- crates/node-bindings/src/helpers/js_callable/serde.rs
+++ crates/node-bindings/src/helpers/js_callable/serde.rs
@@ -1,10 +1,7 @@
-use std::panic;
-
 use napi::bindgen_prelude::Array;
 use napi::bindgen_prelude::FromNapiValue;
 use napi::Env;
 use napi::JsUnknown;
-use napi::NapiRaw;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 
@@ -14,12 +11,7 @@ pub fn map_params_serde<Params: Serialize + Send + Sync + 'static>(
   Box::new(move |env| {
     let result = env.to_js_value(&params)?;
     if result.is_array()? {
-      // SAFETY: type assertion above
-      let result = panic::catch_unwind::<_, napi::Result<Array>>(|| unsafe {
-        Array::from_napi_value(env.raw(), result.raw())
-      })
-      .map_err(|_| napi::Error::from_reason("Unable to cast to array"))??;
-
+      let result = Array::from_unknown(result)?;
       let mut args = vec![];
 
       for index in 0..result.len() {
