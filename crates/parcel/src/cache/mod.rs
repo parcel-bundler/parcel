3901f415f35531c0ddfbbf89e1e4c76aa4e1e603
pyamada (Remote Dev Environment) <pyamada@atlassian.com>
2024-06-27 01:20:25 +0000

    Add LMDB cache implementation using heed crate

--- crates/parcel/src/cache/mod.rs
+++ crates/parcel/src/cache/mod.rs
@@ -1,29 +1,29 @@
 use std::path::Path;
 
-use lmdb::Transaction;
+use heed::types::{Bytes, Str};
+use heed::EnvOpenOptions;
 
 use parcel_core::cache::Cache;
 
 pub struct LMDBCache {
-  environment: lmdb::Environment,
-  database: lmdb::Database,
+  environment: heed::Env,
+  database: heed::Database<Str, Bytes>,
 }
 
 impl LMDBCache {
   pub fn new() -> anyhow::Result<Self> {
     let rust_cache_path = Path::new(".parcel-cache/rust-cache");
     std::fs::create_dir_all(rust_cache_path)?;
-    let environment = lmdb::Environment::new().open(&rust_cache_path)?;
-    let database = environment.open_db(None)?;
+    let environment = unsafe { EnvOpenOptions::new().open(rust_cache_path) }?;
+    let mut write_txn = environment.write_txn()?;
+    let database = environment.create_database(&mut write_txn, None)?;
+    write_txn.commit()?;
+
     Ok(Self {
       environment,
       database,
     })
   }
-}
-
-fn make_rust_db_key(key: &str) -> String {
-  format!("parcel_rust:{}", key)
 }
 
 impl Cache for LMDBCache {
@@ -34,23 +34,19 @@ impl Cache for LMDBCache {
     // Instead, we should be heavily using transactions so that an entire batch of writes can be
     // committed at once. This both ensure consistency of the DB and improves performance as all
     // reads will come from memory, and all writes will be instantaneous.
-    let mut transaction = self.environment.begin_rw_txn()?;
-
-    // TODO: Generally this LMDB crate has design flaws
-    // it requires us to convert types to owned rather than passing references.
-    // we should consider which wrapper or database back-end to use, ideally a mature implementation.
-    let key = make_rust_db_key(key);
-    let blob = blob.to_vec();
-    transaction.put(self.database, &key, &blob, lmdb::WriteFlags::empty())?;
+    let mut transaction = self.environment.write_txn()?;
+    self.database.put(&mut transaction, &key, &blob)?;
     transaction.commit()?;
     Ok(())
   }
 
   fn get_blob(&self, key: &str) -> anyhow::Result<Vec<u8>> {
-    let transaction = self.environment.begin_ro_txn()?;
-    let key = make_rust_db_key(key);
-    let output = transaction.get(self.database, &key)?;
-    Ok(output.to_vec())
+    let transaction = self.environment.read_txn()?;
+    let output = self
+      .database
+      .get(&transaction, &key)?
+      .ok_or_else(|| anyhow::anyhow!("Key not found"))?;
+    Ok(output.to_vec()) // TODO: We don't need to allocate
   }
 }
 
