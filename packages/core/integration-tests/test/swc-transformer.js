0495efa3fb0048f468c47af103aa51e3ffa55180
pyamada (Remote Dev Environment) <pyamada@atlassian.com>
2024-06-28 07:24:43 +0000

    SWC plugin transformer example
    This commit adds a parcel transformer that executes SWC.
    
    This is a way that parcel can support arbitrary SWC plugins,
    although this might not have good performance, and needs more tweaking
    to be usable.
    
    For some reason, the SWC path resolution doesn't work unless the `.swcrc`
    references the absolute path of the plugin WASM file.
    
    Where you see `/home/ubuntu/parcel-2/...` on the `.swcrc` that's the
    path on my machine.

---
+++ packages/core/integration-tests/test/swc-transformer.js
@@ -0,0 +1,39 @@
+// @flow
+import assert from 'assert';
+import path from 'path';
+import {
+  bundle,
+  distDir,
+  outputFS,
+  removeDistDirectory,
+} from '@parcel/test-utils';
+import {execSync} from 'child_process';
+
+const config = path.join(__dirname, './integration/swc-transformer/.parcelrc');
+
+describe('swc-transformer', function () {
+  beforeEach(async () => {
+    await removeDistDirectory();
+  });
+  afterEach(async () => {
+    await removeDistDirectory();
+  });
+
+  it('should support building with swc', async function () {
+    await execSync('yarn', {
+      cwd: path.join(__dirname, '/integration/swc-transformer'),
+    });
+    await bundle(
+      path.join(__dirname, '/integration/swc-transformer/index.js'),
+      {
+        config,
+        defaultConfig: config,
+      },
+    );
+
+    let file = await outputFS.readFile(path.join(distDir, 'index.js'), 'utf8');
+    assert(file.includes('input file here here 13'));
+    assert(file.includes('swc-transformer'));
+    assert(!file.includes("console.log('HEY')"));
+  });
+});
---
+++ packages/transformers/swc/src/SwcTransformer.js
@@ -0,0 +1,22 @@
+// @flow strict-local
+
+import * as path from 'path';
+// $FlowFixMe swc is untyped?
+import * as swc from '@swc/core';
+import {Transformer} from '@parcel/plugin';
+
+const swcTransformer: Transformer = new Transformer({
+  async transform({asset}) {
+    const code = await asset.getCode();
+    const {code: newCode} = await swc.transform(code, {
+      filename: path.resolve(asset.filePath),
+      cwd: path.dirname(asset.filePath),
+      sourceMaps: true,
+    });
+    console.log(code, newCode);
+    asset.setCode(`/* @parcel/swc-transformer */\n${newCode}`);
+    return [asset];
+  },
+});
+
+export default swcTransformer;
