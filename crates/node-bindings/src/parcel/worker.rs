203b1a3eb0e54d9f1bd02f41293c1b77448d3b3d
David Alsh <alshdavid@gmail.com>
2024-07-22 15:54:10 +1000

    Added JsTransferable and using it to init workers
    Update crates/parcel_napi_helpers/src/transferable.rs
    
    Co-authored-by: Monica <monica.j.olejniczak@gmail.com>
    
    pr comments
    
    small changes
    
    don't need arc

--- crates/node-bindings/src/parcel/worker.rs
+++ crates/node-bindings/src/parcel/worker.rs
@@ -1,10 +1,20 @@
+use std::sync::mpsc::Sender;
+
 use napi::{Env, JsObject, JsUndefined};
 use napi_derive::napi;
 use parcel::rpc::nodejs::NodejsWorker;
+use parcel_napi_helpers::JsTransferable;
 
 #[napi]
-pub fn register_worker(env: Env, worker: JsObject) -> napi::Result<JsUndefined> {
+pub fn register_worker(
+  env: Env,
+  channel: JsTransferable<Sender<NodejsWorker>>,
+  worker: JsObject,
+) -> napi::Result<JsUndefined> {
   let worker = NodejsWorker::new(worker)?;
-  NodejsWorker::register_worker(worker);
+  let tx_worker = channel.take()?;
+  if tx_worker.send(worker).is_err() {
+    return Err(napi::Error::from_reason("Unable to register worker"));
+  }
   env.get_undefined()
 }
--- packages/core/core/src/parcel-v3/ParcelV3.js
+++ packages/core/core/src/parcel-v3/ParcelV3.js
@@ -4,6 +4,8 @@ import path from 'path';
 import {Worker} from 'worker_threads';
 import {ParcelNapi, type ParcelNapiOptions} from '@parcel/rust';
 
+const WORKER_PATH = path.join(__dirname, 'worker', 'index.js');
+
 export type ParcelV3Options = {|
   fs?: ParcelNapiOptions['fs'],
   nodeWorkers?: number,
@@ -37,11 +39,16 @@ export class ParcelV3 {
   async build(): Promise<any> {
     const workers = [];
 
-    for (let i = 0; i < this._internal.nodeWorkerCount; i++) {
-      workers.push(new Worker(path.join(__dirname, 'worker', 'index.js')));
-    }
-
-    let result = await this._internal.build();
+    let result = await this._internal.build({
+      registerWorker: tx_worker => {
+        let worker = new Worker(WORKER_PATH, {
+          workerData: {
+            tx_worker,
+          },
+        });
+        workers.push(worker);
+      },
+    });
 
     for (const worker of workers) worker.terminate();
     return result;
