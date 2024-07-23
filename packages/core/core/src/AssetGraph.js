e645bdd6ddf5e22038a98dfec452d03d04992691
Ben Jervis <ben@jervis.net.au>
2024-07-23 15:09:53 +1000

    Refactor shouldDeferDependency

--- packages/core/core/src/AssetGraph.js
+++ packages/core/core/src/AssetGraph.js
@@ -376,41 +376,60 @@ export default class AssetGraph extends ContentGraph<AssetGraphNode> {
     sideEffects: ?boolean,
     canDefer: boolean,
   ): boolean {
-    let defer = false;
     let dependencySymbols = dependency.symbols;
-    if (
-      dependencySymbols &&
+
+    // Doing this separately keeps Flow happy further down
+    if (!dependencySymbols) {
+      return false;
+    }
+
+    let isDeferrable =
       [...dependencySymbols].every(([, {isWeak}]) => isWeak) &&
       sideEffects === false &&
       canDefer &&
-      !dependencySymbols.has('*')
-    ) {
-      let depNodeId = this.getNodeIdByContentKey(dependency.id);
-      let depNode = this.getNode(depNodeId);
-      invariant(depNode);
+      !dependencySymbols.has('*');
 
-      let assets = this.getNodeIdsConnectedTo(depNodeId);
-      let symbols = new Map(
-        [...dependencySymbols].map(([key, val]) => [val.local, key]),
-      );
-      invariant(assets.length === 1);
-      let firstAsset = nullthrows(this.getNode(assets[0]));
-      invariant(firstAsset.type === 'asset');
-      let resolvedAsset = firstAsset.value;
-      let deps = this.getIncomingDependencies(resolvedAsset);
-      defer = deps.every(
-        d =>
-          d.symbols &&
-          !(d.env.isLibrary && d.isEntry) &&
-          !d.symbols.has('*') &&
-          ![...d.symbols.keys()].some(symbol => {
-            if (!resolvedAsset.symbols) return true;
-            let assetSymbol = resolvedAsset.symbols?.get(symbol)?.local;
-            return assetSymbol != null && symbols.has(assetSymbol);
-          }),
-      );
+    if (!isDeferrable) {
+      return false;
     }
-    return defer;
+
+    let depNodeId = this.getNodeIdByContentKey(dependency.id);
+    let depNode = this.getNode(depNodeId);
+    invariant(depNode);
+
+    let assets = this.getNodeIdsConnectedTo(depNodeId);
+    let symbols = new Map(
+      [...dependencySymbols].map(([key, val]) => [val.local, key]),
+    );
+    invariant(assets.length === 1);
+    let firstAsset = nullthrows(this.getNode(assets[0]));
+    invariant(firstAsset.type === 'asset');
+    let resolvedAsset = firstAsset.value;
+
+    // This doesn't change from here, so checking it now saves
+    // us some calls to `getIncomingDependency`
+    if (!resolvedAsset.symbols) {
+      return true;
+    }
+
+    let deps = this.getIncomingDependencies(resolvedAsset);
+
+    for (const d of deps) {
+      let depIsDeferrable =
+        d.symbols &&
+        !(d.env.isLibrary && d.isEntry) &&
+        !d.symbols.has('*') &&
+        ![...d.symbols.keys()].some(symbol => {
+          let assetSymbol = resolvedAsset.symbols?.get(symbol)?.local;
+          return assetSymbol != null && symbols.has(assetSymbol);
+        });
+
+      if (!depIsDeferrable) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   resolveAssetGroup(
