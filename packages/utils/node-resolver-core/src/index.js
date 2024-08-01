6c21293b5d546c3f83f24a143079fbd72cb25825
pyamada (RDE) <pyamada@atlassian.com>
2024-08-01 04:22:10 +0000

    Fix more flow issues

--- packages/utils/node-resolver-core/src/index.js
+++ packages/utils/node-resolver-core/src/index.js
@@ -7,7 +7,8 @@ export const ResolverBase: typeof ResolverNew = getFeatureFlag(
   'ownedResolverStructures',
 )
   ? ResolverNew
-  : ResolverOld;
+  : // $FlowFixMe unfortunately this can't be typed properly. This may be an issue if something does instanceof checks against a direct reference to @parcel/rust, but will be fine otherwise.
+    ResolverOld;
 
 export {default} from './Wrapper';
 export {init} from '@parcel/rust';
