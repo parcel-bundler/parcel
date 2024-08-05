11e8e5bfb35a2acae8b422e0062d098b2370c4e5
pyamada (RDE) <pyamada@atlassian.com>
2024-08-05 05:47:20 +0000

    Try to change linker flags

--- crates/node-bindings/build.rs
+++ crates/node-bindings/build.rs
@@ -3,7 +3,7 @@ extern crate napi_build;
 
 fn main() {
   #[cfg(target_env = "musl")]
-  println!("cargo::rustc-link-arg=-lpthread");
+  println!("cargo:rustc-link-lib=pthread");
 
   #[cfg(not(target_arch = "wasm32"))]
   napi_build::setup();
