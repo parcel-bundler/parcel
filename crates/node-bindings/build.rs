5e35240fdd1cfe0816050138e4a8506d927de817
pyamada (RDE) <pyamada@atlassian.com>
2024-08-05 06:03:33 +0000

    Remove all minidumper from MUSL builds

--- crates/node-bindings/build.rs
+++ crates/node-bindings/build.rs
@@ -2,9 +2,6 @@
 extern crate napi_build;
 
 fn main() {
-  #[cfg(target_env = "musl")]
-  println!("cargo:rustc-link-lib=pthread");
-
   #[cfg(not(target_arch = "wasm32"))]
   napi_build::setup();
 }
