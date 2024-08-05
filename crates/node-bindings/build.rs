09f1c337098d094461caf95008ea17a023d56348
pyamada (RDE) <pyamada@atlassian.com>
2024-08-05 03:27:18 +0000

    Try to link to pthread on MUSL

--- crates/node-bindings/build.rs
+++ crates/node-bindings/build.rs
@@ -2,6 +2,9 @@
 extern crate napi_build;
 
 fn main() {
+  #[cfg(target_env = "musl")]
+  println!("cargo::rustc-link-arg=libpthread");
+
   #[cfg(not(target_arch = "wasm32"))]
   napi_build::setup();
 }
