3319f938945681f60706ce4989aa9542600589bd
David Alsh <alshdavid@gmail.com>
2024-08-15 09:18:31 +1000

    ci

--- packages/utils/node-resolver-core/src/index.js
+++ packages/utils/node-resolver-core/src/index.js
@@ -1,14 +1,14 @@
 // @flow
 
 import {getFeatureFlag} from '@parcel/feature-flags';
-import {Resolver as ResolverNew, ResolverOld} from '@parcel/rust';
+import {Resolver, ResolverOld} from '@parcel/rust';
 
-export type ResolverBase = typeof ResolverNew;
-export const getResolverBase = () =>
+export const getResolverBase = (): typeof Resolver =>
   getFeatureFlag('ownedResolverStructures')
-    ? ResolverNew
+    ? Resolver
     : // $FlowFixMe unfortunately this can't be typed properly. This may be an issue if something does instanceof checks against a direct reference to @parcel/rust, but will be fine otherwise.
       ResolverOld;
 
+export type {Resolver as ResolverBase} from '@parcel/rust';
 export {default} from './Wrapper';
 export {init} from '@parcel/rust';
