35bd5669302e9a5a9225a0e42bd76c69fe5731e8
Devon Govett <devongovett@gmail.com>
2024-09-28 18:40:45 -0700

    migrate to new sass API

--- packages/transformers/sass/src/SassTransformer.js
+++ packages/transformers/sass/src/SassTransformer.js
@@ -1,10 +1,11 @@
 // @flow
 import {Transformer} from '@parcel/plugin';
 import path from 'path';
+import {extname} from 'path';
 import {EOL} from 'os';
 import SourceMap from '@parcel/source-map';
 import sass from 'sass';
-import {promisify} from 'util';
+import {fileURLToPath, pathToFileURL} from 'url';
 
 // E.g: ~library/file.sass
 const NODE_MODULE_ALIAS_RE = /^~[^/\\]/;
@@ -52,39 +53,47 @@ export default (new Transformer({
 
   async transform({asset, options, config, resolve}) {
     let rawConfig = config ?? {};
-    let sassRender = promisify(sass.render.bind(sass));
     let css;
     try {
       let code = await asset.getCode();
-      let result = await sassRender({
-        ...rawConfig,
-        file: asset.filePath,
-        data: rawConfig.data ? rawConfig.data + EOL + code : code,
-        importer: [
-          ...rawConfig.importer,
+      let data = rawConfig.data ? rawConfig.data + EOL + code : code;
+      let indentedSyntax =
+        rawConfig.syntax === 'indented' ||
+        typeof rawConfig.indentedSyntax === 'boolean'
+          ? rawConfig.indentedSyntax
+          : undefined;
+      let result = await sass.compileStringAsync(data, {
+        // ...rawConfig,
+        url: pathToFileURL(asset.filePath),
+        importers: [
+          // ...rawConfig.importer,
           resolvePathImporter({
             asset,
             resolve,
             includePaths: rawConfig.includePaths,
+            indentedSyntax,
             options,
           }),
         ],
-        indentedSyntax:
-          typeof rawConfig.indentedSyntax === 'boolean'
-            ? rawConfig.indentedSyntax
-            : asset.type === 'sass',
+        syntax: (
+          indentedSyntax != null ? indentedSyntax : asset.type === 'sass'
+        )
+          ? 'indented'
+          : 'scss',
+        sourceMap: !!asset.env.sourceMap,
       });
 
       css = result.css;
-      for (let included of result.stats.includedFiles) {
-        if (included !== asset.filePath) {
-          asset.invalidateOnFileChange(included);
+      for (let included of result.loadedUrls) {
+        let file = fileURLToPath(included);
+        if (file !== asset.filePath) {
+          asset.invalidateOnFileChange(file);
         }
       }
 
-      if (result.map != null) {
+      if (result.sourceMap != null) {
         let map = new SourceMap(options.projectRoot);
-        map.addVLQMap(JSON.parse(result.map));
+        map.addVLQMap(result.sourceMap);
         asset.setMap(map);
       }
     } catch (err) {
@@ -104,94 +113,89 @@ export default (new Transformer({
   },
 }): Transformer);
 
-function resolvePathImporter({asset, resolve, includePaths, options}) {
-  // This is a reimplementation of the Sass resolution algorithm that uses Parcel's
-  // FS and tracks all tried files so they are watched for creation.
-  async function resolvePath(
-    url,
-    prev,
-  ): Promise<{filePath: string, contents: string, ...} | void> {
-    /*
-      Imports are resolved by trying, in order:
-        * Loading a file relative to the file in which the `@import` appeared.
-        * Each custom importer.
-        * Loading a file relative to the current working directory (This rule doesn't really make sense for Parcel).
-        * Each load path in `includePaths`
-        * Each load path specified in the `SASS_PATH` environment variable, which should be semicolon-separated on Windows and colon-separated elsewhere.
+function resolvePathImporter({
+  asset,
+  resolve,
+  includePaths,
+  indentedSyntax,
+  options,
+}) {
+  return {
+    // This is a reimplementation of the Sass resolution algorithm that uses Parcel's
+    // FS and tracks all tried files so they are watched for creation.
+    async canonicalize(url, {containingUrl}) {
+      /*
+        Imports are resolved by trying, in order:
+          * Loading a file relative to the file in which the `@import` appeared.
+          * Each custom importer.
+          * Loading a file relative to the current working directory (This rule doesn't really make sense for Parcel).
+          * Each load path in `includePaths`
+          * Each load path specified in the `SASS_PATH` environment variable, which should be semicolon-separated on Windows and colon-separated elsewhere.
 
-      See: https://sass-lang.com/documentation/js-api#importer
-      See also: https://github.com/sass/dart-sass/blob/006e6aa62f2417b5267ad5cdb5ba050226fab511/lib/src/importer/node/implementation.dart
-    */
+        See: https://sass-lang.com/documentation/js-api#importer
+        See also: https://github.com/sass/dart-sass/blob/006e6aa62f2417b5267ad5cdb5ba050226fab511/lib/src/importer/node/implementation.dart
+      */
 
-    let paths = [path.dirname(prev)];
-    if (includePaths) {
-      paths.push(...includePaths);
-    }
+      let containingPath = fileURLToPath(containingUrl);
+      let paths = [path.dirname(containingPath)];
+      if (includePaths) {
+        paths.push(...includePaths);
+      }
 
-    asset.invalidateOnEnvChange('SASS_PATH');
-    if (options.env.SASS_PATH) {
-      paths.push(
-        ...options.env.SASS_PATH.split(
-          process.platform === 'win32' ? ';' : ':',
-        ).map(p => path.resolve(options.projectRoot, p)),
-      );
-    }
+      asset.invalidateOnEnvChange('SASS_PATH');
+      if (options.env.SASS_PATH) {
+        paths.push(
+          ...options.env.SASS_PATH.split(
+            process.platform === 'win32' ? ';' : ':',
+          ).map(p => path.resolve(options.projectRoot, p)),
+        );
+      }
 
-    const urls = [url];
-    const urlFileName = path.basename(url);
-    if (urlFileName[0] !== '_') {
-      urls.push(path.join(path.dirname(url), `_${urlFileName}`));
-    }
+      const urls = [url];
+      const urlFileName = path.basename(url);
+      if (urlFileName[0] !== '_') {
+        urls.push(path.join(path.dirname(url), `_${urlFileName}`));
+      }
 
-    if (url[0] !== '~') {
-      for (let p of paths) {
-        for (let u of urls) {
-          const filePath = path.resolve(p, u);
-          try {
-            const contents = await asset.fs.readFile(filePath, 'utf8');
-            return {
-              filePath,
-              contents,
-            };
-          } catch (err) {
+      if (url[0] !== '~') {
+        for (let p of paths) {
+          for (let u of urls) {
+            const filePath = path.resolve(p, u);
+            if (await asset.fs.exists(filePath)) {
+              return pathToFileURL(filePath);
+            }
+
             asset.invalidateOnFileCreate({filePath});
           }
         }
       }
-    }
 
-    // If none of the default sass rules apply, try Parcel's resolver.
-    for (let u of urls) {
-      if (NODE_MODULE_ALIAS_RE.test(u)) {
-        u = u.slice(1);
-      }
-      try {
-        const filePath = await resolve(prev, u, {
-          packageConditions: ['sass', 'style'],
-        });
-        if (filePath) {
-          const contents = await asset.fs.readFile(filePath, 'utf8');
-          return {filePath, contents};
+      // If none of the default sass rules apply, try Parcel's resolver.
+      for (let u of urls) {
+        if (NODE_MODULE_ALIAS_RE.test(u)) {
+          u = u.slice(1);
         }
-      } catch (err) {
-        continue;
-      }
-    }
-  }
-
-  return function (rawUrl, prev, done) {
-    const url = rawUrl.replace(/^file:\/\//, '');
-    resolvePath(url, prev)
-      .then(resolved => {
-        if (resolved) {
-          done({
-            file: resolved.filePath,
-            contents: resolved.contents,
+        try {
+          const filePath = await resolve(containingPath, u, {
+            packageConditions: ['sass', 'style'],
           });
-        } else {
-          done();
+          return pathToFileURL(filePath);
+        } catch (err) {
+          continue;
         }
-      })
-      .catch(done);
+      }
+    },
+    async load(url) {
+      let path = fileURLToPath(url);
+      const contents = await asset.fs.readFile(path, 'utf8');
+      return {
+        contents,
+        syntax: (
+          indentedSyntax != null ? indentedSyntax : extname(path) === '.sass'
+        )
+          ? 'indented'
+          : 'scss',
+      };
+    },
   };
 }
