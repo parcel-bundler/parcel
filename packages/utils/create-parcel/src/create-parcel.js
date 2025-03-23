02e8fa8eaed7a009cfd4d77501f6dbb18361a912
Devon Govett <devongovett@gmail.com>
2025-03-23 14:29:01 -0700

    Fix create-parcel on windows

--- packages/utils/create-parcel/src/create-parcel.js
+++ packages/utils/create-parcel/src/create-parcel.js
@@ -132,7 +132,7 @@ async function install(template: string, name: string) {
 
 function spawn(cmd, args, opts) {
   return new Promise((resolve, reject) => {
-    let p = _spawn(cmd, args, opts);
+    let p = _spawn(cmd, args, {...opts, shell: process.platform === 'win32'});
     p.on('close', (code, signal) => {
       if (code || signal) {
         reject(new Error(`${cmd} failed with exit code ${code}`));
